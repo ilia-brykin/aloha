4ba4e036d3ac8689287e806b2ad0d8d4ef8dddec
brykin.ilia@gmail.com <ilia.brykin@prounix.de>
2022-01-07 15:04:38 +0100

    Erweiterung für StartSeite(ID-40)

--- docs/views/PageStart/PageStart.js
+++ docs/views/PageStart/PageStart.js
@@ -5,4 +5,30 @@ export default {
   components: {
     ATranslation,
   },
+  data() {
+    return {
+      frameworks: [
+        {
+          name: "Bootstrap",
+          version: "5.1.x",
+          link: "https://getbootstrap.com/",
+        },
+        {
+          name: "Foundation",
+          version: "6.7.4",
+          link: "https://get.foundation/index.html",
+        },
+        {
+          name: "UIkit",
+          version: "3.9.4",
+          link: "https://getuikit.com/",
+        },
+        {
+          name: "Materialize",
+          version: "1.0.0",
+          link: "https://materializecss.com/",
+        },
+      ],
+    };
+  },
 };
