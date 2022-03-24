e4706e08cd5f8b8237d67989728c9cc91b975be8
brykin.ilia@gmail.com <ilia.brykin@prounix.de>
2022-03-24 12:56:52 +0100

    Neue icons

---
+++ src/AIcon/Icons/Pause.js
@@ -0,0 +1,14 @@
+export default `<g>
+    <rect
+        width="6"
+        height="18"
+        x="2"
+        y="0"
+    />
+    <rect
+        width="6"
+        height="18"
+        x="10"
+        y="0"
+    />
+  </g>`;
