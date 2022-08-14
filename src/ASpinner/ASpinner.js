502556dce07517ec5655d293031b405eda9baf50
Ilia Brykin <ilia.brykin@prounix.de>
2022-08-14 18:06:56 +0200

    ASpinner

--- src/ASpinner/ASpinner.js
+++ src/ASpinner/ASpinner.js
@@ -1,9 +1,9 @@
-import ATranslation from "../ATranslation/ATranslation";
-
-import colors from "../const/colors";
 import {
   h,
 } from "vue";
+
+import ATranslation from "../ATranslation/ATranslation";
+
 
 export default {
   name: "ASpinner",
@@ -11,63 +11,32 @@ export default {
     ATranslation,
   },
   props: {
-    size: {
-      type: String,
-      required: false,
-      default: "1",
-      validator: size => ["1", "2", "3", "4", "5", "6"].indexOf(size) !== -1,
-    },
     tag: {
       type: String,
       required: false,
-      default: "div",
+      default: "span",
+    },
+    class: {
+      type: [String, Object],
+      required: false,
+      default: "",
     },
     text: {
       type: String,
       required: false,
       default: "_LOADING_",
     },
-    type: {
-      type: String,
-      required: false,
-      default: "border",
-      validator: type => ["border", "grow"].indexOf(type) !== -1,
-    },
-    typeColor: {
-      type: String,
-      required: false,
-      validator: typeColor => colors.indexOf(typeColor) !== -1,
-    },
-  },
-  computed: {
-    typeClass() {
-      return `spinner-${ this.type }`;
-    },
-
-    typeColorClass() {
-      if (this.typeColor) {
-        return `text-${ this.typeColor }`;
-      }
-    },
-
-    typeSizeClass() {
-      if (this.size) {
-        return `a_spinner_${ this.size }`;
-      }
-    },
   },
   render() {
     return h(this.tag, {
       class: [
         "a_spinner",
-        this.typeClass,
-        this.typeSizeClass,
-        this.typeColorClass,
+        this.class,
       ],
       role: "status",
     }, [
       this.text && h(ATranslation, {
-        class: "visually-hidden",
+        class: "a_sr_only",
         tag: "span",
         text: this.text,
       }),
