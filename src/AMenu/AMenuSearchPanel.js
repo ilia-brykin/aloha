f97ebd92e0b89038ee1fe3597eadc060044a7a45
Ilia Brykin <ilia.brykin@prounix.de>
2022-10-09 15:44:23 +0200

    AMenu Suche 'Keine Ergebnisse gefunden'

--- src/AMenu/AMenuSearchPanel.js
+++ src/AMenu/AMenuSearchPanel.js
@@ -1,8 +1,14 @@
 import {
+  computed,
   h,
+  toRef,
 } from "vue";
 
 import AMenuSearchGroup from "./AMenuSearchGroup";
+
+import {
+  isEmpty,
+} from "lodash-es";
 
 export default {
   name: "AMenuSearchPanel",
@@ -33,24 +39,43 @@ export default {
       default: undefined,
     },
   },
+  setup(props) {
+    const idsSearchVisible = toRef(props, "idsSearchVisible");
+    const isSearchActive = toRef(props, "isSearchActive");
+
+    const isNoneFound = computed(() => {
+      if (isSearchActive.value) {
+        return isEmpty(idsSearchVisible.value.rest);
+      }
+      return false;
+    });
+
+    return {
+      isNoneFound,
+    };
+  },
   render() {
     if (this.isSearchActive) {
       return h("div", {
         class: "a_menu__panel a_menu__panel_opened a_menu__panel_search",
       }, [
-        h("dl", null, [
-          this.dataProParentList.map((items, groupIndex) => {
-            return h(AMenuSearchGroup, {
-              items,
-              key: groupIndex,
-              groupIndex,
-              dataKeyById: this.dataKeyById,
-              dataProParentChildren: this.dataProParentChildren,
-              idsSearchVisible: this.idsSearchVisible,
-              modelSearch: this.modelSearch,
-            });
-          }),
-        ]),
+        this.isNoneFound ? 
+          h("div", {
+            class: "a_panel__none_found",
+          }, "Keine Ergebnisse gefunden.") :
+          h("dl", null, [
+            this.dataProParentList.map((items, groupIndex) => {
+              return h(AMenuSearchGroup, {
+                items,
+                key: groupIndex,
+                groupIndex,
+                dataKeyById: this.dataKeyById,
+                dataProParentChildren: this.dataProParentChildren,
+                idsSearchVisible: this.idsSearchVisible,
+                modelSearch: this.modelSearch,
+              });
+            }),
+          ]),
       ]);
     }
     return "";
