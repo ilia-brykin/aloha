f0ddb6b8db914a9b1697aea69ffb865d6da3ddf6
Ilia Brykin <ilia.brykin@prounix.de>
2023-03-20 09:22:06 +0100

    Verbesserungen für ANotificationAPI

--- src/compositionAPI/ANotificationAPI.js
+++ src/compositionAPI/ANotificationAPI.js
@@ -15,29 +15,6 @@ const notifications = computed(() => {
 let notificationsCount = 0;
 
 export default function ANotificationAPI() {
-  const removeNotification = notificationIndex => {
-    if (notificationsObj.value[notificationIndex]) {
-      delete notificationsObj.value[notificationIndex];
-    }
-  };
-
-  const addNotification = ({ text, type = "success", timeout }) => {
-    const TIMEOUT_LOCAL = timeout || notificationTimeout;
-    const CURRENT_INDEX = notificationsCount;
-    notificationsObj.value[CURRENT_INDEX] = {
-      text,
-      type,
-      index: CURRENT_INDEX,
-    };
-    notificationsCount++;
-
-    if (TIMEOUT_LOCAL) {
-      setTimeout(() => {
-        removeNotification(CURRENT_INDEX);
-      }, TIMEOUT_LOCAL);
-    }
-  };
-
   const setNotificationTimeout = timeout => {
     notificationTimeout = timeout;
   };
@@ -48,4 +25,27 @@ export default function ANotificationAPI() {
     notifications,
     setNotificationTimeout,
   };
+}
+
+export function addNotification({ text, type = "success", timeout }) {
+  const TIMEOUT_LOCAL = timeout || notificationTimeout;
+  const CURRENT_INDEX = notificationsCount;
+  notificationsObj.value[CURRENT_INDEX] = {
+    text,
+    type,
+    index: CURRENT_INDEX,
+  };
+  notificationsCount++;
+
+  if (TIMEOUT_LOCAL) {
+    setTimeout(() => {
+      removeNotification(CURRENT_INDEX);
+    }, TIMEOUT_LOCAL);
+  }
+}
+
+function removeNotification(notificationIndex) {
+  if (notificationsObj.value[notificationIndex]) {
+    delete notificationsObj.value[notificationIndex];
+  }
 }
