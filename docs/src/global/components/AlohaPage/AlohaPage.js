e2f7246d00d0e00809534c70de507e86afe25047
Ilia Brykin <ilia.brykin@prounix.de>
2023-04-20 18:53:49 +0200

    Neue Komponente AlohaPage

---
+++ docs/src/global/components/AlohaPage/AlohaPage.js
@@ -0,0 +1,9 @@
+export default {
+  name: "AlohaPage",
+  props: {
+    pageTitle: {
+      type: String,
+      required: true,
+    },
+  },
+};
--- docs/src/views/PageShowMore/PageShowMore.js
+++ docs/src/views/PageShowMore/PageShowMore.js
@@ -1,8 +1,11 @@
+import AlohaPage from "../../global/components/AlohaPage/AlohaPage.vue";
+
 import AShowMore from "../../../../src/AShowMore/AShowMore";
 
 export default {
   name: "PageShowMore",
   components: {
+    AlohaPage,
     AShowMore,
   },
 };
