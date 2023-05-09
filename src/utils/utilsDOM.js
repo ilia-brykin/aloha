cc2f671c258e106f0a39a025416d8f6c7069aaec
Ilia Brykin <ilia.brykin@prounix.de>
2023-05-09 16:07:02 +0200

    Code cleanup

--- src/utils/utilsDOM.js
+++ src/utils/utilsDOM.js
@@ -1,6 +1,5 @@
 export function scrollToElement({ element, isMiddleOfScreen = true, behavior = "smooth" }) {
   if (isMiddleOfScreen) {
-    console.log("element.offsetTop", element.offsetTop);
     window.scroll({
       top: element.offsetTop - window.innerHeight / 2,
       behavior,
