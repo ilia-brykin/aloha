353cdd600a897c2da38c8af57343fc97afdb0a62
Ilia Brykin <ilia.brykin@prounix.de>
2023-05-11 14:15:15 +0200

    Icon ChevronDown

--- src/AIcon/Icons/ChevronDown.js
+++ src/AIcon/Icons/ChevronDown.js
@@ -1,10 +1,3 @@
-export default `<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 18 18"
-  height="18"
-  width="18"
->
-  <path
-    d="m 0.01351943,8.9836828 3.22924197,-3.21295 5.7572232,5.7409002 5.7572244,-5.7409002 3.22926,3.21295 -9.0027874,9.0027902 z"
-  />
+export default `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
 </svg>`;
