a1d8dd642603299607c69482cda7d1c6e0065f83
Ilia Brykin <ilia.brykin@prounix.de>
2023-06-26 22:40:37 +0200

    Fix für deploy

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
