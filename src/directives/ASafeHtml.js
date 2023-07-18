2e161ba2eccf4abf15f0e4c7a4d2f1115bde5fb5
Ilia Brykin <ilia.brykin@prounix.de>
2023-07-18 08:54:44 +0200

    Fix für ASafeHtml

--- src/directives/ASafeHtml.js
+++ src/directives/ASafeHtml.js
@@ -1,12 +1,15 @@
 import {
   sanitizeLocal,
 } from "../utils/utils";
+import {
+  isNil,
+} from "lodash-es";
 
 export default {
   beforeMount(el, binding) {
-    el.innerHTML = sanitizeLocal(binding.value);
+    el.innerHTML = sanitizeLocal(isNil(binding.value) ? "" : binding.value);
   },
   updated(el, binding) {
-    el.innerHTML = sanitizeLocal(binding.value);
+    el.innerHTML = sanitizeLocal(isNil(binding.value) ? "" : binding.value);
   },
 };
