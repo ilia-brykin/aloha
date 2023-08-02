e3aad3efdffab37a55fc282db8175aa9250e4251
Ilia Brykin <ilia.brykin@prounix.de>
2023-08-02 13:20:58 +0200

    inBody: true für ASelect

--- src/plugins/ASelectPlugin.js
+++ src/plugins/ASelectPlugin.js
@@ -7,7 +7,7 @@ export const selectPluginOptions = ref({
     buttonClass: undefined,
     countMultiselect: 4,
     data: [],
-    inBody: false,
+    inBody: true,
     isCloseByClick: undefined,
     isDataSimpleArray: false,
     isDeselect: true,
