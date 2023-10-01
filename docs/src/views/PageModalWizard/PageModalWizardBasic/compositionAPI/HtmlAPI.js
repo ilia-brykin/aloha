628889b4b8c0b9a9370782761f07030296d11ee4
Ilia Brykin <ilia.brykin@prounix.de>
2023-10-01 16:24:45 +0200

    Erweiterungen für PageModalWizardBasic

--- docs/src/views/PageModalWizard/PageModalWizardBasic/compositionAPI/HtmlAPI.js
+++ docs/src/views/PageModalWizard/PageModalWizardBasic/compositionAPI/HtmlAPI.js
@@ -1,7 +1,55 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-translation
-  text="_SHOW_MORE_"
-></a-translation>`;
+  const codeHtml = `<a-button
+  id="wizard_basic"
+  class="a_btn a_btn_primary"
+  text="open"
+  @click="openModalWizard"
+></a-button>
+<a-modal-wizard
+  v-if="isModalWizardVisible"
+  size="xxl"
+  selector-close-ids="wizard_basic"
+  header-text="Wizard"
+  :close="closeModalWizard"
+  :steps="wizardSteps"
+>
+  <template
+    v-slot:step1
+  >
+    <a-translation
+      tag="h2"
+      text="_A_MODAL_WIZARD_STEP_1_"
+    ></a-translation>
+    <p>{{ text }}</p>
+  </template>
+  <template
+    v-slot:step2
+  >
+    <a-translation
+      tag="h2"
+      text="_A_MODAL_WIZARD_STEP_2_"
+    ></a-translation>
+    <p>{{ text }}</p>
+  </template>
+  <template
+    v-slot:step3
+  >
+    <a-translation
+      tag="h2"
+      text="_A_MODAL_WIZARD_STEP_3_"
+    ></a-translation>
+    <p>{{ text }}</p>
+  </template>
+  <template
+    v-slot:step4
+  >
+    <a-translation
+      tag="h2"
+      text="_A_MODAL_WIZARD_STEP_4_"
+    ></a-translation>
+    <p>{{ text }}</p>
+  </template>
+</a-modal-wizard>`;
 
   return {
     codeHtml,
