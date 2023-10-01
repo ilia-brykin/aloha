cfd720a6a8c0e2c4919bc066ab6f19dc58e335bf
Ilia Brykin <ilia.brykin@prounix.de>
2023-10-01 22:49:51 +0200

    Ergenzungen für Doku für AWizard(#35)

--- docs/src/views/PageWizard/PageWizard.js
+++ docs/src/views/PageWizard/PageWizard.js
@@ -1,30 +1,25 @@
-import {
-  ref,
-} from "vue";
-
-import AForm from "../../../../src/ui/AForm/AForm";
 import AlohaPage from "../../global/components/AlohaPage/AlohaPage.vue";
 import AlohaTableProps from "../../global/components/AlohaTableProps/AlohaTableProps.vue";
 import AlohaTableTranslate from "../../global/components/AlohaTableTranslate/AlohaTableTranslate.vue";
 import ATranslation from "../../../../src/ATranslation/ATranslation";
-import AWizard from "../../../../src/AWizard/AWizard";
 import PageWizardBasic from "./PageWizardBasic/PageWizardBasic.vue";
-import PageWizardType from "./PageWizardType/PageWizardType.vue";
 import PageWizardButtons from "./PageWizardButtons/PageWizardButtons.vue";
 import PageWizardButtonsDisabled from "./PageWizardButtonsDisabled/PageWizardButtonsDisabled.vue";
 import PageWizardButtonsHide from "./PageWizardButtonsHide/PageWizardButtonsHide.vue";
+import PageWizardIsControlOutside from "./PageWizardIsControlOutside/PageWizardIsControlOutside.vue";
+import PageWizardIsStepNumberVisible from "./PageWizardIsStepNumberVisible/PageWizardIsStepNumberVisible.vue";
 import PageWizardIsStepsJustified from "./PageWizardIsStepsJustified/PageWizardIsStepsJustified.vue";
 import PageWizardIsToolbar from "./PageWizardIsToolbar/PageWizardIsToolbar.vue";
+import PageWizardSlot from "./PageWizardSlot/PageWizardSlot.vue";
+import PageWizardSlotLabel from "./PageWizardSlotLabel/PageWizardSlotLabel.vue";
+import PageWizardSlotToolbar from "./PageWizardSlotToolbar/PageWizardSlotToolbar.vue";
 import PageWizardStepActive from "./PageWizardStepActive/PageWizardStepActive.vue";
 import PageWizardStepsErrors from "./PageWizardStepsErrors/PageWizardStepsErrors.vue";
 import PageWizardStepsWarnings from "./PageWizardStepsWarnings/PageWizardStepsWarnings.vue";
-import PageWizardSlotToolbar from "./PageWizardSlotToolbar/PageWizardSlotToolbar.vue";
-import PageWizardIsStepNumberVisible from "./PageWizardIsStepNumberVisible/PageWizardIsStepNumberVisible.vue";
-import PageWizardSlotLabel from "./PageWizardSlotLabel/PageWizardSlotLabel.vue";
-import PageWizardSlot from "./PageWizardSlot/PageWizardSlot.vue";
-import PageWizardIsControlOutside from "./PageWizardIsControlOutside/PageWizardIsControlOutside.vue";
+import PageWizardType from "./PageWizardType/PageWizardType.vue";
 
 import ExposesAPI from "./compositionAPI/ExposesAPI";
+import EventsAPI from "./compositionAPI/EventsAPI";
 import PageTitleAPI from "./compositionAPI/PageTitleAPI";
 import PropsAPI from "./compositionAPI/PropsAPI";
 import SlotsAPI from "./compositionAPI/SlotsAPI";
@@ -33,27 +28,25 @@ import TranslateAPI from "./compositionAPI/TranslateAPI";
 export default {
   name: "PageWizard",
   components: {
-    AForm,
     AlohaPage,
     AlohaTableProps,
     AlohaTableTranslate,
     ATranslation,
-    AWizard,
     PageWizardBasic,
-    PageWizardType,
     PageWizardButtons,
     PageWizardButtonsDisabled,
     PageWizardButtonsHide,
+    PageWizardIsControlOutside,
+    PageWizardIsStepNumberVisible,
     PageWizardIsStepsJustified,
     PageWizardIsToolbar,
+    PageWizardSlot,
+    PageWizardSlotLabel,
+    PageWizardSlotToolbar,
     PageWizardStepActive,
     PageWizardStepsErrors,
     PageWizardStepsWarnings,
-    PageWizardSlotToolbar,
-    PageWizardIsStepNumberVisible,
-    PageWizardSlotLabel,
-    PageWizardSlot,
-    PageWizardIsControlOutside,
+    PageWizardType,
   },
   setup() {
     const {
@@ -73,156 +66,20 @@ export default {
     } = ExposesAPI();
 
     const {
+      dataEvents,
+    } = EventsAPI();
+
+    const {
       dataTranslate,
     } = TranslateAPI();
 
-    const model = ref({
-      type: "square",
-      isForwardStepButtonDisabled: true,
-      isBackStepButtonDisabled: false,
-      isStepNumberVisible: true,
-      isForwardButtonDisabled: false,
-      isBackButtonDisabled: false,
-      isForwardButtonHide: false,
-      isBackButtonHide: false,
-      isForwardLastButtonHide: false,
-      isBackFirstButtonHide: false,
-      isButtonsLoading: false,
-    });
-
-    const formData = [
-      {
-        id: "type",
-        type: "select",
-        label: "_TYPE_",
-        keyId: "value",
-        keyLabel: "label",
-        data: [
-          {
-            label: "_BASIC_",
-            value: "basic",
-          },
-          {
-            label: "_ARROWS_",
-            value: "arrows",
-          },
-          {
-            label: "_SQUARE_",
-            value: "square",
-          },
-          {
-            label: "_ROUND_",
-            value: "round",
-          },
-        ],
-      },
-      {
-        id: "isForwardStepButtonDisabled",
-        type: "switch",
-        label: "isForwardStepButtonDisabled",
-      },
-      {
-        id: "isBackStepButtonDisabled",
-        type: "switch",
-        label: "isBackStepButtonDisabled",
-      },
-      {
-        id: "isForwardButtonDisabled",
-        type: "switch",
-        label: "isForwardButtonDisabled",
-      },
-      {
-        id: "isBackButtonDisabled",
-        type: "switch",
-        label: "isBackButtonDisabled",
-      },
-      {
-        id: "isForwardButtonHide",
-        type: "switch",
-        label: "isForwardButtonHide",
-      },
-      {
-        id: "isBackButtonHide",
-        type: "switch",
-        label: "isBackButtonHide",
-      },
-      {
-        id: "isForwardLastButtonHide",
-        type: "switch",
-        label: "isForwardLastButtonHide",
-      },
-      {
-        id: "isBackFirstButtonHide",
-        type: "switch",
-        label: "isBackFirstButtonHide",
-      },
-      {
-        id: "isStepNumberVisible",
-        type: "switch",
-        label: "isStepNumberVisible",
-      },
-      {
-        id: "isButtonsLoading",
-        type: "switch",
-        label: "isButtonsLoading",
-      },
-    ];
-
-    const wizardSteps = [
-      {
-        slot: "step1",
-        label: "Schritt 1",
-        title: "Schritt 1",
-      },
-      {
-        slot: "step2",
-        label: "Schritt 2",
-        title: "Schritt 2",
-      },
-      {
-        slot: "step3",
-        label: "Schritt 3",
-        title: "Schritt 3",
-        error: true,
-      },
-      {
-        slot: "step4",
-        label: "Schritt 4",
-        title: "Schritt 4",
-        warning: true,
-      },
-      {
-        slot: "step5",
-        label: "Schritt 5",
-        title: "Schritt 5",
-      },
-      {
-        slot: "step6",
-        label: "Schritt langeeeeeeeeeeeeeeeeeeeeeeee text 6",
-        title: "Schritt 6",
-      },
-      {
-        slot: "step7",
-        label: "Schritt 7",
-        title: "Schritt 7",
-      },
-    ];
-
-    const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-        pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. 
-        Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, 
-        in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent`;
-
     return {
       dataExposes,
+      dataEvents,
       dataProps,
       dataSlots,
       dataTranslate,
       pageTitle,
-      formData,
-      model,
-      text,
-      wizardSteps,
     };
   },
 };
--- docs/src/views/PageWizard/compositionAPI/SlotsAPI.js
+++ docs/src/views/PageWizard/compositionAPI/SlotsAPI.js
@@ -1,8 +1,16 @@
 export default function SlotsAPI() {
   const dataSlots = [
     {
-      name: "default",
-      description: "_A_TRANSLATION_SLOT_DEFAULT_DESCRIPTION_",
+      name: "toolbar",
+      description: "_A_WIZARD_SLOTS_TOOLBAR_DESCRIPTION_",
+    },
+    {
+      name: "slot",
+      description: "_A_WIZARD_SLOTS_SLOT_DESCRIPTION_",
+    },
+    {
+      name: "slotLabel",
+      description: "_A_WIZARD_SLOTS_SLOT_LABEL_DESCRIPTION_",
     },
   ];
 
