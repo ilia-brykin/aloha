d01cc370430317d599f443b2364c71a0f7269e06
Ilia Brykin <ilia.brykin@prounix.de>
2023-11-02 14:42:04 +0100

    Fix für toggleMultipleActions

--- src/ATable/compositionAPI/MultipleActionAPI.js
+++ src/ATable/compositionAPI/MultipleActionAPI.js
@@ -49,6 +49,7 @@ export default function MultipleActionAPI({ emit }, {
       setTimeout(() => {
         checkVisibleColumns();
       });
+      emit("toggleMultipleActions", ({ isActive, action }));
       return;
     }
 
