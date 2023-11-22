e115c8c25a8c9ccb540d69d81abdfa3c827c970e
Ilia Brykin <ilia.brykin@prounix.de>
2023-11-22 11:10:33 +0100

    Toggle CSS-Klass 'a_mobile' in AMobileAPI

--- src/compositionAPI/AMobileAPI.js
+++ src/compositionAPI/AMobileAPI.js
@@ -9,6 +9,11 @@ export const isMobileWidth = ref(undefined);
 export function setIsMobileWidth(isMobileWidthLocal) {
   isMobileWidth.value = isMobileWidthLocal;
   EventBus.$emit("updateViewOnResize", isMobileWidthLocal);
+  if (isMobileWidthLocal) {
+    document.body.classList.add("a_mobile");
+  } else {
+    document.body.classList.remove("a_mobile");
+  }
 }
 
 export default function AMobileAPI() {
