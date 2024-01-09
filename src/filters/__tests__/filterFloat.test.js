6a4c7920254ea84b08d3917dd6179b5bf753800b
Ilia Brykin <ilia.brykin@prounix.de>
2024-01-09 16:13:07 +0100

    Tests for filterFloat

---
+++ src/filters/__tests__/filterFloat.test.js
@@ -0,0 +1,68 @@
+import filterFloat from "../filterFloat";
+
+describe("filterFloat", () => {
+  test("without extra attributes", () => {
+    expect(filterFloat(123456)).toBe("123.456,00");
+    expect(filterFloat(1000)).toBe("1.000,00");
+    expect(filterFloat(1000000)).toBe("1.000.000,00");
+    expect(filterFloat(1000.12)).toBe("1.000,12");
+    expect(filterFloat(0.12)).toBe("0,12");
+    expect(filterFloat(0.1234)).toBe("0,12");
+    expect(filterFloat(0)).toBe("0,00");
+    expect(filterFloat(0.000)).toBe("0,00");
+    expect(filterFloat(undefined)).toBe("");
+    expect(filterFloat(null)).toBe("");
+    expect(filterFloat("")).toBe("0,00");
+    expect(filterFloat("1000.12")).toBe("1.000,12");
+    expect(filterFloat("1.000.000,00")).toBe("1.000.000,00");
+    expect(filterFloat("Aloha")).toBe("Aloha");
+  });
+
+  test("suffix", () => {
+    expect(filterFloat(123456, { suffix: "%" })).toBe("123.456,00 %");
+    expect(filterFloat(1000, { suffix: "€" })).toBe("1.000,00 €");
+    expect(filterFloat("1000", { suffix: "%" })).toBe("1.000,00 %");
+    expect(filterFloat("Aloha", { suffix: "%" })).toBe("Aloha");
+    expect(filterFloat(undefined, { suffix: "%" })).toBe("");
+    expect(filterFloat(null, { suffix: "%" })).toBe("");
+    expect(filterFloat("", { suffix: "%" })).toBe("0,00 %");
+    expect(filterFloat("1.000.000,00", { suffix: "%" })).toBe("1.000.000,00 %");
+  });
+
+  test("digits", () => {
+    expect(filterFloat(123456, { digits: 2 })).toBe("123.456,00");
+    expect(filterFloat(123456, { digits: 4 })).toBe("123.456,0000");
+    expect(filterFloat(123.12345, { digits: 3 })).toBe("123,123");
+    expect(filterFloat(123.12345, { digits: 1 })).toBe("123,1");
+    expect(filterFloat(123.12345, { digits: 0 })).toBe("123");
+    expect(filterFloat(1000.12345, { digits: 0 })).toBe("1.000");
+    expect(filterFloat("", { digits: 0 })).toBe("0");
+    expect(filterFloat("Aloha", { digits: 0 })).toBe("Aloha");
+    expect(filterFloat(undefined, { digits: 0 })).toBe("");
+    expect(filterFloat(null, { digits: 0 })).toBe("");
+    expect(filterFloat("1.000.000,00", { digits: 0 })).toBe("1.000.000");
+  });
+
+  test("digitGrouping", () => {
+    expect(filterFloat(123456, { digitGrouping: true })).toBe("123.456,00");
+    expect(filterFloat(123456, { digitGrouping: false })).toBe("123456,00");
+    expect(filterFloat(1000000, { digitGrouping: true })).toBe("1.000.000,00");
+    expect(filterFloat(1000000, { digitGrouping: false })).toBe("1000000,00");
+  });
+
+  test("negative value", () => {
+    expect(filterFloat(-123456)).toBe("-123.456,00");
+    expect(filterFloat(-1000000)).toBe("-1.000.000,00");
+    expect(filterFloat(-1)).toBe("-1,00");
+    expect(filterFloat(-0)).toBe("0,00");
+    expect(filterFloat("-1.000.000,00")).toBe("-1.000.000,00");
+    expect(filterFloat("-1.000.000,01234")).toBe("-1.000.000,01");
+  });
+
+  test("complex example", () => {
+    expect(filterFloat(1000000, { suffix: "%", digits: 0, digitGrouping: false })).toBe("1000000 %");
+    expect(filterFloat(0.123, { suffix: "%", digits: 0, digitGrouping: false })).toBe("0 %");
+    expect(filterFloat(0.123, { suffix: "%", digits: 4, digitGrouping: false })).toBe("0,1230 %");
+  });
+});
+
