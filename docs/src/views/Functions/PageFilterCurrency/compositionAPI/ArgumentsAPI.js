28f061d100fff2d9db686b6617043d3417500746
Ilia Brykin <ilia.brykin@prounix.de>
2024-01-14 13:33:53 +0100

    Fix for filterCurrency #107

---
+++ docs/src/views/Functions/PageFilterCurrency/compositionAPI/ArgumentsAPI.js
@@ -0,0 +1,37 @@
+export default function ArgumentsAPI() {
+  const argumentsText = [
+    {
+      value: "value",
+      types: [
+        "Number",
+        "String",
+      ],
+      text: "_PAGE_FILTER_CURRENCY_ARGUMENTS_VALUE_",
+    },
+    {
+      value: "[suffix=\"€\"]",
+      types: [
+        "String",
+      ],
+      text: "_PAGE_FILTER_CURRENCY_ARGUMENTS_SUFFIX_",
+    },
+    {
+      value: "[digits=2]",
+      types: [
+        "Number",
+      ],
+      text: "_PAGE_FILTER_CURRENCY_ARGUMENTS_DIGITS_",
+    },
+    {
+      value: "[digitGrouping=true]",
+      types: [
+        "Boolean",
+      ],
+      text: "_PAGE_FILTER_CURRENCY_ARGUMENTS_DIGIT_GROUPING_",
+    },
+  ];
+
+  return {
+    argumentsText,
+  };
+}
