f06b08f3fb7df9be779db27aa1d21581b68bad35
Ilia Brykin <ilia.brykin@prounix.de>
2024-01-14 12:02:04 +0100

    Tests for filterList part 2

--- src/filters/__tests__/filterList.test.js
+++ src/filters/__tests__/filterList.test.js
@@ -42,5 +42,92 @@ describe("filterList", () => {
     expect(filterList(["x", "y", "z"], { isHtml: false })).toBe("x, y, z");
     expect(filterList(123, { isHtml: false })).toBe(123);
   });
+
+  test("separator", () => {
+    expect(filterList(["label 1", "label 2", "label 3"], { isHtml: false, separator: "," })).toBe("label 1,label 2,label 3");
+    expect(filterList(["label 1", "label 2", "label 3"], { isHtml: false, separator: "; " })).toBe("label 1; label 2; label 3");
+    expect(filterList(["x", "y", "z"], { isHtml: false, separator: " - " })).toBe("x - y - z");
+  });
+
+  test("tag", () => {
+    expect(filterList(["label 1", "label 2", "label 3"], { tag: "ul" })).toBe("<ul><li>label 1</li><li>label 2</li><li>label 3</li></ul>");
+    expect(filterList(["label 1", "label 2", "label 3"], { tag: "ol" })).toBe("<ol><li>label 1</li><li>label 2</li><li>label 3</li></ol>");
+  });
+
+  test("listClass", () => {
+    expect(filterList(["label 1", "label 2", "label 3"], { listClass: "test" })).toBe("<ul class=\"test\"><li>label 1</li><li>label 2</li><li>label 3</li></ul>");
+    expect(filterList(["label 1", "label 2", "label 3"], { listClass: "a_list_without_styles" })).toBe("<ul class=\"a_list_without_styles\"><li>label 1</li><li>label 2</li><li>label 3</li></ul>");
+  });
+
+  test("keyChildren", () => {
+    const LIST = [
+      {
+        label: "Level 1",
+        children: [
+          {
+            label: "Level 1.1",
+            children: [
+              {
+                label: "Level 1.1.1",
+              },
+              {
+                label: "Level 1.1.2",
+              },
+            ],
+          },
+          {
+            label: "Level 1.2",
+          },
+        ],
+      },
+      {
+        label: "Level 2",
+        children: [
+          {
+            label: "Level 2.1",
+          },
+          {
+            label: "Level 2.2",
+          },
+        ],
+      },
+    ];
+
+    expect(filterList(LIST, {
+      keyLabel: "label",
+      keyChildren: "children",
+    })).toBe("<ul><li>Level 1<ul><li>Level 1.1<ul><li>Level 1.1.1</li><li>Level 1.1.2</li></ul></li><li>Level 1.2</li></ul></li><li>Level 2<ul><li>Level 2.1</li><li>Level 2.2</li></ul></li></ul>");
+    expect(filterList(LIST, {
+      keyLabel: "label",
+      keyChildren: "children",
+      listClass: "a_list_without_styles"
+    })).toBe("<ul class=\"a_list_without_styles\"><li>Level 1<ul class=\"a_list_without_styles\"><li>Level 1.1<ul class=\"a_list_without_styles\"><li>Level 1.1.1</li><li>Level 1.1.2</li></ul></li><li>Level 1.2</li></ul></li><li>Level 2<ul class=\"a_list_without_styles\"><li>Level 2.1</li><li>Level 2.2</li></ul></li></ul>");
+    expect(filterList(LIST, {
+      keyLabel: "label",
+      keyChildren: "children",
+      tag: "ol",
+    })).toBe("<ol><li>Level 1<ol><li>Level 1.1<ol><li>Level 1.1.1</li><li>Level 1.1.2</li></ol></li><li>Level 1.2</li></ol></li><li>Level 2<ol><li>Level 2.1</li><li>Level 2.2</li></ol></li></ol>");
+  });
+
+  test("tree isSimpleArray=true", () => {
+    const LIST = ["Level 1", ["Level 2", ["Level 2.1", "Level 2.2"]], ["Level 3", ["Level 3.1", ["Level 3.2", ["Level 3.2.1", "Level 3.2.2"]]]]];
+
+    expect(filterList(LIST, { isSimpleArray: true, })).toBe("<ul><li>Level 1</li><li>Level 2<ul><li>Level 2.1</li><li>Level 2.2</li></ul></li><li>Level 3<ul><li>Level 3.1</li><li>Level 3.2<ul><li>Level 3.2.1</li><li>Level 3.2.2</li></ul></li></ul></li></ul>");
+
+    expect(filterList(LIST, {
+      isSimpleArray: true,
+      listClass: "a_list_without_styles",
+    })).toBe("<ul class=\"a_list_without_styles\"><li>Level 1</li><li>Level 2<ul class=\"a_list_without_styles\"><li>Level 2.1</li><li>Level 2.2</li></ul></li><li>Level 3<ul class=\"a_list_without_styles\"><li>Level 3.1</li><li>Level 3.2<ul class=\"a_list_without_styles\"><li>Level 3.2.1</li><li>Level 3.2.2</li></ul></li></ul></li></ul>");
+
+    expect(filterList(LIST, {
+      isSimpleArray: true,
+      tag: "ol",
+    })).toBe("<ol><li>Level 1</li><li>Level 2<ol><li>Level 2.1</li><li>Level 2.2</li></ol></li><li>Level 3<ol><li>Level 3.1</li><li>Level 3.2<ol><li>Level 3.2.1</li><li>Level 3.2.2</li></ol></li></ol></li></ol>");
+
+    expect(filterList([["x1", ["x1.1", "x1.2"]]], { isSimpleArray: true })).toBe("<ul><li>x1<ul><li>x1.1</li><li>x1.2</li></ul></li></ul>");
+
+    expect(filterList(["x0", ["x1", ["x1.1", "x1.2"]], "x3"], { isSimpleArray: true })).toBe("<ul><li>x0</li><li>x1<ul><li>x1.1</li><li>x1.2</li></ul></li><li>x3</li></ul>");
+  });
 });
+
 
