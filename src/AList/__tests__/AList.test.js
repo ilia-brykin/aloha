efa82bf5649017f9674632a185e834de4f74f093
Ilia Brykin <ilia.brykin@prounix.de>
2024-01-19 21:34:03 +0100

    Fix Error in Test Alist

--- src/AList/__tests__/AList.test.js
+++ src/AList/__tests__/AList.test.js
@@ -20,7 +20,7 @@ describe("AList", () => {
       },
     });
 
-    expect(wrapper.findAll("li")).toHaveLength(5);
+    expect(wrapper.findAll("li")).toHaveLength(4);
     expect(wrapper.text()).toContain("label 1label 2label 3label 4");
     expect(wrapper.html({ raw: true })).toBe("<ul><li><span>label 1</span></li><li><span>label 2</span></li><li><span>label 3</span></li><li><span>label 4</span></li></ul>");
   });
