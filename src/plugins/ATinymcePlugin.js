6ba78019105ecc88b10d34187e8983df4aa5bbbb
Ilia Brykin <ilia.brykin@prounix.de>
2024-01-31 16:14:47 +0100

    Erweiterungen für TinyMCE und MyPowerPaste plugin

--- src/plugins/ATinymcePlugin.js
+++ src/plugins/ATinymcePlugin.js
@@ -4,7 +4,40 @@ import {
 
 export const tinymcePluginOptions = ref({
   propsDefault: {
-    validElements: "a[href|target=_blank],strong/b,div,br,p,span,ul,ol,li,table,thead,tbody,th,tr,td",
+    branding: true,
+    contentCustomStyle: undefined,
+    contentLangs: [
+      { title: "English", code: "en" },
+      { title: "Spanish", code: "es" },
+      { title: "French", code: "fr" },
+      { title: "Deutsch", code: "de" },
+      { title: "Portuguese", code: "pt" },
+      { title: "Chinese", code: "zh" },
+    ],
+    languageDefault: "de",
+    maxlength: undefined,
+    menu: undefined,
+    menubar: false,
+    plugins: "advlist code emoticons link lists table help example",
+    promotion: false,
+    rows: undefined,
+    toolbar: [
+      { name: "Formatierung", items: ["bold", "italic", "underline"] },
+      { name: "Ausrichtung", items: ["alignleft", "aligncenter", "alignright", "alignjustify"] },
+      { name: "Liste", items: ["bullist", "numlist"] },
+      { name: "Einrücken", items: ["indent", "outdent"] },
+      { name: "Sprache", items: ["language"] },
+      { name: "Link", items: ["link", "unlink"] },
+      { name: "Historie", items: ["undo", "redo"] },
+      { name: "Hilfe", items: ["help"] },
+    ],
+    toolbarMode: "wrap",
+    validElements: "@[style],a[href|target|title],strong/b[style],em/i[style],div[style],br[],p[style],span[style],ul[style],ol[style],li[style],table[],thead[],tbody[],th[],tr[],td[]",
+    validStyles: {
+      "*": "text-align,padding-left,text-decoration",
+      ul: "list-style-type",
+      ol: "list-style-type",
+    },
   },
 });
 
