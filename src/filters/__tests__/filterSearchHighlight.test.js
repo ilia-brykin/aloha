9b32776f3c8288737932ec65d34f003369a4cc75
ilia.brykin <brykin.ilia@gmail.com>
2024-02-14 22:45:20 +0100

    Test for filterSearchHighlight

---
+++ src/filters/__tests__/filterSearchHighlight.test.js
@@ -0,0 +1,28 @@
+import filterSearchHighlight from "../filterSearchHighlight";
+
+describe("filterSearchHighlight function", () => {
+  // test focuses on a single use case
+  it("should return empty string when the value provided is non truthy", () => {
+    const result = filterSearchHighlight(null, { searchModel: "", searchClass: "a_search_highlight" });
+    expect(result).toBe("");
+  });
+
+  it("should return original value when searchModel is empty, undefined or null", () => {
+    let result = filterSearchHighlight("test value", { searchModel: "", searchClass: "a_search_highlight" });
+    expect(result).toBe("test value");
+
+    result = filterSearchHighlight("test value", { searchModel: undefined, searchClass: "a_search_highlight" });
+    expect(result).toBe("test value");
+
+    result = filterSearchHighlight("test value", { searchModel: null, searchClass: "a_search_highlight" });
+    expect(result).toBe("test value");
+  });
+
+  it("should return value with replaced searchModel content wrapped in span with searchClass, when searchModel is provided", () => {
+    let result = filterSearchHighlight("test searchModel", { searchModel: "searchModel", searchClass: "test" });
+    expect(result).toBe('test <span class="test">searchModel</span>');
+
+    result = filterSearchHighlight("test searchModel", { searchModel: "searchModel" });
+    expect(result).toBe('test <span class="a_search_highlight">searchModel</span>');
+  });
+});
