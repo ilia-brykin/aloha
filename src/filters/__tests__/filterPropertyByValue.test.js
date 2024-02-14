b85b07d8d6422ea83dfdaf3a7f392d1c1bd87a5c
ilia.brykin <brykin.ilia@gmail.com>
2024-02-14 22:49:29 +0100

    Tests for filterPropertyByValue

---
+++ src/filters/__tests__/filterPropertyByValue.test.js
@@ -0,0 +1,18 @@
+import filterPropertyByValue from "../filterPropertyByValue";
+
+describe("Filter property by value", () => {
+  test("Returns the mapped value if it exists", () => {
+    const mapping = { "20": "twenty" };
+    expect(filterPropertyByValue("20", { mapping })).toEqual("twenty");
+  });
+
+  test("Returns the defaultValue if the value does not exist in the mapping", () => {
+    const mapping = { "30": "thirty" };
+    expect(filterPropertyByValue("20", { mapping, defaultValue: "default" })).toEqual("default");
+  });
+
+  test("Returns an empty string if the value does not exist in the mapping and no defaultValue is set", () => {
+    const mapping = { "30": "thirty" };
+    expect(filterPropertyByValue("20", { mapping })).toEqual("");
+  });
+});
