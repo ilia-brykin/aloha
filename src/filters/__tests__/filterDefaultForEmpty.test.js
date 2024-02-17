5a8038ec650b522b72dc454664a783f5c463d947
ilia.brykin <brykin.ilia@gmail.com>
2024-02-17 19:25:19 +0100

    Test for filterDefaultForEmpty

---
+++ src/filters/__tests__/filterDefaultForEmpty.test.js
@@ -0,0 +1,18 @@
+import filterDefaultForEmpty from "../filterDefaultForEmpty";
+
+describe("filterDefaultForEmpty Function", () => {
+  test("should return 'emptyValue', when the input is null", () => {
+    expect(filterDefaultForEmpty(null)).toBe("-");
+    expect(filterDefaultForEmpty(null, { emptyValue: "default" })).toBe("default");
+  });
+
+  test("should return 'emptyValue', when the input is an empty string", () => {
+    expect(filterDefaultForEmpty("")).toBe("-");
+    expect(filterDefaultForEmpty("", { emptyValue: "default" })).toBe("default");
+  });
+
+  test("should return the inputted value, when it is not null or an empty string", () => {
+    expect(filterDefaultForEmpty("Non-empty value")).toBe("Non-empty value");
+    expect(filterDefaultForEmpty(123)).toBe(123);
+  });
+});
