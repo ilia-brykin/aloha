6a76d2426572f1b5defd15438eb0297afa68a7ac
ilia.brykin <brykin.ilia@gmail.com>
2024-02-17 19:15:22 +0100

    Test for filterCurrency

---
+++ src/filters/__tests__/filterCurrency.test.js
@@ -0,0 +1,15 @@
+import filterCurrency from "../filterCurrency";
+
+describe("filterCurrency", () => {
+  it("should properly format a given number with default parameters", () => {
+    expect(filterCurrency(1234.5678)).toEqual("1.234,57 €");
+  });
+
+  it("should properly format a given number with custom parameters", () => {
+    expect(filterCurrency(1234.5678, { suffix: "$", digits: 3, digitGrouping: false })).toEqual("1234,568 $");
+  });
+
+  it("should return undefined when a non-number value is passed", () => {
+    expect(filterCurrency(null)).toEqual("");
+  });
+});
