fbb2df92f1196b1cf90b7ad5bc47a73bcc2987b6
nikita.kovalev <nikita.kovalev@prounix.de>
2024-02-27 16:02:06 +0100

    Fix AFilters layout for touch

--- src/AFilters/AFiltersHorizontal/compositionAPI/FormAPI.js
+++ src/AFilters/AFiltersHorizontal/compositionAPI/FormAPI.js
@@ -17,11 +17,12 @@ export default function FormAPI(props) {
     forEach(filtersGroup.value.alwaysVisible, filter => {
       const FILTER = cloneDeep(filter);
       FILTER.labelClass = "a_column a_column_12_touch a_column_3_fullhd a_column_3_widescreen a_column_3_desktop";
-      FILTER.classColumn = "a_column a_column_12_touch a_column_9_fullhd a_column_9_widescreen a_column_9_desktop";
+      FILTER.classColumn = "a_filters__form_element a_column a_column_12_touch a_column_9_fullhd a_column_9_widescreen a_column_9_desktop";
 
       DATA.push({
         type: "group",
         id: `group_${ filter.id }`,
+        classColumns: "a_columns a_columns_count_12 a_columns_gab_x_2 a_columns_gab_y_2 a_columns_gab_y_1_touch",
         children: [
           FILTER
         ],
@@ -31,13 +32,14 @@ export default function FormAPI(props) {
     forEach(filtersVisible.value, filter => {
       const FILTER = cloneDeep(filter);
       FILTER.labelClass = "a_column a_column_12_touch a_column_3_fullhd a_column_3_widescreen a_column_3_desktop";
-      FILTER.classColumn = "a_column a_column_12_touch a_column_9_fullhd a_column_9_widescreen a_column_9_desktop a_d_flex a_align_items_center";
+      FILTER.classColumn = "a_filters__form_element a_column a_column_12_touch a_column_9_fullhd a_column_9_widescreen a_column_9_desktop a_d_flex a_align_items_center";
       FILTER.slotAppend = "groupAppend";
       FILTER.class = "a_width_100";
 
       DATA.push({
         type: "group",
         id: `group_${ filter.id }`,
+        classColumns: "a_columns a_columns_count_12 a_columns_gab_x_2 a_columns_gab_y_2 a_columns_gab_y_1_touch",
         children: [
           FILTER
         ],
