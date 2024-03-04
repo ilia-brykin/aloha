cb0e216f36c4ac8c2a62e297d67acc09f34077c0
nikita.kovalev <nikita.kovalev@prounix.de>
2024-03-04 20:32:14 +0100

    Check columns ordering after drag and drop

--- src/ATable/compositionAPI/ColumnsOrderingAPI.js
+++ src/ATable/compositionAPI/ColumnsOrderingAPI.js
@@ -49,7 +49,9 @@ export default function ColumnsOrderingAPI(props, { emit }, {
       columnIndexOver,
       modelColumnsOrdering: modelColumnsOrderingLocal,
     });
-    checkVisibleColumns();
+    setTimeout(() => {
+      checkVisibleColumns();
+    });
   };
 
   const checkColumnsOrdering = () => {
