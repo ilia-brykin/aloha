3d534e08d6d3139b33334d65810417a4001ed575
Ilia Brykin <ilia.brykin@prounix.de>
2024-03-13 14:00:51 +0100

    ATableSortingAdditional.DropdownAPI.test

---
+++ src/ATable/ATableSortingAdditional/__tests__/ATableSortingAdditional.DropdownAPI.test.js
@@ -0,0 +1,40 @@
+import {
+  computed,
+} from "vue";
+
+import DropdownAPI from "../compositionAPI/DropdownAPI";
+
+describe("ATableSortingAdditional DropdownAPI", () => {
+  it("hides the dropdown when there are no columns available", () => {
+    const columnsAll = computed(() => {
+      return [];
+    });
+
+    const {
+      isDropdownVisible,
+    } = DropdownAPI({}, {
+      columnsAll,
+    });
+
+    expect(isDropdownVisible.value).toBe(false);
+  });
+
+  it("shows the dropdown when there are available columns", () => {
+    const columnsAll = computed(() => {
+      return [
+        { label: "Column 1", sortId: "1" },
+        { label: "Column 2", sortId: "2" },
+        { label: "Column 3", sortId: "3" },
+        { label: "Column 4", sortId: "4" },
+      ];
+    });
+
+    const {
+      isDropdownVisible,
+    } = DropdownAPI({}, {
+      columnsAll,
+    });
+
+    expect(isDropdownVisible.value).toBe(true);
+  });
+});
