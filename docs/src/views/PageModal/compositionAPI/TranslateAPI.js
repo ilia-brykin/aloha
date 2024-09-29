09b5d06a7eb1dd8b1b5befd90e9d95c51066773e
Ilia <brykin.ilia@gmail.com>
2024-09-29 14:32:24 +0200

    AModal docu translations

--- docs/src/views/PageModal/compositionAPI/TranslateAPI.js
+++ docs/src/views/PageModal/compositionAPI/TranslateAPI.js
@@ -2,6 +2,9 @@ export default function TranslateAPI() {
   const dataTranslate = [
     "_A_MODAL_BTN_CANCEL_",
     "_A_MODAL_BTN_SAVE_",
+    "_A_MODAL_BTN_TEXT_SCREEN_READER_CLOSE_HEADER_",
+    "_A_MODAL_BTN_TEXT_SCREEN_READER_CLOSE_FOOTER_",
+    "_A_MODAL_BTN_TEXT_SCREEN_READER_SAVE_",
   ];
 
   return {
