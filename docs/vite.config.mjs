3a11f7f0c13f6514a4b84e4ead1c763404f3f1a5
Ilia <brykin.ilia@gmail.com>
2024-12-20 18:53:09 +0100

    deploy fix 2

--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -5,6 +5,16 @@ import {
   defineConfig,
 } from "vite";
 
+import {
+  fileURLToPath,
+} from "url";
+import {
+  dirname,
+} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const isDevelopment = process.env.DEV === "true";
 
 export default defineConfig({
