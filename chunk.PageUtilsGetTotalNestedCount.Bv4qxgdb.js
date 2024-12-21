669c24ee9f82558c1b313b0862b013a1ab3c1261
ilia-brykin <ilia-brykin@users.noreply.github.com>
2024-12-21 13:47:03 +0000

    deploy: 045d222c2f915222bb574bc77a3350b804e6e629

---
+++ chunk.PageUtilsGetTotalNestedCount.Bv4qxgdb.js
@@ -0,0 +1,61 @@
+import{A as y,a as C}from"./chunk.AlohaExample.DKBt6BF9.js";import{R as o,_ as s,A as N}from"./bundle.index.hzSDncRt.js";import{P as f,a as A,b as x}from"./chunk.PageFilterTest.DTs_uuYz.js";import{k as t,J as i,H as d,aO as E,O as n}from"./chunk.vendor.CwuJ9Ljq.js";import"./chunk.vendor-lodash.BBRAkY5X.js";import"./chunk.translations-ar.CGH9gktb.js";import"./chunk.translations-de.S4PF1KYg.js";import"./chunk.translations-en.Ck05aPwN.js";import"./chunk.translations-es.CN4IYWAw.js";import"./chunk.translations-fr.CCwWtZH3.js";import"./chunk.translations-hr.C7fo1LlO.js";import"./chunk.translations-it.BFH_rAJL.js";import"./chunk.translations-ru.C3zXigta.js";function k(){return{codeJs:`getTotalNestedCount({ array: ["hello", "Aloha"], keyChildren: "" });
+// ${o({array:["hello","Aloha"],keyChildren:""})}
+
+getTotalNestedCount({ array: [], keyChildren: "" });
+// ${o({array:[],keyChildren:""})}
+
+getTotalNestedCount({ array: [{ children: ["hello", "world"] }, "test"], keyChildren: "children" });
+// ${o({array:[{children:["hello","world"]},"test"],keyChildren:"children"})}
+
+getTotalNestedCount({ array: [{ aloha: [{ aloha: ["hello", "world"] }, { aloha: ["hello1", "world1"] }] }, "test"], keyChildren: "aloha" });
+// ${o({array:[{aloha:[{aloha:["hello","world"]},{aloha:["hello1","world1"]}]},"test"],keyChildren:"aloha"})}`}}const w={name:"PageUtilsGetTotalNestedCountExample",components:{AlohaExample:y},setup(){const{codeJs:e}=k();return{codeJs:e}}};function P(e,a,l,c,u,h){const r=t("aloha-example");return d(),i(r,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const $=s(w,[["render",P]]);function G(){return{argumentsText:[{value:"array",types:["Array"],text:"_PAGE_UTILS_GET_TOTAL_NESTED_COUNT_ARGUMENTS_ARRAY_"},{value:"keyChildren",types:["String"],text:"_PAGE_UTILS_GET_TOTAL_NESTED_COUNT_ARGUMENTS_KEY_CHILDREN_"}]}}function U(){return{pageTitle:"getTotalNestedCount"}}const S=`import {
+  getTotalNestedCount,
+} from "../utils";
+
+describe("getTotalNestedCount function", () => {
+  test("should correctly count elements in a flat array without nested children", () => {
+    const array = ["hello", "world"];
+
+    expect(getTotalNestedCount({ array, keyChildren: "test" })).toBe(2);
+  });
+
+  test("should accurately count elements in a mixed array with and without specified nested children", () => {
+    const array = [{ children: ["hello", "world"] }, "test"];
+
+    expect(getTotalNestedCount({ array, keyChildren: "children" })).toBe(4);
+    expect(getTotalNestedCount({ array, keyChildren: "" })).toBe(2);
+  });
+
+  test("should handle deeply nested structures and count all elements correctly", () => {
+    const array = [
+      {
+        children: [
+          { children: ["hello", "world"] },
+          { children: ["hello", "world"] },
+        ],
+      },
+      {
+        children: [
+          { children: ["hello", "world"] },
+          { children: ["hello", "world"] },
+          { children: ["hello", "world"] },
+        ],
+      },
+      {
+        children: [
+          { children: ["hello", "world"] },
+        ],
+      },
+    ];
+
+    expect(getTotalNestedCount({ array, keyChildren: "children" })).toBe(21);
+  });
+
+  test("should return zero for an empty array, indicating no elements to count", () => {
+    const array = [];
+
+    expect(getTotalNestedCount({ array, keyChildren: "test" })).toBe(0);
+  });
+});
+
+`,I={name:"PageUtilsGetTotalNestedCount",components:{AlohaPage:C,ATranslation:N,PageFilterArguments:f,PageFilterImportFunction:A,PageFilterTest:x,PageUtilsGetTotalNestedCountExample:$},setup(){const{pageTitle:e}=U(),{argumentsText:a}=G();return{argumentsText:a,pageTitle:e,test:S}}};function O(e,a,l,c,u,h){const r=t("a-translation"),p=t("page-filter-import-function"),_=t("page-filter-arguments"),m=t("page-utils-get-total-nested-count-example"),g=t("page-filter-test"),T=t("aloha-page");return d(),i(T,{"page-title":e.pageTitle},{body:E(()=>[n(r,{tag:"p",html:"_PAGE_UTILS_GET_TOTAL_NESTED_COUNT_DESCRIPTION_"}),n(p,{"function-name":"getTotalNestedCount","type-import":"utils"}),n(_,{"arguments-text":e.argumentsText,"function-description":"getTotalNestedCount({ array, keyChildren })"},null,8,["arguments-text"]),n(m),n(g,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const K=s(I,[["render",O]]);export{K as default};
---
+++ chunk.PageUtilsMathIsEven.CePIPGfg.js
@@ -0,0 +1,66 @@
+import{A as d,a as x}from"./chunk.AlohaExample.DKBt6BF9.js";import{J as t,_ as r,A as h}from"./bundle.index.hzSDncRt.js";import{P as A,a as T,b as $}from"./chunk.PageFilterTest.DTs_uuYz.js";import{k as n,J as p,H as l,aO as y,O as s}from"./chunk.vendor.CwuJ9Ljq.js";import"./chunk.vendor-lodash.BBRAkY5X.js";import"./chunk.translations-ar.CGH9gktb.js";import"./chunk.translations-de.S4PF1KYg.js";import"./chunk.translations-en.Ck05aPwN.js";import"./chunk.translations-es.CN4IYWAw.js";import"./chunk.translations-fr.CCwWtZH3.js";import"./chunk.translations-hr.C7fo1LlO.js";import"./chunk.translations-it.BFH_rAJL.js";import"./chunk.translations-ru.C3zXigta.js";function B(){return{codeJs:`isEven(2);
+// ${t(2)}
+isEven(0);
+// ${t(0)}
+isEven(246);
+// ${t(246)}
+
+isEven(-2);
+// ${t(-2)}
+
+isEven(1);
+// ${t(1)}
+isEven(3);
+// ${t(3)}
+isEven(353);
+// ${t(353)}
+
+isEven(-1);
+// ${t(-1)}
+
+isEven(2.5);
+// ${t(2.5)}
+
+isEven("Aloha");
+// ${t("Aloha")}
+isEven(null);
+// ${t(null)}
+isEven(undefined);
+// ${t(void 0)}`}}const F={name:"PageUtilsMathIsOddExample",components:{AlohaExample:d},setup(){const{codeJs:e}=B();return{codeJs:e}}};function P(e,a,i,c,u,m){const o=n("aloha-example");return l(),p(o,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const I=r(F,[["render",P]]);function b(){return{argumentsText:[{value:"value",types:["Number"],text:"_PAGE_UTILS_MATH_IS_EVEN_ARGUMENTS_VALUE_"}]}}function M(){return{pageTitle:"isEven"}}const U=`import {
+  isEven,
+} from "../utilsMath";
+
+describe("isEven function", () => {
+  test("Return true if given an even number", () => {
+    expect(isEven(0)).toBeTruthy();
+    expect(isEven(2)).toBeTruthy();
+    expect(isEven(123454568)).toBeTruthy();
+  });
+
+  test("Return false if given an odd number", () => {
+    expect(isEven(1)).toBeFalsy();
+    expect(isEven(3)).toBeFalsy();
+    expect(isEven(123457)).toBeFalsy();
+  });
+
+  test("Return false if given a non-integer", () => {
+    expect(isEven(2.5)).toBeFalsy();
+    expect(isEven("Aloha")).toBeFalsy();
+    expect(isEven(null)).toBeFalsy();
+    expect(isEven(undefined)).toBeFalsy();
+    expect(isEven({})).toBeFalsy();
+    expect(isEven([])).toBeFalsy();
+  });
+
+  test("Return true if given a negative even number", () => {
+    expect(isEven(-2)).toBeTruthy();
+    expect(isEven(-346)).toBeTruthy();
+  });
+
+  test("Return false if given a negative odd number", () => {
+    expect(isEven(-1)).toBeFalsy();
+    expect(isEven(-3)).toBeFalsy();
+    expect(isEven(-267)).toBeFalsy();
+  });
+});
+`,S={name:"PageUtilsMathIsEven",components:{AlohaPage:x,ATranslation:h,PageFilterArguments:A,PageFilterImportFunction:T,PageFilterTest:$,PageUtilsMathIsEvenExample:I},setup(){const{pageTitle:e}=M(),{argumentsText:a}=b();return{argumentsText:a,pageTitle:e,test:U}}};function J(e,a,i,c,u,m){const o=n("a-translation"),_=n("page-filter-import-function"),v=n("page-filter-arguments"),E=n("page-utils-math-is-even-example"),g=n("page-filter-test"),f=n("aloha-page");return l(),p(f,{"page-title":e.pageTitle},{body:y(()=>[s(o,{tag:"p",html:"_PAGE_UTILS_MATH_IS_EVEN_DESCRIPTION_"}),s(_,{"function-name":"isEven","type-import":"utilsMath"}),s(v,{"arguments-text":e.argumentsText,"function-description":"isEven(value)"},null,8,["arguments-text"]),s(E),s(g,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const z=r(S,[["render",J]]);export{z as default};
