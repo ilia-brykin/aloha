c4cd80da0783cc5179ceb272eaa28224ff55baca
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-03-22 14:51:24 +0000

    deploy: 30b028064b493cc8ca26c997bf20e959ddd95aa0

---
+++ chunk.PageSvg.CHpb4Cao.js
@@ -0,0 +1,11 @@
+import{r as a,_ as l}from"./bundle.index.BJbfSpRI.js";import{V as r,W as o,w as n,bn as p,U as s}from"./chunk.vendor.C8MJL5OZ.js";import"./chunk.vendor-lodash.D7ZMGyiW.js";import"./chunk.translations-ar.CSRM3YJ8.js";import"./chunk.translations-de.Cdlrfk3w.js";import"./chunk.translations-en.BVrNHVVQ.js";import"./chunk.translations-es.DXEdiuYo.js";import"./chunk.translations-fr.CVKEU5Mu.js";import"./chunk.translations-hr.DL1l5v8s.js";import"./chunk.translations-it.BKw_NW0S.js";import"./chunk.translations-ru.CQ8apMH7.js";const m={name:"PageSvg",directives:{ASafeHtml:a},setup(){return{linkHtml:"<a href='https://www.google.com/' target='_blank'>https://www.google.com/</a>",pathD:`M3,1 
+    A3,1 0 0,0 3,2
+    A0.8,1 0 0,1 3,4 
+    A3,1 0 0,0 0,5 
+    V15 
+    A5,1 0 1,0 10,15 
+    V5 
+    A3,1 0 0,0 7,4 
+    A0.8,1 0 0,1 7,2 
+    A3,1 0 0,0 7,1
+    A2.4,1 0 0,0 3,1`}}},c={width:"300",height:"300",xmlns:"http://www.w3.org/2000/svg",version:"1.1",viewBox:"0 0 18 18",style:{border:"1px solid red"}},d=["d"];function h(t,e,w,g,v,_){const i=p("a-safe-html");return s(),r("div",null,[e[0]||(e[0]=o("h1",null,"SVG",-1)),(s(),r("svg",c,[o("path",{d:t.pathD},null,8,d)])),n(o("div",null,null,512),[[i,t.linkHtml]])])}const b=l(m,[["render",h]]);export{b as default};
