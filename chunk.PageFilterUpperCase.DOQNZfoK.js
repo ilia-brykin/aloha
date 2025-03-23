31557300ca26952e6b7bd6626c9e3f089e7d712f
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-03-23 19:55:39 +0000

    deploy: b6f946d8603cb83054515ef9efd77d0dd64d17f3

---
+++ chunk.PageFilterUpperCase.DOQNZfoK.js
@@ -0,0 +1,46 @@
+import{A,a as x}from"./chunk.AlohaExample.DBxxfY2N.js";import{P as h,a as P,b as B}from"./chunk.PageFilterTest.p0_3Y1RO.js";import{P as E}from"./chunk.PageFilterImportCompositionApi.u3fIPoQs.js";import{as as a,_ as s,A as R}from"./bundle.index.DfK_ND9w.js";import{R as i,e as t,U as l,aR as F,Y as r}from"./chunk.vendor.CDkHfsNc.js";import"./chunk.vendor-lodash.BXW4-Hvb.js";import"./chunk.APageTabTitle.POul4upW.js";import"./chunk.AlohaHighlightjs.DI9-5vSP.js";import"./chunk.translations-ar.BW4YNjOf.js";import"./chunk.translations-de.pnZGXXfR.js";import"./chunk.translations-en.CzHrEMzS.js";import"./chunk.translations-es.CVdE8egl.js";import"./chunk.translations-fr.hqb9JGfS.js";import"./chunk.translations-hr.DYwzCGFq.js";import"./chunk.translations-it.C5e5-FF3.js";import"./chunk.translations-ru.eTKud94F.js";function O(){return{codeJs:`filterUpperCase("--foo-bar--");
+// ${a("--foo-bar--")}
+
+filterUpperCase("fooBar");
+// ${a("fooBar")}
+
+filterUpperCase("__foo_bar__");
+// ${a("__foo_bar__")}`}}const T={name:"PageFilterUpperCaseExample",components:{AlohaExample:A},setup(){const{codeJs:e}=O();return{codeJs:e}}};function $(e,o,p,c,_,m){const n=t("aloha-example");return l(),i(n,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const b=s(T,[["render",$]]);function L(){return{argumentsText:[{value:"value",types:["String"],text:"_PAGE_FILTER_UPPER_CASE_ARGUMENTS_VALUE_"}]}}function I(){return{pageTitle:"filterUpperCase"}}const S=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterUpperCase from "../filterUpperCase";
+
+describe("filterUpperCase", () => {
+  it("should convert a string to lowercase", () => {
+    expect(filterUpperCase("--Foo-Bar--")).toBe("--FOO-BAR--");
+    expect(filterUpperCase("fooBar")).toBe("FOOBAR");
+    expect(filterUpperCase("__FOO_BAR__")).toBe("__FOO_BAR__");
+  });
+
+  it("should handle empty strings", () => {
+    expect(filterUpperCase("")).toBe("");
+  });
+
+  it("should handle strings with no alphabetic characters", () => {
+    expect(filterUpperCase("12345")).toBe("12345");
+    expect(filterUpperCase("!@#$%")).toBe("!@#$%");
+  });
+
+  it("should handle mixed case strings", () => {
+    expect(filterUpperCase("FoObAr")).toBe("FOOBAR");
+    expect(filterUpperCase("HELLOworld")).toBe("HELLOWORLD");
+  });
+
+  it("should handle strings with spaces", () => {
+    expect(filterUpperCase("Hello World")).toBe("HELLO WORLD");
+    expect(filterUpperCase(" foo bar ")).toBe(" FOO BAR ");
+  });
+
+  it("should return the same string if already in uppercase", () => {
+    expect(filterUpperCase("ALREADY UPPERCASE")).toBe("ALREADY UPPERCASE");
+  });
+});
+`,v={name:"PageFilterUpperCase",components:{AlohaPage:x,ATranslation:R,PageFilterArguments:B,PageFilterImportCompositionApi:E,PageFilterImportFunction:P,PageFilterTest:h,PageFilterUpperCaseExample:b},setup(){const{pageTitle:e}=I(),{argumentsText:o}=L();return{argumentsText:o,pageTitle:e,test:S}}};function y(e,o,p,c,_,m){const n=t("a-translation"),f=t("page-filter-import-function"),u=t("page-filter-import-composition-api"),g=t("page-filter-arguments"),d=t("page-filter-upper-case-example"),C=t("page-filter-test"),U=t("aloha-page");return l(),i(U,{"page-title":e.pageTitle},{body:F(()=>[r(n,{tag:"p",html:"_PAGE_FILTER_UPPER_CASE_DESCRIPTION_"}),r(f,{"function-name":"filterUpperCase","type-import":"filters"}),r(u,{"function-name":"filterUpperCase"}),r(g,{"arguments-text":e.argumentsText,"function-description":"filterUpperCase(value)"},null,8,["arguments-text"]),r(d),r(C,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const X=s(v,[["render",y]]);export{X as default};
---
+++ chunk.PageTableTree.C58Wqqo5.js
@@ -0,0 +1,101 @@
+import{A as u,a as T}from"./chunk.AlohaExample.DBxxfY2N.js";import{_ as r,g as A}from"./bundle.index.DfK_ND9w.js";import{r as E,R as d,aR as i,e as l,U as c,W as L,Y as b,d as n}from"./chunk.vendor.CDkHfsNc.js";import"./chunk.vendor-lodash.BXW4-Hvb.js";import{A as f}from"./chunk.ATable.oW9cAnbQ.js";import"./chunk.APageTabTitle.POul4upW.js";import"./chunk.AlohaHighlightjs.DI9-5vSP.js";import"./chunk.translations-ar.BW4YNjOf.js";import"./chunk.translations-de.pnZGXXfR.js";import"./chunk.translations-en.CzHrEMzS.js";import"./chunk.translations-es.CVdE8egl.js";import"./chunk.translations-fr.hqb9JGfS.js";import"./chunk.translations-hr.DYwzCGFq.js";import"./chunk.translations-it.C5e5-FF3.js";import"./chunk.translations-ru.eTKud94F.js";function h(){return{codeHtml:`<a-table
+  :columns="columns"
+  :data="data"
+  :is-tree="true"
+  key-id="id"
+  label="_A_TABLE_GROUP_TREE_LABEL_"
+></a-table>`}}function g(){return{codeJs:`import {
+  ref,
+} from "vue";
+
+import { ATable } from "aloha-vue";
+    
+export default {
+  name: "PageTableTreeExample",
+  components: {
+    ATable,
+  },
+  setup() {
+     const columns = [
+      {
+        id: "column1",
+        keyLabel: "id",
+        label: "_A_TABLE_COLUMN_1_",
+      },
+      {
+        id: "column2",
+        keyLabel: "label",
+        label: "_A_TABLE_COLUMN_2_",
+      },
+      {
+        id: "column3",
+        keyLabel: "test",
+        label: "_A_TABLE_COLUMN_3_",
+      },
+    ];
+
+    const data = ref([
+      {
+        id: "1",
+        label: "label 1",
+        test: "test 1",
+      },
+      {
+        id: "2",
+        label: "label 2",
+        test: "test 2",
+        children: [
+          {
+            id: "2_1",
+            label: "label 2.1",
+            test: "test 2.1",
+          },
+          {
+            id: "2_2",
+            label: "label 2.2",
+            test: "test 2.2",
+            children: [
+              {
+                id: "2_2_1",
+                label: "label 2.2.1",
+                test: "test 2.2.1",
+              },
+            ],
+          },
+          {
+            id: "2_3",
+            label: "label 2.3",
+            test: "test 2.3",
+          },
+        ],
+      },
+      {
+        id: "3",
+        label: "label 3",
+        test: "test 3",
+        children: [
+          {
+            id: "3_1",
+            label: "label 3.1",
+            test: "test 3.1",
+          },
+          {
+            id: "3_2",
+            label: "label 3.2",
+            test: "test 3.2",
+          },
+          {
+            id: "3_3",
+            label: "label 3.3",
+            test: "test 3.3",
+          },
+        ],
+      },
+    ]);
+    
+    return {
+      columns,
+      data,
+    };
+  },
+};`}}const B={name:"PageTableTreeExample",components:{AlohaExample:u,ATable:f},setup(){const{codeHtml:e}=h(),{codeJs:t}=g(),a=[{id:"column1",keyLabel:"id",label:"_A_TABLE_COLUMN_1_"},{id:"column2",keyLabel:"label",label:"_A_TABLE_COLUMN_2_"},{id:"column3",keyLabel:"test",label:"_A_TABLE_COLUMN_3_"}],o=E([{id:"1",label:"label 1",test:"test 1"},{id:"2",label:"label 2",test:"test 2",children:[{id:"2_1",label:"label 2.1",test:"test 2.1"},{id:"2_2",label:"label 2.2",test:"test 2.2",children:[{id:"2_2_1",label:"label 2.2.1",test:"test 2.2.1"}]},{id:"2_3",label:"label 2.3",test:"test 2.3"}]},{id:"3",label:"label 3",test:"test 3",children:[{id:"3_1",label:"label 3.1",test:"test 3.1"},{id:"3_2",label:"label 3.2",test:"test 3.2"},{id:"3_3",label:"label 3.3",test:"test 3.3"}]}]);return{codeHtml:e,codeJs:t,columns:a,data:o}}};function P(e,t,a,o,m,p){const s=l("a-table"),_=l("aloha-example");return c(),d(_,{"code-html":e.codeHtml,"code-js":e.codeJs,header:"_A_TABLE_GROUP_TREE_HEADER_",description:"_A_TABLE_GROUP_TREE_DESCRIPTION_",props:"is-tree"},{default:i(()=>[L("div",null,[b(s,{columns:e.columns,data:e.data,"is-tree":!0,"key-id":"id",label:"_A_TABLE_GROUP_TREE_LABEL_"},null,8,["columns","data"])])]),_:1},8,["code-html","code-js"])}const N=r(B,[["render",P]]);function O(){const e=n(()=>A({placeholder:"_A_TABLE_TREE_COMPONENT_NAME_"}));return{pageTitle:n(()=>`ATable ${e.value}`)}}const R={name:"PageTableTree",components:{AlohaPage:T,PageTableTreeExample:N},setup(){const{pageTitle:e}=O();return{pageTitle:e}}};function U(e,t,a,o,m,p){const s=l("page-table-tree-example"),_=l("aloha-page");return c(),d(_,{"page-title":e.pageTitle},{body:i(()=>[b(s)]),_:1},8,["page-title"])}const S=r(R,[["render",U]]);export{S as default};
