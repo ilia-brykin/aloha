5fbb2113f2d1ce21108a59e750cc364b5e8fee88
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-03-24 21:35:33 +0000

    deploy: e6dd9ba6eb73dc4bacbc83998e9586932c3ca9ed

---
+++ chunk.PageFilterFileSize.DkNgeQkW.js
@@ -0,0 +1,45 @@
+import{A as m,a as T}from"./chunk.AlohaExample.CQ1J-0tM.js";import{A as h}from"./chunk.AlohaTableTranslate.ZS9mzsCk.js";import{P as U,a as x,b as J}from"./chunk.PageFilterTest.C_jGJ5-r.js";import{az as t,_ as l,A as L}from"./bundle.index.DylgISd6.js";import{d as f,U as _,e as i,V as c,aS as Z,Z as s}from"./chunk.vendor.YBdBFhvS.js";import"./chunk.vendor-lodash.fC0_u3pc.js";import{P as B}from"./chunk.PageFilterImportCompositionApi.2DbXx5Ea.js";import{f as v}from"./chunk.filterBoolean.test.9cX79x5q.js";import"./chunk.APageTabTitle.Cz1fhALq.js";import"./chunk.AlohaHighlightjs.BcqcN4_H.js";import"./chunk.ATable.B_rdJunK.js";import"./chunk.translations-ar.BW4YNjOf.js";import"./chunk.translations-de.pnZGXXfR.js";import"./chunk.translations-en.CzHrEMzS.js";import"./chunk.translations-es.CVdE8egl.js";import"./chunk.translations-fr.hqb9JGfS.js";import"./chunk.translations-hr.DYwzCGFq.js";import"./chunk.translations-it.C5e5-FF3.js";import"./chunk.translations-ru.eTKud94F.js";function G(){return{codeJs:f(()=>`filterFileSize(1024, { digits: 0 });
+// ${t(1024,{digits:0})}
+
+filterFileSize(10241, { digits: 2 });
+// ${t(10241,{digits:2})}`)}}const R={name:"PageFilterFileSizeDigits",components:{AlohaExample:m},setup(){const{codeJs:e}=G();return{codeJs:e}}};function j(e,o,r,a,p,u){const n=i("aloha-example");return c(),_(n,{"code-js":e.codeJs,header:"digits","is-code-visible-default":!0},null,8,["code-js"])}const y=l(R,[["render",j]]);function N(){return{codeJs:f(()=>`filterFileSize(2000);
+// ${t(2e3)}
+
+filterFileSize(1024);
+// ${t(1024)}`)}}const k={name:"PageFilterFileSizeExample",components:{AlohaExample:m},setup(){const{codeJs:e}=N();return{codeJs:e}}};function C(e,o,r,a,p,u){const n=i("aloha-example");return c(),_(n,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const M=l(k,[["render",C]]);function D(){return{codeJs:f(()=>`filterFileSize(1024, { sourceUnits: "b" });
+// ${t(1024,{sourceUnits:"b"})}
+
+filterFileSize(2, { sourceUnits: "kb" });
+// ${t(2,{sourceUnits:"kb"})}
+
+filterFileSize(1, { sourceUnits: "mb" });
+// ${t(1,{sourceUnits:"mb"})}
+
+filterFileSize(0.1, { sourceUnits: "gb" });
+// ${t(.1,{sourceUnits:"gb"})}
+
+filterFileSize(0.0001, { sourceUnits: "tb" });
+// ${t(1e-4,{sourceUnits:"tb"})}`)}}const V={name:"PageFilterFileSizeSourceUnits",components:{AlohaExample:m},setup(){const{codeJs:e}=D();return{codeJs:e}}};function O(e,o,r,a,p,u){const n=i("aloha-example");return c(),_(n,{"code-js":e.codeJs,header:"sourceUnits","is-code-visible-default":!0},null,8,["code-js"])}const w=l(V,[["render",O]]);function K(){return{codeJs:f(()=>`filterFileSize(1024, { units: "b" });
+// ${t(1024,{units:"b"})}
+
+filterFileSize(1024, { units: "kb" });
+// ${t(1024,{units:"kb"})}
+
+filterFileSize(1048576, { units: "mb" });
+// ${t(1048576,{units:"mb"})}
+
+filterFileSize(1073741824, { units: "gb" });
+// ${t(1073741824,{units:"gb"})}
+
+filterFileSize(1073741824 * 1024, { units: "tb" });
+// ${t(1099511627776,{units:"tb"})}`)}}const Y={name:"PageFilterFileSizeUnits",components:{AlohaExample:m},setup(){const{codeJs:e}=K();return{codeJs:e}}};function q(e,o,r,a,p,u){const n=i("aloha-example");return c(),_(n,{"code-js":e.codeJs,header:"units","is-code-visible-default":!0},null,8,["code-js"])}const H=l(Y,[["render",q]]);function Q(){return{codeJs:f(()=>`filterFileSize(1024, { units: "" });
+// ${t(1024,{units:""})}
+
+filterFileSize(1048576, { units: "" });
+// ${t(1048576,{units:""})}
+
+filterFileSize(1073741824, { units: "" });
+// ${t(1073741824,{units:""})}
+
+filterFileSize(1073741824 * 1024, { units: "" });
+// ${t(1099511627776,{units:""})}`)}}const W={name:"PageFilterFileSizeUnitsEmpty",components:{AlohaExample:m},setup(){const{codeJs:e}=Q();return{codeJs:e}}};function X(e,o,r,a,p,u){const n=i("aloha-example");return c(),_(n,{"code-js":e.codeJs,header:'units=""',"is-code-visible-default":!0},null,8,["code-js"])}const ee=l(W,[["render",X]]);function te(){return{argumentsText:[{value:"value",types:["Number"],text:"_PAGE_FILTER_FILE_SIZE_ARGUMENTS_VALUE_"},{value:'[units="kb"]',types:["String"],text:"_PAGE_FILTER_FILE_SIZE_ARGUMENTS_UNITS_"},{value:'[sourceUnits="b"]',types:["String"],text:"_PAGE_FILTER_FILE_SIZE_ARGUMENTS_SOURCE_UNITS_"},{value:"[digits=2]",types:["Number"],text:"_PAGE_FILTER_FILE_SIZE_ARGUMENTS_DIGITS_"}]}}function ie(){return{pageTitle:"filterFileSize"}}function ne(){return{dataTranslate:["_A_FILE_SIZE_B_","_A_FILE_SIZE_KB_","_A_FILE_SIZE_MB_","_A_FILE_SIZE_GB_","_A_FILE_SIZE_TB_","_A_FILE_SIZE_PB_","_A_FILE_SIZE_EB_","_A_FILE_SIZE_ZB_","_A_FILE_SIZE_YB_"]}}const se={name:"PageFilterFileSize",components:{AlohaPage:T,AlohaTableTranslate:h,ATranslation:L,PageFilterArguments:J,PageFilterFileSizeDigits:y,PageFilterFileSizeExample:M,PageFilterFileSizeSourceUnits:w,PageFilterFileSizeUnits:H,PageFilterFileSizeUnitsEmpty:ee,PageFilterImportCompositionApi:B,PageFilterImportFunction:x,PageFilterTest:U},setup(){const{pageTitle:e}=ie(),{dataTranslate:o}=ne(),{argumentsText:r}=te();return{argumentsText:r,dataTranslate:o,pageTitle:e,test:v}}};function oe(e,o,r,a,p,u){const n=i("a-translation"),d=i("page-filter-import-function"),g=i("page-filter-import-composition-api"),F=i("page-filter-arguments"),S=i("page-filter-file-size-example"),$=i("page-filter-file-size-units"),E=i("page-filter-file-size-units-empty"),I=i("page-filter-file-size-source-units"),z=i("page-filter-file-size-digits"),A=i("aloha-table-translate"),b=i("page-filter-test"),P=i("aloha-page");return c(),_(P,{"page-title":e.pageTitle},{body:Z(()=>[s(n,{tag:"p",html:"_PAGE_FILTER_FILE_SIZE_DESCRIPTION_"}),s(d,{"function-name":"filterFileSize","type-import":"filters"}),s(g,{"function-name":"filterFileSize"}),s(F,{"arguments-text":e.argumentsText,"function-description":'filterFileSize(value, { [units="kb"], [sourceUnits="b"], [digits=2] })'},null,8,["arguments-text"]),s(S),s($),s(E),s(I),s(z),s(A,{data:e.dataTranslate},null,8,["data"]),s(b,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const Pe=l(se,[["render",oe]]);export{Pe as default};
