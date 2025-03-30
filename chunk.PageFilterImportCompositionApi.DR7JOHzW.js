e097ce015bc206c4378f2f63656ddd589b6ce712
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-03-30 10:13:49 +0000

    deploy: 9774924da69fce08e109a36ca777cdccde61d82c

---
+++ chunk.PageFilterImportCompositionApi.DR7JOHzW.js
@@ -0,0 +1,9 @@
+import{A as s}from"./chunk.AlohaExample.iwUIzvUM.js";import{t as n,d as a,U as p,e as c,V as i}from"./chunk.vendor.YBdBFhvS.js";import{_ as m}from"./bundle.index.mmOfAMLP.js";function u(e){const o=n(e,"functionName");return{codeJs:a(()=>`import { AFiltersAPI } from "aloha-vue";
+
+export default {
+  setup() {  
+    const {
+      ${o.value},
+    } = AFiltersAPI();
+  },
+};`)}}const l={name:"PageFilterImportCompositionApi",components:{AlohaExample:s},props:{functionName:{type:String,required:!0}},setup(e){const{codeJs:o}=u(e);return{codeJs:o}}};function d(e,o,t,_,f,A){const r=c("aloha-example");return i(),p(r,{"code-js":e.codeJs,header:"_PAGE_FUNCTIONS_IMPORT_COMPOSITION_API_HEADER_","is-code-visible-default":!0},null,8,["code-js"])}const x=m(l,[["render",d]]);export{x as P};
