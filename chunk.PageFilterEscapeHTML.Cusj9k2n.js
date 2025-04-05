92df6f74cf5333071bbb5e4ef7c3ac772d7d1874
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-04-05 13:10:55 +0000

    deploy: 1b0cb8d050a5360a57e922c85c85c219c55a3eb9

---
+++ chunk.PageFilterEscapeHTML.Cusj9k2n.js
@@ -0,0 +1,50 @@
+import{A as H,a as h}from"./chunk.AlohaExample.BCxAmxzu.js";import{P as T,a as A,b as P}from"./chunk.PageFilterTest.C8Jd98IN.js";import{aq as a,_ as i,A as S}from"./bundle.index.CnTfoJ-B.js";import{U as r,e,V as l,aS as F,Z as n}from"./chunk.vendor.CJgiAfkr.js";import"./chunk.vendor-lodash.UgDJRseT.js";import{P as O}from"./chunk.PageFilterImportCompositionApi.CDwsm8b6.js";import"./chunk.APageTabTitle.VVPl8iQY.js";import"./chunk.AlohaHighlightjs.CS2rrB7v.js";import"./chunk.translations-ar.C215GjCi.js";import"./chunk.translations-de.2OTgW6Cj.js";import"./chunk.translations-en.BDnCg_Z9.js";import"./chunk.translations-es.BP9eJkFz.js";import"./chunk.translations-fr.BOe55yrn.js";import"./chunk.translations-hr.B3s_PrJE.js";import"./chunk.translations-it.Cc3k7wyX.js";import"./chunk.translations-ru.D-swuOFG.js";function L(){return{codeJs:`filterEscapeHtml("<script>alert('XSS Attack')<\/script>");
+// ${a('<script>alert("XSS Attack")<\/script>')}
+
+filterEscapeHtml("Hello, World!");
+// ${a("Hello, World!")}`}}const $={name:"PageFilterEscapeHTMLExample",components:{AlohaExample:H},setup(){const{codeJs:t}=L();return{codeJs:t}}};function I(t,o,s,c,u,m){const p=e("aloha-example");return l(),r(p,{"code-js":t.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const M=i($,[["render",I]]);function B(){return{argumentsText:[{value:"value",types:["String"],text:"_PAGE_FILTER_ESCAPE_HTML_ARGUMENTS_VALUE_"}]}}function C(){return{pageTitle:"filterEscapeHtml"}}const k=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterEscapeHtml from "../filterEscapeHtml";
+
+describe("filterEscapeHtml", () => {
+  it("should escape HTML special characters", () => {
+    const input = "<script>alert(\\"XSS Attack\\")<\/script>";
+    const expectedOutput = "&lt;script&gt;alert(\\"XSS Attack\\")&lt;/script&gt;";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should return the same string if no special characters", () => {
+    const input = "Hello, World!";
+    const expectedOutput = "Hello, World!";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should return an empty string if input is empty", () => {
+    const input = "";
+    const expectedOutput = "";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should escape only special HTML characters and leave the rest", () => {
+    const input = "Hello <strong>world</strong>";
+    const expectedOutput = "Hello &lt;strong&gt;world&lt;/strong&gt;";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should handle special characters like &, \\", '", () => {
+    const input = "Fish & Chips \\"Delicious\\"";
+    const expectedOutput = "Fish &amp; Chips \\"Delicious\\"";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should escape HTML list tags", () => {
+    const input = "<ul><li>test</li></ul>";
+    const expectedOutput = "&lt;ul&gt;&lt;li&gt;test&lt;/li&gt;&lt;/ul&gt;";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+});
+`,y={name:"PageFilterEscapeHTML",components:{AlohaPage:h,ATranslation:S,PageFilterArguments:P,PageFilterEscapeHTMLExample:M,PageFilterImportCompositionApi:O,PageFilterImportFunction:A,PageFilterTest:T},setup(){const{pageTitle:t}=C(),{argumentsText:o}=B();return{argumentsText:o,pageTitle:t,test:k}}};function b(t,o,s,c,u,m){const p=e("a-translation"),_=e("page-filter-import-function"),g=e("page-filter-import-composition-api"),f=e("page-filter-arguments"),d=e("page-filter-escape-h-t-m-l-example"),E=e("page-filter-test"),x=e("aloha-page");return l(),r(x,{"page-title":t.pageTitle},{body:F(()=>[n(p,{tag:"p",html:"_PAGE_FILTER_ESCAPE_HTML_DESCRIPTION_"}),n(_,{"function-name":"filterEscapeHtml","type-import":"filters"}),n(g,{"function-name":"filterEscapeHtml"}),n(f,{"arguments-text":t.argumentsText,"function-description":"filterEscapeHtml(value)"},null,8,["arguments-text"]),n(d),n(E,{test:t.test},null,8,["test"])]),_:1},8,["page-title"])}const Q=i(y,[["render",b]]);export{Q as default};
---
+++ chunk.PageUtilsIsArrayOfStrings.W6SLNcKy.js
@@ -0,0 +1,63 @@
+import{A as h,a as S}from"./chunk.AlohaExample.BCxAmxzu.js";import{P as d,a as O,b as x}from"./chunk.PageFilterTest.C8Jd98IN.js";import{aF as e,_ as i,A as $}from"./bundle.index.CnTfoJ-B.js";import{U as l,e as r,V as p,aS as I,Z as n}from"./chunk.vendor.CJgiAfkr.js";import"./chunk.vendor-lodash.UgDJRseT.js";import"./chunk.APageTabTitle.VVPl8iQY.js";import"./chunk.AlohaHighlightjs.CS2rrB7v.js";import"./chunk.translations-ar.C215GjCi.js";import"./chunk.translations-de.2OTgW6Cj.js";import"./chunk.translations-en.BDnCg_Z9.js";import"./chunk.translations-es.BP9eJkFz.js";import"./chunk.translations-fr.BOe55yrn.js";import"./chunk.translations-hr.B3s_PrJE.js";import"./chunk.translations-it.Cc3k7wyX.js";import"./chunk.translations-ru.D-swuOFG.js";function T(){return{codeJs:`isArrayOfStrings(["hello", "Aloha"]);
+// ${e(["hello","Aloha"])}
+
+isArrayOfStrings("hello Aloha");
+// ${e("hello Aloha")}
+
+isArrayOfStrings(["Aloha", 1]);
+// ${e(["Aloha",1])}
+isArrayOfStrings(["Aloha", false]);
+// ${e(["Aloha",!1])}
+isArrayOfStrings(["Aloha", undefined]);
+// ${e(["Aloha",void 0])}
+isArrayOfStrings(["Aloha", null]);
+// ${e(["Aloha",null])}
+
+isArrayOfStrings([]);
+// ${e([])}
+isArrayOfStrings("Aloha");
+// ${e("Aloha")}
+isArrayOfStrings(1);
+// ${e(1)}
+isArrayOfStrings(undefined);
+// ${e(void 0)}
+isArrayOfStrings(null);
+// ${e(null)}
+isArrayOfStrings({});
+// ${e({})}`}}const P={name:"PageUtilsIsArrayOfStringsExample",components:{AlohaExample:h},setup(){const{codeJs:t}=T();return{codeJs:t}}};function B(t,s,o,f,u,c){const a=r("aloha-example");return p(),l(a,{"code-js":t.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const U=i(P,[["render",B]]);function E(){return{argumentsText:[{value:"value",types:["Array"],text:"_PAGE_UTILS_IS_ARRAY_OF_STRINGS_ARGUMENTS_VALUE_"}]}}function F(){return{pageTitle:"isArrayOfStrings"}}const R=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import {
+  isArrayOfStrings,
+} from "../utils";
+
+describe("isArrayOfStrings function", () => {
+  it("should return true when input is an array of strings", () => {
+    const array = ["hello", "world"];
+    const result = isArrayOfStrings(array);
+
+    expect(result).toBe(true);
+  });
+
+  it("should return false when input is not an array", () => {
+    const notArray = "hello world";
+    const result = isArrayOfStrings(notArray);
+
+    expect(result).toBe(false);
+  });
+
+  it("should return false if provided with a empty array", () => {
+    expect(isArrayOfStrings([])).toBe(false);
+  });
+
+  it("should return false when input is an array but not all elements are strings", () => {
+    expect(isArrayOfStrings(["hello", 1234])).toBe(false);
+    expect(isArrayOfStrings(["hello", false])).toBe(false);
+    expect(isArrayOfStrings(["hello", undefined])).toBe(false);
+    expect(isArrayOfStrings(["hello", null])).toBe(false);
+  });
+});
+`,b={name:"PageUtilsIsArrayOfStrings",components:{AlohaPage:S,ATranslation:$,PageFilterArguments:x,PageFilterImportFunction:O,PageFilterTest:d,PageUtilsIsArrayOfStringsExample:U},setup(){const{pageTitle:t}=F(),{argumentsText:s}=E();return{argumentsText:s,pageTitle:t,test:R}}};function w(t,s,o,f,u,c){const a=r("a-translation"),g=r("page-filter-import-function"),A=r("page-filter-arguments"),m=r("page-utils-is-array-of-strings-example"),_=r("page-filter-test"),y=r("aloha-page");return p(),l(y,{"page-title":t.pageTitle},{body:I(()=>[n(a,{tag:"p",html:"_PAGE_UTILS_IS_ARRAY_OF_STRINGS_DESCRIPTION_"}),n(g,{"function-name":"isArrayOfStrings","type-import":"utils"}),n(A,{"arguments-text":t.argumentsText,"function-description":"isArrayOfStrings(value)"},null,8,["arguments-text"]),n(m),n(_,{test:t.test},null,8,["test"])]),_:1},8,["page-title"])}const H=i(b,[["render",w]]);export{H as default};
