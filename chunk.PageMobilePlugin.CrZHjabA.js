b5d52ffb7433f53a7e15254d1780c04b73315794
NKDante <NKDante@users.noreply.github.com>
2025-04-11 08:50:09 +0000

    deploy: 604b05d5eedc62f43309b4956fd5573ec6b4a053

---
+++ chunk.PageMobilePlugin.CrZHjabA.js
@@ -0,0 +1,36 @@
+import{A as l}from"./chunk.AlohaHighlightjs.CS2rrB7v.js";import{A as r,_}from"./bundle.index.BgnoJFSz.js";import{W as m,Z as e,Y as s,e as i,V as c}from"./chunk.vendor.CJgiAfkr.js";import"./chunk.vendor-lodash.UgDJRseT.js";import{A as u}from"./chunk.APageTabTitle.DnquuzTo.js";import"./chunk.translations-ar.C215GjCi.js";import"./chunk.translations-de.2OTgW6Cj.js";import"./chunk.translations-en.BDnCg_Z9.js";import"./chunk.translations-es.BP9eJkFz.js";import"./chunk.translations-fr.BOe55yrn.js";import"./chunk.translations-hr.B3s_PrJE.js";import"./chunk.translations-it.Cc3k7wyX.js";import"./chunk.translations-ru.D-swuOFG.js";function d(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  AMobilePlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(AMobilePlugin, {
+  breakpoint: 1023,
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <p v-if="isMobileWidth">You are using a mobile device.</p>
+    <p v-else>You are using a desktop device.</p>
+  </div>
+</template>
+
+<script>
+import {
+  AMobileAPI,
+} from "aloha-vue";
+
+export default {
+  setup() {
+    const {
+      isMobileWidth,
+    } = AMobileAPI;
+    
+    return {
+      isMobileWidth,
+    };
+  },
+};
+<\/script>`}}const g={name:"PageMobilePlugin",components:{AlohaHighlightjs:l,APageTabTitle:u,ATranslation:r},setup(){const{connectionCode:t,useCode:o}=d();return{connectionCode:t,useCode:o}}};function A(t,o,P,h,f,E){const p=i("a-page-tab-title"),a=i("a-translation"),n=i("aloha-highlightjs");return c(),m("div",null,[e(p,{title:"_PAGE_PLUGIN_MOBILE_PAGE_TITLE_"}),e(a,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_MOBILE_H1_"}),e(a,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_INTRODUCTION_"}),o[0]||(o[0]=s("hr",{class:"a_my_5"},null,-1)),e(a,{tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_CONNECTION_"}),e(n,{code:t.connectionCode,language:"javascript"},null,8,["code"]),e(a,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_CONNECTION_NOTES_"}),o[1]||(o[1]=s("hr",{class:"a_my_5"},null,-1)),e(a,{tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_EXAMPLES_"}),e(n,{code:t.useCode,language:"html"},null,8,["code"])])}const k=_(g,[["render",A]]);export{k as default};
