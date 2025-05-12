26651ae1aa1f11878d6b0d45034e6274f594833f
NKDante <NKDante@users.noreply.github.com>
2025-05-12 13:35:59 +0000

    deploy: 3e6a9278178686d41168ebb8916763719a235461

---
+++ chunk.PageFilterCapitalize.KA644xi-.js
@@ -0,0 +1,46 @@
+import{A,a as h}from"./chunk.AlohaExample.BG9TUkST.js";import{P as x,a as P,b as F}from"./chunk.PageFilterTest.mu7AcTJW.js";import{av as n,_ as p,A as T}from"./bundle.index.DWAZ6fTH.js";import{U as l,e as t,V as s,aS as B,Z as a}from"./chunk.vendor.w-kCrpQl.js";import"./chunk.vendor-lodash.GmlDDh1v.js";import{P as b}from"./chunk.PageFilterImportCompositionApi.D8-Apz3I.js";import"./chunk.APageTabTitle.jzvq5ZEN.js";import"./chunk.AlohaHighlightjs.Cjego80K.js";import"./chunk.translations-ar.7kyFlj_7.js";import"./chunk.translations-de.BMzqk4tK.js";import"./chunk.translations-en.CYLIQ7pD.js";import"./chunk.translations-es.D962VZ2D.js";import"./chunk.translations-fr.CcjuPFMN.js";import"./chunk.translations-hr.D1P_Fjd9.js";import"./chunk.translations-it.BsI54FWd.js";import"./chunk.translations-ru.hctQRJz1.js";function I(){return{codeJs:`filterCapitalize("foobar");
+// ${n("foobar")}
+
+filterCapitalize("FOOBAR");
+// ${n("FOOBAR")}
+
+filterCapitalize("fOoBaR");
+// ${n("fOoBaR")}`}}const E={name:"PageFilterCapitalizeExample",components:{AlohaExample:A},setup(){const{codeJs:e}=I();return{codeJs:e}}};function $(e,i,r,c,m,f){const o=t("aloha-example");return s(),l(o,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const O=p(E,[["render",$]]);function R(){return{argumentsText:[{value:"value",types:["String"],text:"_PAGE_FILTER_CAPITALIZE_ARGUMENTS_VALUE_"}]}}function v(){return{pageTitle:"filterCapitalize"}}const y=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterCapitalize from "../filterCapitalize";
+
+describe("filterCapitalize", () => {
+  it("should capitalize a lowercase string", () => {
+    expect(filterCapitalize("foobar")).toBe("Foobar");
+  });
+
+  it("should capitalize an uppercase string", () => {
+    expect(filterCapitalize("FOOBAR")).toBe("Foobar");
+  });
+
+  it("should capitalize a mixed case string", () => {
+    expect(filterCapitalize("fOoBaR")).toBe("Foobar");
+  });
+
+  it("should handle an empty string", () => {
+    expect(filterCapitalize("")).toBe("");
+  });
+
+  it("should handle a string with one character", () => {
+    expect(filterCapitalize("a")).toBe("A");
+    expect(filterCapitalize("A")).toBe("A");
+  });
+
+  it("should handle strings with spaces", () => {
+    expect(filterCapitalize(" foo bar ")).toBe(" foo bar ");
+  });
+
+  it("should not change already capitalized strings", () => {
+    expect(filterCapitalize("Foobar")).toBe("Foobar");
+  });
+});
+`,S={name:"PageFilterCapitalize",components:{AlohaPage:h,ATranslation:T,PageFilterArguments:F,PageFilterCapitalizeExample:O,PageFilterImportCompositionApi:b,PageFilterImportFunction:P,PageFilterTest:x},setup(){const{pageTitle:e}=v(),{argumentsText:i}=R();return{argumentsText:i,pageTitle:e,test:y}}};function J(e,i,r,c,m,f){const o=t("a-translation"),_=t("page-filter-import-function"),u=t("page-filter-import-composition-api"),g=t("page-filter-arguments"),d=t("page-filter-capitalize-example"),C=t("page-filter-test"),z=t("aloha-page");return s(),l(z,{"page-title":e.pageTitle},{body:B(()=>[a(o,{tag:"p",html:"_PAGE_FILTER_CAPITALIZE_DESCRIPTION_"}),a(_,{"function-name":"filterCapitalize","type-import":"filters"}),a(u,{"function-name":"filterCapitalize"}),a(g,{"arguments-text":e.argumentsText,"function-description":"filterCapitalize(value)"},null,8,["arguments-text"]),a(d),a(C,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const X=p(S,[["render",J]]);export{X as default};
