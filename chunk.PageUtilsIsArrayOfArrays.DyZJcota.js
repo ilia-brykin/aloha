55884780218046b67fd5083df717aefcdda90857
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-05-16 20:06:21 +0000

    deploy: 6d898dab5fc7e4bbb714b68e3dae1c6ff8c4f705

---
+++ chunk.PageUtilsIsArrayOfArrays.DyZJcota.js
@@ -0,0 +1,67 @@
+import{A as g,a as O}from"./chunk.AlohaExample.DBbDhVgH.js";import{P as h,a as x,b as $}from"./chunk.PageFilterTest.6rWbCCKS.js";import{aH as t,_ as i,A as T}from"./bundle.index.B8rwby-a.js";import{U as l,e as a,V as p,aS as P,Z as e}from"./chunk.vendor.w-kCrpQl.js";import"./chunk.vendor-lodash.GmlDDh1v.js";import"./chunk.APageTabTitle.Drw3yveG.js";import"./chunk.AlohaHighlightjs.Cjego80K.js";import"./chunk.translations-ar.C8xAF5pd.js";import"./chunk.translations-de.eUvDf8xn.js";import"./chunk.translations-en.DEpj9U8S.js";import"./chunk.translations-es.BUvs_K_5.js";import"./chunk.translations-fr.Qi6uRa8b.js";import"./chunk.translations-hr.hUcxziT-.js";import"./chunk.translations-it.DFfE6hGj.js";import"./chunk.translations-ru.CS-1tvJT.js";function I(){return{codeJs:`isArrayOfArrays([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+// ${t([[1,2,3],[4,5,6],[7,8,9]])}
+isArrayOfArrays([[], [], []]);
+// ${t([[],[],[]])}
+
+isArrayOfArrays([1, 2, 3]);
+// ${t([1,2,3])}
+
+isArrayOfArrays([]);
+// ${t([])}
+isArrayOfArrays("Aloha");
+// ${t("Aloha")}
+isArrayOfArrays(1);
+// ${t(1)}
+isArrayOfArrays(undefined);
+// ${t(void 0)}
+isArrayOfArrays(null);
+// ${t(null)}
+isArrayOfArrays({});
+// ${t({})}
+
+isArrayOfArrays([[1, 2, 3], "This is not an array", [7, 8, 9]]);
+// ${t([[1,2,3],"This is not an array",[7,8,9]])}`}}const B={name:"PageUtilsIsArrayOfArraysExample",components:{AlohaExample:g},setup(){const{codeJs:r}=I();return{codeJs:r}}};function S(r,s,o,A,f,y){const n=a("aloha-example");return p(),l(n,{"code-js":r.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const v=i(B,[["render",S]]);function R(){return{argumentsText:[{value:"value",types:["Array"],text:"_PAGE_UTILS_IS_ARRAY_OF_ARRAYS_ARGUMENTS_VALUE_"}]}}function U(){return{pageTitle:"isArrayOfArrays"}}const E=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import {
+  isArrayOfArrays,
+} from "../utils";
+
+describe("isArrayOfArrays", () => {
+  it("should return true if provided with an array of arrays", () => {
+    const testData = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    const result = isArrayOfArrays(testData);
+
+    expect(result).toBe(true);
+  });
+
+  it("should return false if provided with an array of non-arrays", () => {
+    const testData = [1, 2, 3, 4, 5];
+    const result = isArrayOfArrays(testData);
+
+    expect(result).toBe(false);
+  });
+
+  it("should return false if provided with a non-array", () => {
+    expect(isArrayOfArrays("Aloha")).toBe(false);
+    expect(isArrayOfArrays(123)).toBe(false);
+    expect(isArrayOfArrays(undefined)).toBe(false);
+    expect(isArrayOfArrays(null)).toBe(false);
+    expect(isArrayOfArrays({})).toBe(false);
+  });
+
+  it("should return false if provided with a empty array", () => {
+    expect(isArrayOfArrays([])).toBe(false);
+  });
+
+  it("should return false if provided with an array that contains non-array elements", () => {
+    const testData = [[1, 2, 3], "This is not an array", [7, 8, 9]];
+    const result = isArrayOfArrays(testData);
+
+    expect(result).toBe(false);
+  });
+});
+`,D={name:"PageUtilsIsArrayOfArrays",components:{AlohaPage:O,ATranslation:T,PageFilterArguments:$,PageFilterImportFunction:x,PageFilterTest:h,PageUtilsIsArrayOfArraysExample:v},setup(){const{pageTitle:r}=U(),{argumentsText:s}=R();return{argumentsText:s,pageTitle:r,test:E}}};function F(r,s,o,A,f,y){const n=a("a-translation"),c=a("page-filter-import-function"),u=a("page-filter-arguments"),m=a("page-utils-is-array-of-arrays-example"),_=a("page-filter-test"),d=a("aloha-page");return p(),l(d,{"page-title":r.pageTitle},{body:P(()=>[e(n,{tag:"p",html:"_PAGE_UTILS_IS_ARRAY_OF_ARRAYS_DESCRIPTION_"}),e(c,{"function-name":"isArrayOfArrays","type-import":"utils"}),e(u,{"arguments-text":r.argumentsText,"function-description":"isArrayOfArrays(value)"},null,8,["arguments-text"]),e(m),e(_,{test:r.test},null,8,["test"])]),_:1},8,["page-title"])}const z=i(D,[["render",F]]);export{z as default};
