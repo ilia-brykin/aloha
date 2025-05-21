d4ce5ade5d26939efdccca1430d432e05d155e72
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-05-21 07:08:43 +0000

    deploy: 2b18e53657a4112756c9038c0e7199858ce297c0

---
+++ chunk.PageCloakPlugin.B1QvUWTF.js
@@ -0,0 +1,36 @@
+import{A as i}from"./chunk.AlohaHighlightjs.Cjego80K.js";import{A as r,_}from"./bundle.index.CWJbIJ4K.js";import{W as m,Z as o,Y as s,e as n,V as c}from"./chunk.vendor.w-kCrpQl.js";import"./chunk.vendor-lodash.GmlDDh1v.js";import{A as u}from"./chunk.APageTabTitle.COHJDAnG.js";import"./chunk.translations-ar.C8xAF5pd.js";import"./chunk.translations-de.eUvDf8xn.js";import"./chunk.translations-en.DEpj9U8S.js";import"./chunk.translations-es.BUvs_K_5.js";import"./chunk.translations-fr.Qi6uRa8b.js";import"./chunk.translations-hr.hUcxziT-.js";import"./chunk.translations-it.DFfE6hGj.js";import"./chunk.translations-ru.CS-1tvJT.js";function A(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ACloakPlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(ACloakPlugin, {
+  propsDefault: {
+    align: "left",
+    size: 8,
+    tag: "section",
+    text: "Loading, please wait...",
+    textAlign: "center",
+  },
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <a-cloak></a-cloak>
+  </div>
+</template>
+
+<script>
+import { 
+  ACloak,
+} from "aloha-vue";
+
+export default {
+  name: "CloakExample",
+  components: {
+    ACloak,
+  },
+};
+<\/script>`}}const d={name:"PageCloakPlugin",components:{AlohaHighlightjs:i,APageTabTitle:u,ATranslation:r},setup(){const{connectionCode:a,useCode:t}=A();return{connectionCode:a,useCode:t}}};function g(a,t,C,P,f,h){const p=n("a-page-tab-title"),e=n("a-translation"),l=n("aloha-highlightjs");return c(),m("div",null,[o(p,{title:"_PAGE_PLUGIN_CLOAK_PAGE_TITLE_"}),o(e,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_CLOAK_H1_"}),o(e,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_INTRODUCTION_"}),t[0]||(t[0]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_CONNECTION_"}),o(l,{code:a.connectionCode,language:"javascript"},null,8,["code"]),o(e,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_CONNECTION_NOTES_"}),t[1]||(t[1]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_EXAMPLES_"}),o(l,{code:a.useCode,language:"html"},null,8,["code"])])}const j=_(d,[["render",g]]);export{j as default};
