949b647024e8b1e7820c9ea309be9953e41098a9
Ilia <brykin.ilia@gmail.com>
2025-05-30 19:31:16 +0200

    Add PageCarouselBasic example with ACarousel component
    Implemented a detailed example showcasing the ACarousel component, including HTML structure and dynamic data setup. Enhanced usability with internationalized alt text and aria labels.

--- docs/src/views/Components/PageCarousel/PageCarouselBasic/compositionAPI/JsAPI.js
+++ docs/src/views/Components/PageCarousel/PageCarouselBasic/compositionAPI/JsAPI.js
@@ -1,12 +1,51 @@
 export default function JsAPI() {
   const codeJs = `import { 
-  ACloak,
+  ACarousel,
+  getTranslatedText,
 } from "aloha-vue";
     
 export default {
-  name: "PageCloakBasic",
+  name: "PageCarouselBasic",
   components: {
-    ACloak,
+    ACarousel,
+  },
+  setup() {
+    const data = computed(() => {
+      return [
+        {
+          id: 1,
+          src: "./assets/Basketball_ball_red_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_BASKETBALL_BALL_",
+          }),
+        },
+        {
+          id: 2,
+          src: "./assets/Soccer_ball_red_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_SOCCER_BALL_",
+          }),
+        },
+        {
+          id: 3,
+          src: "./assets/Tennis_ball_purple_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_TENNIS_BALL_",
+          }),
+        },
+        {
+          id: 4,
+          src: "./assets/Volleyball_ball_red_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_VOLLEY_BALL_",
+          }),
+        },
+      ];
+    });
+    
+    return {
+      data,
+    };
   },
 };`;
 
