50c396d2e271bd656d23d1c931b1e8bd78482e18
Ilia <brykin.ilia@gmail.com>
2025-05-30 19:37:53 +0200

    Add PageCarouselArrowsTrigger example with dynamic ACarousel configurations
    Implemented a comprehensive example of the ACarousel component using various `arrows-trigger` configurations (hover, focus, hover-focus, always). Included dynamic data setup, internationalized alt text, and aria labels to improve accessibility and usability.

--- docs/src/views/Components/PageCarousel/PageCarouselArrowsTrigger/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageCarousel/PageCarouselArrowsTrigger/compositionAPI/HtmlAPI.js
@@ -1,5 +1,89 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-cloak></a-cloak>`;
+  const codeHtml = `<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_12_touch a_column_4">
+    <a-translation
+      tag="h3"
+      text="_A_CAROUSEL_COMPONENT_ARROWS_TRIGGER_HOVER_"
+    ></a-translation>
+    <a-carousel
+      :data="data"
+      aria-label="Carousel with balls"
+      arrows-trigger="hover"
+      key-id="id"
+    >
+      <template v-slot:item="{ item }">
+        <img
+          class="a_height_auto a_width_100"
+          :src="item.src"
+          :alt="item.alt"
+        />
+      </template>
+    </a-carousel>
+  </div>
+
+  <div class="a_column a_column_12_touch a_column_4">
+    <a-translation
+      tag="h3"
+      text="_A_CAROUSEL_COMPONENT_ARROWS_TRIGGER_FOCUS_"
+    ></a-translation>
+    <a-carousel
+      :data="data"
+      aria-label="Carousel with balls"
+      arrows-trigger="focus"
+      key-id="id"
+    >
+      <template v-slot:item="{ item }">
+        <img
+          class="a_height_auto a_width_100"
+          :src="item.src"
+          :alt="item.alt"
+        />
+      </template>
+    </a-carousel>
+  </div>
+
+  <div class="a_column a_column_12_touch a_column_4">
+    <a-translation
+      tag="h3"
+      text="_A_CAROUSEL_COMPONENT_ARROWS_TRIGGER_HOVER_FOCUS_"
+    ></a-translation>
+    <a-carousel
+      :arrows-trigger="['hover', 'focus']"
+      :data="data"
+      aria-label="Carousel with balls"
+      key-id="id"
+    >
+      <template v-slot:item="{ item }">
+        <img
+          class="a_height_auto a_width_100"
+          :src="item.src"
+          :alt="item.alt"
+        />
+      </template>
+    </a-carousel>
+  </div>
+
+  <div class="a_column a_column_12_touch a_column_4">
+    <a-translation
+      tag="h3"
+      text="_A_CAROUSEL_COMPONENT_ARROWS_TRIGGER_ALWAYS_"
+    ></a-translation>
+    <a-carousel
+      :data="data"
+      aria-label="Carousel with balls"
+      arrows-trigger="always"
+      key-id="id"
+    >
+      <template v-slot:item="{ item }">
+        <img
+          class="a_height_auto a_width_100"
+          :src="item.src"
+          :alt="item.alt"
+        />
+      </template>
+    </a-carousel>
+  </div>
+</div>`;
 
   return {
     codeHtml,
--- docs/src/views/Components/PageCarousel/PageCarouselArrowsTrigger/compositionAPI/JsAPI.js
+++ docs/src/views/Components/PageCarousel/PageCarouselArrowsTrigger/compositionAPI/JsAPI.js
@@ -1,12 +1,55 @@
 export default function JsAPI() {
-  const codeJs = `import { 
-  ACloak,
+  const codeJs = `import {
+  computed,
+} from "vue";
+
+import {
+  ACarousel,
+  getTranslatedText,
 } from "aloha-vue";
-    
+
 export default {
   name: "PageCarouselArrowsTrigger",
   components: {
-    ACloak,
+    ACarousel,
+  },
+  setup() {
+    const data = computed(() => {
+      return [
+        {
+          id: 1,
+          src: "./assets/Basketball_ball_red_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_BASKETBALL_BALL_",
+          }),
+        },
+        {
+          id: 2,
+          src: "./assets/Soccer_ball_red_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_SOCCER_BALL_",
+          }),
+        },
+        {
+          id: 3,
+          src: "./assets/Tennis_ball_purple_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_TENNIS_BALL_",
+          }),
+        },
+        {
+          id: 4,
+          src: "./assets/Volleyball_ball_red_background.png",
+          alt: getTranslatedText({
+            placeholder: "_A_CAROUSEL_COMPONENT_ALT_VOLLEY_BALL_",
+          }),
+        },
+      ];
+    });
+
+    return {
+      data,
+    };
   },
 };`;
 
