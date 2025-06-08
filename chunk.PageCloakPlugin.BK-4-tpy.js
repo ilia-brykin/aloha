d1f3e7141fddd65ae0a0818e2c5fc0879f56072d
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-06-08 21:00:07 +0000

    deploy: 6e3dd8eac21350565e8d7d31a39d3af16c44b06d

---
+++ chunk.PageCloakPlugin.BK-4-tpy.js
@@ -0,0 +1,36 @@
+import{A as i}from"./chunk.AlohaHighlightjs.CW1W2LCP.js";import{A as r,_}from"./bundle.index.CZxfLHGa.js";import{W as m,Z as o,Y as s,e as n,V as c}from"./chunk.vendor.COYgx38y.js";import"./chunk.vendor-lodash.DdGPhwvj.js";import{A as u}from"./chunk.APageTabTitle.TSVSk0Zw.js";import"./chunk.translations-ar.CRFwteN1.js";import"./chunk.translations-de.CT_a-h9Y.js";import"./chunk.translations-en.Cc-CgU0K.js";import"./chunk.translations-es.CsqDVO6e.js";import"./chunk.translations-fr.C5Cpzf1_.js";import"./chunk.translations-hr.BU-lPWVN.js";import"./chunk.translations-it.BGVGkPpO.js";import"./chunk.translations-ru.BuJh6s8h.js";function A(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ACloakPlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(ACloakPlugin, {
+  propsDefault: {
+    align: "left",
+    size: 8,
+    tag: "section",
+    text: "Loading, please wait...",
+    textAlign: "center",
+  },
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <a-cloak></a-cloak>
+  </div>
+</template>
+
+<script>
+import { 
+  ACloak,
+} from "aloha-vue";
+
+export default {
+  name: "CloakExample",
+  components: {
+    ACloak,
+  },
+};
+<\/script>`}}const d={name:"PageCloakPlugin",components:{AlohaHighlightjs:i,APageTabTitle:u,ATranslation:r},setup(){const{connectionCode:a,useCode:t}=A();return{connectionCode:a,useCode:t}}};function g(a,t,C,P,f,h){const p=n("a-page-tab-title"),e=n("a-translation"),l=n("aloha-highlightjs");return c(),m("div",null,[o(p,{title:"_PAGE_PLUGIN_CLOAK_PAGE_TITLE_"}),o(e,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_CLOAK_H1_"}),o(e,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_INTRODUCTION_"}),t[0]||(t[0]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_CONNECTION_"}),o(l,{code:a.connectionCode,language:"javascript"},null,8,["code"]),o(e,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_CONNECTION_NOTES_"}),t[1]||(t[1]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_CLOAK_EXAMPLES_"}),o(l,{code:a.useCode,language:"html"},null,8,["code"])])}const j=_(d,[["render",g]]);export{j as default};
---
+++ chunk.PageFilterDefaultForEmpty.Bkq5cFne.js
@@ -0,0 +1,39 @@
+import{A as c,a as h}from"./chunk.AlohaExample.R7LhbMEI.js";import{P as A,a as P,b as x}from"./chunk.PageFilterTest.DuTQZ__S.js";import{ar as n,_ as p,A as T}from"./bundle.index.CZxfLHGa.js";import{U as i,e as t,V as m,aS as $,Z as o}from"./chunk.vendor.COYgx38y.js";import"./chunk.vendor-lodash.DdGPhwvj.js";import{P as V}from"./chunk.PageFilterImportCompositionApi.TJSahd1t.js";import"./chunk.APageTabTitle.TSVSk0Zw.js";import"./chunk.AlohaHighlightjs.CW1W2LCP.js";import"./chunk.translations-ar.CRFwteN1.js";import"./chunk.translations-de.CT_a-h9Y.js";import"./chunk.translations-en.Cc-CgU0K.js";import"./chunk.translations-es.CsqDVO6e.js";import"./chunk.translations-fr.C5Cpzf1_.js";import"./chunk.translations-hr.BU-lPWVN.js";import"./chunk.translations-it.BGVGkPpO.js";import"./chunk.translations-ru.BuJh6s8h.js";function v(){return{codeJs:`filterDefaultForEmpty(undefined, { emptyValue: "---" });
+// ${n(void 0,{emptyValue:"---"})}
+
+filterDefaultForEmpty(undefined, { emptyValue: "false" });
+// ${n(void 0,{emptyValue:"false"})}`}}const I={name:"PageFilterDefaultForEmptyEmptyValue",components:{AlohaExample:c},setup(){const{codeJs:e}=v();return{codeJs:e}}};function J(e,r,l,u,s,f){const a=t("aloha-example");return m(),i(a,{"code-js":e.codeJs,header:"emptyValue","is-code-visible-default":!0},null,8,["code-js"])}const B=p(I,[["render",J]]);function R(){return{codeJs:`filterDefaultForEmpty(undefined);
+// ${n(void 0)}
+
+filterDefaultForEmpty(null);
+// ${n(null)}
+
+filterDefaultForEmpty("");
+// ${n("")}
+
+filterDefaultForEmpty("aloha");
+// ${n("aloha")}`}}const U={name:"PageFilterDefaultForEmptyExample",components:{AlohaExample:c},setup(){const{codeJs:e}=R();return{codeJs:e}}};function L(e,r,l,u,s,f){const a=t("aloha-example");return m(),i(a,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const b=p(U,[["render",L]]);function S(){return{argumentsText:[{value:"value",types:["Any"],text:"_PAGE_FILTER_DEFAULT_FOR_EMPTY_ARGUMENTS_VALUE_"},{value:'[emptyValue="-"]',types:["String"],text:"_PAGE_FILTER_DEFAULT_FOR_EMPTY_ARGUMENTS_EMPTY_VALUE_"}]}}function G(){return{pageTitle:"filterDefaultForEmpty"}}const M=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterDefaultForEmpty from "../filterDefaultForEmpty";
+
+describe("filterDefaultForEmpty Function", () => {
+  it("should return 'emptyValue', when the input is null", () => {
+    expect(filterDefaultForEmpty(null)).toBe("-");
+    expect(filterDefaultForEmpty(null, { emptyValue: "default" })).toBe("default");
+  });
+
+  it("should return 'emptyValue', when the input is an empty string", () => {
+    expect(filterDefaultForEmpty("")).toBe("-");
+    expect(filterDefaultForEmpty("", { emptyValue: "default" })).toBe("default");
+  });
+
+  it("should return the inputted value, when it is not null or an empty string", () => {
+    expect(filterDefaultForEmpty("Non-empty value")).toBe("Non-empty value");
+    expect(filterDefaultForEmpty(123)).toBe(123);
+  });
+});
+`,N={name:"PageFilterDefaultForEmpty",components:{AlohaPage:h,ATranslation:T,PageFilterArguments:x,PageFilterDefaultForEmptyEmptyValue:B,PageFilterDefaultForEmptyExample:b,PageFilterImportCompositionApi:V,PageFilterImportFunction:P,PageFilterTest:A},setup(){const{pageTitle:e}=G(),{argumentsText:r}=S();return{argumentsText:r,pageTitle:e,test:M}}};function j(e,r,l,u,s,f){const a=t("a-translation"),_=t("page-filter-import-function"),d=t("page-filter-import-composition-api"),y=t("page-filter-arguments"),E=t("page-filter-default-for-empty-example"),F=t("page-filter-default-for-empty-empty-value"),g=t("page-filter-test"),D=t("aloha-page");return m(),i(D,{"page-title":e.pageTitle},{body:$(()=>[o(a,{tag:"p",html:"_PAGE_FILTER_DEFAULT_FOR_EMPTY_DESCRIPTION_"}),o(_,{"function-name":"filterDefaultForEmpty","type-import":"filters"}),o(d,{"function-name":"filterDefaultForEmpty"}),o(y,{"arguments-text":e.argumentsText,"function-description":'filterDefaultForEmpty(value, { [emptyValue="-"] })'},null,8,["arguments-text"]),o(E),o(F),o(g,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const ne=p(N,[["render",j]]);export{ne as default};
