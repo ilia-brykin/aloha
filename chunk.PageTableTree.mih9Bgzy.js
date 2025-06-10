894edbca068d2fcb31855a371e82dde622b4ebf9
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-06-10 07:14:17 +0000

    deploy: d4c6821e80af3227466035301a17a8a14ba38cbd

---
+++ chunk.PageTableTree.mih9Bgzy.js
@@ -0,0 +1,101 @@
+import{A as u,a as T}from"./chunk.AlohaExample.aIxDyYQY.js";import{_ as r,g as A}from"./bundle.index.ATZ0849r.js";import{r as E,U as d,aS as i,e as l,V as c,Y as L,Z as b,d as n}from"./chunk.vendor.COYgx38y.js";import"./chunk.vendor-lodash.DdGPhwvj.js";import{A as f}from"./chunk.ATable.DsNIZS4o.js";import"./chunk.APageTabTitle.O3wC-zJm.js";import"./chunk.AlohaHighlightjs.CW1W2LCP.js";import"./chunk.translations-ar.b162dXrG.js";import"./chunk.translations-de.Bp1FMTbI.js";import"./chunk.translations-en.7H3HHq0Y.js";import"./chunk.translations-es.BNEf9Gj8.js";import"./chunk.translations-fr.CB5Ou-2i.js";import"./chunk.translations-hr.722iD7RH.js";import"./chunk.translations-it.DEMf9FEV.js";import"./chunk.translations-ru.DaYIq0kx.js";function h(){return{codeHtml:`<a-table
+  :columns="columns"
+  :data="data"
+  :is-tree="true"
+  key-id="id"
+  label="_A_TABLE_GROUP_TREE_LABEL_"
+></a-table>`}}function g(){return{codeJs:`import {
+  ref,
+} from "vue";
+
+import { ATable } from "aloha-vue";
+    
+export default {
+  name: "PageTableTreeExample",
+  components: {
+    ATable,
+  },
+  setup() {
+     const columns = [
+      {
+        id: "column1",
+        keyLabel: "id",
+        label: "_A_TABLE_COLUMN_1_",
+      },
+      {
+        id: "column2",
+        keyLabel: "label",
+        label: "_A_TABLE_COLUMN_2_",
+      },
+      {
+        id: "column3",
+        keyLabel: "test",
+        label: "_A_TABLE_COLUMN_3_",
+      },
+    ];
+
+    const data = ref([
+      {
+        id: "1",
+        label: "label 1",
+        test: "test 1",
+      },
+      {
+        id: "2",
+        label: "label 2",
+        test: "test 2",
+        children: [
+          {
+            id: "2_1",
+            label: "label 2.1",
+            test: "test 2.1",
+          },
+          {
+            id: "2_2",
+            label: "label 2.2",
+            test: "test 2.2",
+            children: [
+              {
+                id: "2_2_1",
+                label: "label 2.2.1",
+                test: "test 2.2.1",
+              },
+            ],
+          },
+          {
+            id: "2_3",
+            label: "label 2.3",
+            test: "test 2.3",
+          },
+        ],
+      },
+      {
+        id: "3",
+        label: "label 3",
+        test: "test 3",
+        children: [
+          {
+            id: "3_1",
+            label: "label 3.1",
+            test: "test 3.1",
+          },
+          {
+            id: "3_2",
+            label: "label 3.2",
+            test: "test 3.2",
+          },
+          {
+            id: "3_3",
+            label: "label 3.3",
+            test: "test 3.3",
+          },
+        ],
+      },
+    ]);
+    
+    return {
+      columns,
+      data,
+    };
+  },
+};`}}const B={name:"PageTableTreeExample",components:{AlohaExample:u,ATable:f},setup(){const{codeHtml:e}=h(),{codeJs:t}=g(),a=[{id:"column1",keyLabel:"id",label:"_A_TABLE_COLUMN_1_"},{id:"column2",keyLabel:"label",label:"_A_TABLE_COLUMN_2_"},{id:"column3",keyLabel:"test",label:"_A_TABLE_COLUMN_3_"}],o=E([{id:"1",label:"label 1",test:"test 1"},{id:"2",label:"label 2",test:"test 2",children:[{id:"2_1",label:"label 2.1",test:"test 2.1"},{id:"2_2",label:"label 2.2",test:"test 2.2",children:[{id:"2_2_1",label:"label 2.2.1",test:"test 2.2.1"}]},{id:"2_3",label:"label 2.3",test:"test 2.3"}]},{id:"3",label:"label 3",test:"test 3",children:[{id:"3_1",label:"label 3.1",test:"test 3.1"},{id:"3_2",label:"label 3.2",test:"test 3.2"},{id:"3_3",label:"label 3.3",test:"test 3.3"}]}]);return{codeHtml:e,codeJs:t,columns:a,data:o}}};function P(e,t,a,o,m,p){const s=l("a-table"),_=l("aloha-example");return c(),d(_,{"code-html":e.codeHtml,"code-js":e.codeJs,header:"_A_TABLE_GROUP_TREE_HEADER_",description:"_A_TABLE_GROUP_TREE_DESCRIPTION_",props:"is-tree"},{default:i(()=>[L("div",null,[b(s,{columns:e.columns,data:e.data,"is-tree":!0,"key-id":"id",label:"_A_TABLE_GROUP_TREE_LABEL_"},null,8,["columns","data"])])]),_:1},8,["code-html","code-js"])}const N=r(B,[["render",P]]);function O(){const e=n(()=>A({placeholder:"_A_TABLE_TREE_COMPONENT_NAME_"}));return{pageTitle:n(()=>`ATable ${e.value}`)}}const R={name:"PageTableTree",components:{AlohaPage:T,PageTableTreeExample:N},setup(){const{pageTitle:e}=O();return{pageTitle:e}}};function U(e,t,a,o,m,p){const s=l("page-table-tree-example"),_=l("aloha-page");return c(),d(_,{"page-title":e.pageTitle},{body:i(()=>[b(s)]),_:1},8,["page-title"])}const w=r(R,[["render",U]]);export{w as default};
