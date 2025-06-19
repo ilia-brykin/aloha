59062ef97c9b244c88ac20fb53d7db240374bd47
Ilia <brykin.ilia@gmail.com>
2025-06-19 14:37:47 +0200

    Refactor PageButton components to optimize layout structure and improve maintainability

--- docs/src/views/Components/PageButton/PageButtonAriaDisabled/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonAriaDisabled/compositionAPI/HtmlAPI.js
@@ -1,205 +1,183 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_primary"
-  text="primary"
-  :aria-disabled="true"
-  @click="onAlert('primary')"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_secondary a_ml_2"
-  text="secondary"
-  :aria-disabled="true"
-  @click="onAlert('secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_tertiary a_ml_2"
-  text="tertiary"
-  :aria-disabled="true"
-  @click="onAlert('tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2"
-  text="success"
-  :aria-disabled="true"
-  @click="onAlert('success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_info a_ml_2"
-  text="info"
-  :aria-disabled="true"
-  @click="onAlert('info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_warning a_ml_2"
-  text="warning"
-  :aria-disabled="true"
-  @click="onAlert('warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_danger a_ml_2"
-  text="danger"
-  :aria-disabled="true"
-  @click="onAlert('danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_dark a_ml_2"
-  text="dark"
-  :aria-disabled="true"
-  @click="onAlert('dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_light a_ml_2"
-  text="light"
-  :aria-disabled="true"
-  @click="onAlert('light')"
-></a-button>
-<a-button
-  class="a_ml_2"
-  text="switch"
-  :is-switch="true"
-  :aria-disabled="true"
-></a-button>
-<a-button
-  class="a_ml_2"
-  text="switch active"
-  :is-switch="true"
-  :model-switch="true"
-  :aria-disabled="true"
-></a-button>
-<a-button
-  class="a_btn a_btn_outline_primary a_ml_2"
-  text="outline-primary"
-  :aria-disabled="true"
-  @click="onAlert('outline-primary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_secondary a_ml_2"
-  text="outline-secondary"
-  :aria-disabled="true"
-  @click="onAlert('outline-secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_tertiary a_ml_2"
-  text="outline-tertiary"
-  :aria-disabled="true"
-  @click="onAlert('outline-tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_success a_ml_2"
-  text="outline-success"
-  :aria-disabled="true"
-  @click="onAlert('outline-success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_info a_ml_2"
-  text="outline-info"
-  :aria-disabled="true"
-  @click="onAlert('outline-info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_warning a_ml_2"
-  text="outline-warning"
-  :aria-disabled="true"
-  @click="onAlert('outline-warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_danger a_ml_2"
-  text="outline-danger"
-  :aria-disabled="true"
-  @click="onAlert('outline-danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_dark a_ml_2"
-  text="outline-dark"
-  :aria-disabled="true"
-  @click="onAlert('outline-dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_light a_ml_2"
-  text="outline-light"
-  :aria-disabled="true"
-  @click="onAlert('outline-light')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_primary a_ml_2"
-  text="transparent-primary"
-  :aria-disabled="true"
-  @click="onAlert('transparent-primary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_secondary a_ml_2"
-  text="transparent-secondary"
-  :aria-disabled="true"
-  @click="onAlert('transparent-secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_tertiary a_ml_2"
-  text="transparent-tertiary"
-  :aria-disabled="true"
-  @click="onAlert('transparent-tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_success a_ml_2"
-  text="transparent-success"
-  :aria-disabled="true"
-  @click="onAlert('transparent-success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_info a_ml_2"
-  text="transparent-info"
-  :aria-disabled="true"
-  @click="onAlert('transparent-info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_warning a_ml_2"
-  text="transparent-warning"
-  :aria-disabled="true"
-  @click="onAlert('transparent-warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_danger a_ml_2"
-  text="transparent-danger"
-  :aria-disabled="true"
-  @click="onAlert('transparent-danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_dark a_ml_2"
-  text="transparent-dark"
-  :aria-disabled="true"
-  @click="onAlert('transparent-dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_light a_ml_2"
-  text="transparent-light"
-  :aria-disabled="true"
-  @click="onAlert('transparent-light')"
->
-</a-button>`;
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_primary"
+    text="primary"
+    @click="onAlert('primary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_secondary a_ml_2"
+    text="secondary"
+    @click="onAlert('secondary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_tertiary a_ml_2"
+    text="tertiary"
+    @click="onAlert('tertiary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_success a_ml_2"
+    text="success"
+    @click="onAlert('success')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_info a_ml_2"
+    text="info"
+    @click="onAlert('info')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_warning a_ml_2"
+    text="warning"
+    @click="onAlert('warning')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_danger a_ml_2"
+    text="danger"
+    @click="onAlert('danger')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_dark a_ml_2"
+    text="dark"
+    @click="onAlert('dark')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_light a_ml_2"
+    text="light"
+    @click="onAlert('light')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    :is-switch="true"
+    class="a_ml_2"
+    text="switch"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    :is-switch="true"
+    :model-switch="true"
+    class="a_ml_2"
+    text="switch active"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_primary a_ml_2"
+    text="outline-primary"
+    @click="onAlert('outline-primary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_secondary a_ml_2"
+    text="outline-secondary"
+    @click="onAlert('outline-secondary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_tertiary a_ml_2"
+    text="outline-tertiary"
+    @click="onAlert('outline-tertiary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_success a_ml_2"
+    text="outline-success"
+    @click="onAlert('outline-success')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_info a_ml_2"
+    text="outline-info"
+    @click="onAlert('outline-info')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_warning a_ml_2"
+    text="outline-warning"
+    @click="onAlert('outline-warning')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_danger a_ml_2"
+    text="outline-danger"
+    @click="onAlert('outline-danger')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_dark a_ml_2"
+    text="outline-dark"
+    @click="onAlert('outline-dark')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_outline_light a_ml_2"
+    text="outline-light"
+    @click="onAlert('outline-light')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_primary a_ml_2"
+    text="transparent-primary"
+    @click="onAlert('transparent-primary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_secondary a_ml_2"
+    text="transparent-secondary"
+    @click="onAlert('transparent-secondary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_tertiary a_ml_2"
+    text="transparent-tertiary"
+    @click="onAlert('transparent-tertiary')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_success a_ml_2"
+    text="transparent-success"
+    @click="onAlert('transparent-success')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_info a_ml_2"
+    text="transparent-info"
+    @click="onAlert('transparent-info')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_warning a_ml_2"
+    text="transparent-warning"
+    @click="onAlert('transparent-warning')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_danger a_ml_2"
+    text="transparent-danger"
+    @click="onAlert('transparent-danger')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_dark a_ml_2"
+    text="transparent-dark"
+    @click="onAlert('transparent-dark')"
+  ></a-button>
+  <a-button
+    :aria-disabled="true"
+    class="a_btn a_btn_transparent_light a_ml_2"
+    text="transparent-light"
+    @click="onAlert('transparent-light')"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
--- docs/src/views/Components/PageButton/PageButtonDisabled/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonDisabled/compositionAPI/HtmlAPI.js
@@ -1,205 +1,183 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_primary"
-  text="primary"
-  :disabled="true"
-  @click="onAlert('primary')"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_secondary a_ml_2"
-  text="secondary"
-  :disabled="true"
-  @click="onAlert('secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_tertiary a_ml_2"
-  text="tertiary"
-  :disabled="true"
-  @click="onAlert('tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2"
-  text="success"
-  :disabled="true"
-  @click="onAlert('success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_info a_ml_2"
-  text="info"
-  :disabled="true"
-  @click="onAlert('info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_warning a_ml_2"
-  text="warning"
-  :disabled="true"
-  @click="onAlert('warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_danger a_ml_2"
-  text="danger"
-  :disabled="true"
-  @click="onAlert('danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_dark a_ml_2"
-  text="dark"
-  :disabled="true"
-  @click="onAlert('dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_light a_ml_2"
-  text="light"
-  :disabled="true"
-  @click="onAlert('light')"
-></a-button>
-<a-button
-  class="a_ml_2"
-  text="switch"
-  :is-switch="true"
-  :disabled="true"
-></a-button>
-<a-button
-  class="a_ml_2"
-  text="switch active"
-  :is-switch="true"
-  :model-switch="true"
-  :disabled="true"
-></a-button>
-<a-button
-  class="a_btn a_btn_outline_primary a_ml_2"
-  text="outline-primary"
-  :disabled="true"
-  @click="onAlert('outline-primary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_secondary a_ml_2"
-  text="outline-secondary"
-  :disabled="true"
-  @click="onAlert('outline-secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_tertiary a_ml_2"
-  text="outline-tertiary"
-  :disabled="true"
-  @click="onAlert('outline-tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_success a_ml_2"
-  text="outline-success"
-  :disabled="true"
-  @click="onAlert('outline-success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_info a_ml_2"
-  text="outline-info"
-  :disabled="true"
-  @click="onAlert('outline-info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_warning a_ml_2"
-  text="outline-warning"
-  :disabled="true"
-  @click="onAlert('outline-warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_danger a_ml_2"
-  text="outline-danger"
-  :disabled="true"
-  @click="onAlert('outline-danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_dark a_ml_2"
-  text="outline-dark"
-  :disabled="true"
-  @click="onAlert('outline-dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_outline_light a_ml_2"
-  text="outline-light"
-  :disabled="true"
-  @click="onAlert('outline-light')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_primary a_ml_2"
-  text="transparent-primary"
-  :disabled="true"
-  @click="onAlert('transparent-primary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_secondary a_ml_2"
-  text="transparent-secondary"
-  :disabled="true"
-  @click="onAlert('transparent-secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_tertiary a_ml_2"
-  text="transparent-tertiary"
-  :disabled="true"
-  @click="onAlert('transparent-tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_success a_ml_2"
-  text="transparent-success"
-  :disabled="true"
-  @click="onAlert('transparent-success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_info a_ml_2"
-  text="transparent-info"
-  :disabled="true"
-  @click="onAlert('transparent-info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_warning a_ml_2"
-  text="transparent-warning"
-  :disabled="true"
-  @click="onAlert('transparent-warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_danger a_ml_2"
-  text="transparent-danger"
-  :disabled="true"
-  @click="onAlert('transparent-danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_dark a_ml_2"
-  text="transparent-dark"
-  :disabled="true"
-  @click="onAlert('transparent-dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_transparent_light a_ml_2"
-  text="transparent-light"
-  :disabled="true"
-  @click="onAlert('transparent-light')"
->
-</a-button>`;
+  <a-button
+    class="a_btn a_btn_primary"
+    text="primary"
+    :disabled="true"
+    @click="onAlert('primary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_secondary"
+    text="secondary"
+    :disabled="true"
+    @click="onAlert('secondary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_tertiary"
+    text="tertiary"
+    :disabled="true"
+    @click="onAlert('tertiary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_success"
+    text="success"
+    :disabled="true"
+    @click="onAlert('success')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_info"
+    text="info"
+    :disabled="true"
+    @click="onAlert('info')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_warning"
+    text="warning"
+    :disabled="true"
+    @click="onAlert('warning')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_danger"
+    text="danger"
+    :disabled="true"
+    @click="onAlert('danger')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_dark"
+    text="dark"
+    :disabled="true"
+    @click="onAlert('dark')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_light"
+    text="light"
+    :disabled="true"
+    @click="onAlert('light')"
+  ></a-button>
+  <a-button
+    class="a_ml_2"
+    text="switch"
+    :is-switch="true"
+    :disabled="true"
+  ></a-button>
+  <a-button
+    class="a_ml_2"
+    text="switch active"
+    :is-switch="true"
+    :model-switch="true"
+    :disabled="true"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_primary"
+    text="outline-primary"
+    :disabled="true"
+    @click="onAlert('outline-primary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_secondary"
+    text="outline-secondary"
+    :disabled="true"
+    @click="onAlert('outline-secondary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_tertiary"
+    text="outline-tertiary"
+    :disabled="true"
+    @click="onAlert('outline-tertiary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_success"
+    text="outline-success"
+    :disabled="true"
+    @click="onAlert('outline-success')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_info"
+    text="outline-info"
+    :disabled="true"
+    @click="onAlert('outline-info')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_warning"
+    text="outline-warning"
+    :disabled="true"
+    @click="onAlert('outline-warning')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_danger"
+    text="outline-danger"
+    :disabled="true"
+    @click="onAlert('outline-danger')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_dark"
+    text="outline-dark"
+    :disabled="true"
+    @click="onAlert('outline-dark')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_outline_light"
+    text="outline-light"
+    :disabled="true"
+    @click="onAlert('outline-light')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_primary"
+    text="transparent-primary"
+    :disabled="true"
+    @click="onAlert('transparent-primary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_secondary"
+    text="transparent-secondary"
+    :disabled="true"
+    @click="onAlert('transparent-secondary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_tertiary"
+    text="transparent-tertiary"
+    :disabled="true"
+    @click="onAlert('transparent-tertiary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_success"
+    text="transparent-success"
+    :disabled="true"
+    @click="onAlert('transparent-success')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_info"
+    text="transparent-info"
+    :disabled="true"
+    @click="onAlert('transparent-info')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_warning"
+    text="transparent-warning"
+    :disabled="true"
+    @click="onAlert('transparent-warning')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_danger"
+    text="transparent-danger"
+    :disabled="true"
+    @click="onAlert('transparent-danger')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_dark"
+    text="transparent-dark"
+    :disabled="true"
+    @click="onAlert('transparent-dark')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_transparent_light"
+    text="transparent-light"
+    :disabled="true"
+    @click="onAlert('transparent-light')"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
