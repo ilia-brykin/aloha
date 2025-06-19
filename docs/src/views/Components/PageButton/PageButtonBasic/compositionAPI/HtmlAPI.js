25bd4fab0f627aa25607e853920a3291df01d016
Ilia <brykin.ilia@gmail.com>
2025-06-19 14:40:23 +0200

    Refactor PageButton examples to use a flex layout for better structure and simplified code

--- docs/src/views/Components/PageButton/PageButtonBasic/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonBasic/compositionAPI/HtmlAPI.js
@@ -1,64 +1,58 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_link"
-  text="link"
-  @click="onAlert('link')"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_primary a_ml_2"
-  text="primary"
-  @click="onAlert('primary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_secondary a_ml_2"
-  text="secondary"
-  @click="onAlert('secondary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_tertiary a_ml_2"
-  text="tertiary"
-  @click="onAlert('tertiary')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2"
-  text="success"
-  @click="onAlert('success')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_info a_ml_2"
-  text="info"
-  @click="onAlert('info')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_warning a_ml_2"
-  text="warning"
-  @click="onAlert('warning')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_danger a_ml_2"
-  text="danger"
-  @click="onAlert('danger')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_dark a_ml_2"
-  text="dark"
-  @click="onAlert('dark')"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_light a_ml_2"
-  text="light"
-  @click="onAlert('light')"
->
-</a-button>`;
+  <a-button
+    class="a_btn a_btn_link"
+    text="link"
+    @click="onAlert('link')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_primary"
+    text="primary"
+    @click="onAlert('primary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_secondary"
+    text="secondary"
+    @click="onAlert('secondary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_tertiary"
+    text="tertiary"
+    @click="onAlert('tertiary')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_success"
+    text="success"
+    @click="onAlert('success')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_info"
+    text="info"
+    @click="onAlert('info')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_warning"
+    text="warning"
+    @click="onAlert('warning')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_danger"
+    text="danger"
+    @click="onAlert('danger')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_dark"
+    text="dark"
+    @click="onAlert('dark')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_light"
+    text="light"
+    @click="onAlert('light')"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
