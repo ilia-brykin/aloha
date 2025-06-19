3c16a812ef42ec049c530c054eb28618ede12c57
Ilia <brykin.ilia@gmail.com>
2025-06-19 14:44:19 +0200

    Refactor PageButtonGroup examples to adopt flex layout, add `role="group"` attributes, and improve code structure

--- docs/src/views/Components/PageButton/PageButtonGroup/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonGroup/compositionAPI/HtmlAPI.js
@@ -1,59 +1,57 @@
 export default function HtmlAPI() {
-  const codeHtml = `<div
-  class="a_btn_group"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-  <a-button
-    class="a_btn a_btn_primary"
-    text="primary"
-    @click="onAlert('primary')"
+  <div
+    class="a_btn_group"
+    role="group"
   >
-  </a-button>
-  <a-button
-    class="a_btn a_btn_secondary"
-    text="secondary"
-    @click="onAlert('secondary')"
+    <a-button
+      class="a_btn a_btn_primary"
+      text="primary"
+      @click="onAlert('primary')"
+    ></a-button>
+    <a-button
+      class="a_btn a_btn_secondary"
+      text="secondary"
+      @click="onAlert('secondary')"
+    ></a-button>
+    <a-button
+      class="a_btn a_btn_success"
+      text="success"
+      @click="onAlert('success')"
+    ></a-button>
+    <a-button
+      class="a_btn a_btn_info"
+      text="info"
+      @click="onAlert('info')"
+    ></a-button>
+  </div>
+  <div
+    class="a_btn_group"
+    role="group"
   >
-  </a-button>
-  <a-button
-    class="a_btn a_btn_success"
-    text="success"
-    @click="onAlert('success')"
-  >
-  </a-button>
-  <a-button
-    class="a_btn a_btn_info"
-    text="info"
-    @click="onAlert('info')"
-  >
-  </a-button>
-</div>
-<div
-  class="a_btn_group a_ml_3"
->
-  <a-button
-    class="a_btn a_btn_outline_primary"
-    text="outline-primary"
-    @click="onAlert('outline-primary')"
-  >
-  </a-button>
-  <a-button
-    class="a_btn a_btn_outline_secondary"
-    text="outline-secondary"
-    @click="onAlert('outline-secondary')"
-  >
-  </a-button>
-  <a-button
-    class="a_btn a_btn_outline_success"
-    text="outline-success"
-    @click="onAlert('outline-success')"
-  >
-  </a-button>
-  <a-button
-    class="a_btn a_btn_outline_info"
-    text="outline-info"
-    @click="onAlert('outline-info')"
-  >
-  </a-button>
+    <a-button
+      class="a_btn a_btn_outline_primary"
+      text="outline-primary"
+      @click="onAlert('outline-primary')"
+    ></a-button>
+    <a-button
+      class="a_btn a_btn_outline_secondary"
+      text="outline-secondary"
+      @click="onAlert('outline-secondary')"
+    ></a-button>
+    <a-button
+      class="a_btn a_btn_outline_success"
+      text="outline-success"
+      @click="onAlert('outline-success')"
+    ></a-button>
+    <a-button
+      class="a_btn a_btn_outline_info"
+      text="outline-info"
+      @click="onAlert('outline-info')"
+    ></a-button>
+  </div>
 </div>`;
 
   return {
