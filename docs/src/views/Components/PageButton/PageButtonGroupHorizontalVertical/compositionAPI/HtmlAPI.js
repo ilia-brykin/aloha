e28087f9dd3c0e975c59556f6a3eb589256e1297
Ilia <brykin.ilia@gmail.com>
2025-06-19 14:46:27 +0200

    Add `role="group"` attribute to PageButtonGroup examples for improved accessibility

--- docs/src/views/Components/PageButton/PageButtonGroupHorizontalVertical/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonGroupHorizontalVertical/compositionAPI/HtmlAPI.js
@@ -1,90 +1,82 @@
 export default function HtmlAPI() {
   const codeHtml = `<div
   class="a_btn_group_vertical"
+  role="group"
 >
   <div
     class="a_btn_group"
+    role="group"
   >
     <a-button
       class="a_btn a_btn_outline_primary"
       text="outline-primary"
       @click="onAlert('outline-primary')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_secondary"
       text="outline-secondary"
       @click="onAlert('outline-secondary')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_success"
       text="outline-success"
       @click="onAlert('outline-success')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_info"
       text="outline-info"
       @click="onAlert('outline-info')"
-    >
-    </a-button>
+    ></a-button>
   </div>
   <div
     class="a_btn_group"
+    role="group"
   >
     <a-button
       class="a_btn a_btn_outline_primary"
       text="outline-primary"
       @click="onAlert('outline-primary')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_secondary"
       text="outline-secondary"
       @click="onAlert('outline-secondary')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_success"
       text="outline-success"
       @click="onAlert('outline-success')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_info"
       text="outline-info"
       @click="onAlert('outline-info')"
-    >
-    </a-button>
+    ></a-button>
   </div>
   <div
     class="a_btn_group"
+    role="group"
   >
     <a-button
       class="a_btn a_btn_outline_primary"
       text="outline-primary"
       @click="onAlert('outline-primary')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_secondary"
       text="outline-secondary"
       @click="onAlert('outline-secondary')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_success"
       text="outline-success"
       @click="onAlert('outline-success')"
-    >
-    </a-button>
+    ></a-button>
     <a-button
       class="a_btn a_btn_outline_info"
       text="outline-info"
       @click="onAlert('outline-info')"
-    >
-    </a-button>
+    ></a-button>
   </div>
 </div>`;
 
