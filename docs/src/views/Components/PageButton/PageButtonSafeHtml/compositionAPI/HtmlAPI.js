2cb7efc6d69a144080da86dc8069aea8061d50fb
Ilia <brykin.ilia@gmail.com>
2025-06-19 15:03:59 +0200

    Refactor PageButtonSafeHtml examples to adopt flex layout, adjust HTML structure, and improve code consistency

--- docs/src/views/Components/PageButton/PageButtonSafeHtml/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonSafeHtml/compositionAPI/HtmlAPI.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-useless-escape */
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_primary"
-  safe-html="_A_BUTTON_EXAMPLE_HTML_"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_primary a_ml_2"
-  safe-html="<span onclick='alert(\\"Aloha\\")'>Aloha</button>"
->
-</a-button>`;
+  <a-button
+    class="a_btn a_btn_primary"
+    safe-html="_A_BUTTON_EXAMPLE_HTML_"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_primary"
+    safe-html="<span onclick='alert(\"Aloha\")'>Aloha</button>"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
