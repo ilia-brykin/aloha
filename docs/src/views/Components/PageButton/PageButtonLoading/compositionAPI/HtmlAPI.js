0023c8924764a645c6e7317355916dcf685e28a2
Ilia <brykin.ilia@gmail.com>
2025-06-19 15:01:12 +0200

    Refactor PageButtonLoading examples to adopt flex layout, remove redundant `a_ml_2` class, and improve code consistency

--- docs/src/views/Components/PageButton/PageButtonLoading/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonLoading/compositionAPI/HtmlAPI.js
@@ -1,20 +1,23 @@
+/* eslint-disable no-useless-escape */
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_primary"
-  :loading="loading"
-  loading-align="left"
-  text="loading-align=\\"left\\""
-  @click="toggleLoading"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_primary a_ml_2"
-  :loading="loading"
-  loading-align="right"
-  text="loading-align=\\"right\\""
-  @click="toggleLoading"
->
-</a-button>`;
+  <a-button
+    class="a_btn a_btn_primary"
+    :loading="loading"
+    loading-align="left"
+    text="loading-align=\"left\""
+    @click="toggleLoading"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_primary"
+    :loading="loading"
+    loading-align="right"
+    text="loading-align=\"right\""
+    @click="toggleLoading"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
