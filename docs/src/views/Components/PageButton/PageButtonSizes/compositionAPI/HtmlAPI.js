77f2a99f38a87fcaec221cb8ba4eff29aaa7ab2c
Ilia <brykin.ilia@gmail.com>
2025-06-19 15:04:56 +0200

    Refactor PageButtonSizes examples to adopt flex layout, adjust HTML structure, and improve code consistency

--- docs/src/views/Components/PageButton/PageButtonSizes/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonSizes/compositionAPI/HtmlAPI.js
@@ -1,16 +1,18 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_primary a_btn_large"
-  text="large"
-  @click="onAlert('large')"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_primary a_btn_small a_ml_2"
-  text="small"
-  @click="onAlert('small')"
->
-</a-button>`;
+  <a-button
+    class="a_btn a_btn_primary a_btn_large"
+    text="large"
+    @click="onAlert('large')"
+  ></a-button>
+  <a-button
+    class="a_btn a_btn_primary a_btn_small"
+    text="small"
+    @click="onAlert('small')"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
