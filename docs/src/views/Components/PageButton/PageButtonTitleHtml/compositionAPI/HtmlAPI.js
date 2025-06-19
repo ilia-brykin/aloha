4363e8be11375ee635fb311d3af4807190854d2b
Ilia <brykin.ilia@gmail.com>
2025-06-19 15:12:14 +0200

    Refactor PageButtonTitleHtml examples to adopt flex layout, adjust HTML structure, and improve code consistency

--- docs/src/views/Components/PageButton/PageButtonTitleHtml/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonTitleHtml/compositionAPI/HtmlAPI.js
@@ -1,133 +1,121 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-button
-  class="a_btn a_btn_success a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: top"
-  :is-title-html="true"
-  title-placement="top"
+  const codeHtml = `<div 
+  class="a_d_flex a_flex_wrap gap_1"
 >
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: auto"
-  :is-title-html="true"
-  title-placement="auto"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: auto-start"
-  :is-title-html="true"
-  title-placement="auto-start"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: auto-end"
-  :is-title-html="true"
-  title-placement="auto-end"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: top-start"
-  :is-title-html="true"
-  title-placement="top-start"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: top-end"
-  :is-title-html="true"
-  title-placement="top-end"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: right"
-  :is-title-html="true"
-  title-placement="right"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: right-start"
-  :is-title-html="true"
-  title-placement="right-start"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: right-end"
-  :is-title-html="true"
-  title-placement="right-end"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: bottom"
-  :is-title-html="true"
-  title-placement="bottom"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: bottom-start"
-  :is-title-html="true"
-  title-placement="bottom-start"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: bottom-end"
-  :is-title-html="true"
-  title-placement="bottom-end"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: left"
-  :is-title-html="true"
-  title-placement="left"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: left-start"
-  :is-title-html="true"
-  title-placement="left-start"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: left-end"
-  :is-title-html="true"
-  title-placement="left-end"
->
-</a-button>
-<a-button
-  class="a_btn a_btn_success a_ml_2 a_mt_2"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
-  text="placement: auto, html-title max-width 100"
-  :is-title-html="true"
-  title-placement="auto"
-  :title-attributes="{ maxWidth: 150 }"
->
-</a-button>`;
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: top"
+    title-placement="top"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: auto"
+    title-placement="auto"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: auto-start"
+    title-placement="auto-start"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: auto-end"
+    title-placement="auto-end"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: top-start"
+    title-placement="top-start"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: top-end"
+    title-placement="top-end"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: right"
+    title-placement="right"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: right-start"
+    title-placement="right-start"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: right-end"
+    title-placement="right-end"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: bottom"
+    title-placement="bottom"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: bottom-start"
+    title-placement="bottom-start"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: bottom-end"
+    title-placement="bottom-end"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: left"
+    title-placement="left"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: left-start"
+    title-placement="left-start"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    class="a_btn a_btn_success"
+    text="placement: left-end"
+    title-placement="left-end"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+  <a-button
+    :is-title-html="true"
+    :title-attributes="{ maxWidth: 150 }"
+    class="a_btn a_btn_success"
+    text="placement: auto, html-title max-width 150"
+    title-placement="auto"
+    title="_A_BUTTON_EXAMPLE_BIG_TITLE_"
+  ></a-button>
+</div>`;
 
   return {
     codeHtml,
