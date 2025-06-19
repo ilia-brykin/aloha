b031128bcf2692de22856915c11e92b7f3d06b3d
Ilia <brykin.ilia@gmail.com>
2025-06-19 15:12:52 +0200

    Refactor PageButtonTitleHtmlExtra examples to adjust prop placement, update HTML structure, and improve code consistency

--- docs/src/views/Components/PageButton/PageButtonTitleHtmlExtra/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/PageButton/PageButtonTitleHtmlExtra/compositionAPI/HtmlAPI.js
@@ -1,10 +1,10 @@
 export default function HtmlAPI() {
   const codeHtml = `<a-button
-  class="a_btn a_btn_success"
-  title="_A_BUTTON_EXAMPLE_BIG_TITLE_{{number}}_"
-  text="extra"
-  :is-title-html="true"
   :extra="{ number: 2 }"
+  :is-title-html="true"
+  class="a_btn a_btn_success"
+  text="extra"
+  title="_A_BUTTON_EXAMPLE_BIG_TITLE_{{number}}_"
 ></a-button>`;
 
   return {
