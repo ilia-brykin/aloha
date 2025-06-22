71394e094b482e3e43742001d4287a9f8251190d
Ilia <brykin.ilia@gmail.com>
2025-06-22 22:04:22 +0200

    Refactor ACarousel styles: replace hardcoded colors and dimensions with CSS variables, update SVGs to remove fixed dimensions for improved flexibility.

--- src/plugins/ACarouselPlugin.js
+++ src/plugins/ACarouselPlugin.js
@@ -3,12 +3,12 @@ export const ACarouselPluginOptions = {
 
   },
   icons: {
-    dots: `<svg width="34" height="34" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    dots: `<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
   <circle class="border" cx="16" cy="15" r="10"></circle>
   <circle class="tab-background" cx="16" cy="15" r="8"></circle>
   <circle class="tab" cx="16" cy="15" r="6"></circle>
 </svg>`,
-    squares: `<svg width="34" height="34" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    squares: `<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
   <rect class="border" x="6" y="5" width="20" height="20" rx="2" ry="2"></rect>
   <rect class="tab-background" x="8" y="7" width="16" height="16" rx="2" ry="2"></rect>
   <rect class="tab" x="10" y="9" width="12" height="12" rx="2" ry="2"></rect>
