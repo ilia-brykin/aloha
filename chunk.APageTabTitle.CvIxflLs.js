3bb7e8fd28be115819ce051bae06b916c3d35ded
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-06-22 13:40:23 +0000

    deploy: dc91a05ef369231655c3484a0e28eaeef4220664

--- chunk.APageTabTitle.CvIxflLs.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./bundle.index.D1ZGo2_x.js";const a={name:"APageTabTitle",props:{title:{type:String,required:!0},extra:{type:Object,required:!1,default:void 0}},setup(e){t(e)},render(){return""}};export{a as A};
