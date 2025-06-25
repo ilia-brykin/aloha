9e431d79453fae21e301dae71671d1cd47dc00e5
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-06-25 18:59:46 +0000

    deploy: 445dafcd9a3de94a727fdbc3de963e226e440462

---
+++ chunk.PageFilterFloat.DEajd319.js
@@ -0,0 +1,97 @@
+import{A as g,a as G}from"./chunk.AlohaExample.CIA6MZWW.js";import{P,a as T,b as I}from"./chunk.PageFilterTest.Cir8b_z_.js";import{aH as o,_ as r,A as E}from"./bundle.index.CNwRIZDd.js";import{U as s,e,V as p,aS as J,Z as n}from"./chunk.vendor.COYgx38y.js";import"./chunk.vendor-lodash.xyEKcfD7.js";import{P as S}from"./chunk.PageFilterImportCompositionApi.woAu01C0.js";import"./chunk.APageTabTitle.CTgdS-Fo.js";import"./chunk.AlohaHighlightjs.CW1W2LCP.js";import"./chunk.translations-ar.Ba3-jBbd.js";import"./chunk.translations-de.BkjBBp2O.js";import"./chunk.translations-en.BXMPX4Pv.js";import"./chunk.translations-es.C_lJjw9A.js";import"./chunk.translations-fr.Duvlphhj.js";import"./chunk.translations-hr.BcoPLaR6.js";import"./chunk.translations-it.B0l4u3QD.js";import"./chunk.translations-ru.BBvNAu0E.js";function v(){return{codeJs:`filterFloat(1000222.55, { digitGrouping: false });
+// ${o(100022255e-2,{digitGrouping:!1})}
+
+filterFloat(1000222.55, { digitGrouping: true });
+// ${o(100022255e-2,{digitGrouping:!0})}`}}const b={name:"PageFilterFloatDigitGrouping",components:{AlohaExample:g},setup(){const{codeJs:t}=v();return{codeJs:t}}};function L(t,l,a,f,c,u){const i=e("aloha-example");return p(),s(i,{"code-js":t.codeJs,header:"digitGrouping","is-code-visible-default":!0},null,8,["code-js"])}const R=r(b,[["render",L]]);function j(){return{codeJs:`filterFloat(2000, { digits: 0 });
+// ${o(2e3,{digits:0})}
+
+filterFloat("25.53451", { digits: 4 });
+// ${o("25.53451",{digits:4})}`}}const N={name:"PageFilterFloatDigits",components:{AlohaExample:g},setup(){const{codeJs:t}=j();return{codeJs:t}}};function U(t,l,a,f,c,u){const i=e("aloha-example");return p(),s(i,{"code-js":t.codeJs,header:"digits","is-code-visible-default":!0},null,8,["code-js"])}const D=r(N,[["render",U]]);function O(){return{codeJs:`filterFloat(123456);
+// ${o(123456)}
+
+filterFloat("1000000.2345");
+// ${o("1000000.2345")}
+
+filterFloat("aloha");
+// ${o("aloha")}
+
+filterFloat(undefined);
+// ${o(void 0)}`}}const y={name:"PageFilterFloatExample",components:{AlohaExample:g},setup(){const{codeJs:t}=O();return{codeJs:t}}};function C(t,l,a,f,c,u){const i=e("aloha-example");return p(),s(i,{"code-js":t.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const M=r(y,[["render",C]]);function V(){return{codeJs:`filterFloat(2000, { suffix: "€" });
+// ${o(2e3,{suffix:"€"})}
+
+filterFloat("25.5", { suffix: "%" });
+// ${o("25.5",{suffix:"%"})}`}}const k={name:"PageFilterFloatSuffix",components:{AlohaExample:g},setup(){const{codeJs:t}=V();return{codeJs:t}}};function w(t,l,a,f,c,u){const i=e("aloha-example");return p(),s(i,{"code-js":t.codeJs,header:"suffix","is-code-visible-default":!0},null,8,["code-js"])}const H=r(k,[["render",w]]);function X(){return{argumentsText:[{value:"value",types:["Number","String"],text:"_PAGE_FILTER_FLOAT_ARGUMENTS_VALUE_"},{value:'[suffix=""]',types:["String"],text:"_PAGE_FILTER_FLOAT_ARGUMENTS_SUFFIX_"},{value:"[digits=2]",types:["Number"],text:"_PAGE_FILTER_FLOAT_ARGUMENTS_DIGITS_"},{value:"[digitGrouping=true]",types:["Boolean"],text:"_PAGE_FILTER_FLOAT_ARGUMENTS_DIGIT_GROUPING_"}]}}function Z(){return{pageTitle:"filterFloat"}}const q=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterFloat from "../filterFloat";
+
+describe("filterFloat", () => {
+  it("without extra attributes", () => {
+    expect(filterFloat(123456)).toBe("123.456,00");
+    expect(filterFloat(1000)).toBe("1.000,00");
+    expect(filterFloat(1000000)).toBe("1.000.000,00");
+    expect(filterFloat(1000.12)).toBe("1.000,12");
+    expect(filterFloat(0.12)).toBe("0,12");
+    expect(filterFloat(0.1234)).toBe("0,12");
+    expect(filterFloat(0)).toBe("0,00");
+    expect(filterFloat(0.000)).toBe("0,00");
+    expect(filterFloat(undefined)).toBe("");
+    expect(filterFloat(null)).toBe("");
+    expect(filterFloat("")).toBe("0,00");
+    expect(filterFloat("1000.12")).toBe("1.000,12");
+    expect(filterFloat("1.000.000,00")).toBe("1.000.000,00");
+    expect(filterFloat("Aloha")).toBe("Aloha");
+  });
+
+  it("suffix", () => {
+    expect(filterFloat(123456, { suffix: "%" })).toBe("123.456,00 %");
+    expect(filterFloat(1000, { suffix: "€" })).toBe("1.000,00 €");
+    expect(filterFloat("1000", { suffix: "%" })).toBe("1.000,00 %");
+    expect(filterFloat("Aloha", { suffix: "%" })).toBe("Aloha");
+    expect(filterFloat(undefined, { suffix: "%" })).toBe("");
+    expect(filterFloat(null, { suffix: "%" })).toBe("");
+    expect(filterFloat("", { suffix: "%" })).toBe("0,00 %");
+    expect(filterFloat("1.000.000,00", { suffix: "%" })).toBe("1.000.000,00 %");
+  });
+
+  it("digits", () => {
+    expect(filterFloat(123456, { digits: 2 })).toBe("123.456,00");
+    expect(filterFloat(123456, { digits: 4 })).toBe("123.456,0000");
+    expect(filterFloat(123.12345, { digits: 3 })).toBe("123,123");
+    expect(filterFloat(123.12345, { digits: 1 })).toBe("123,1");
+    expect(filterFloat(123.12345, { digits: 0 })).toBe("123");
+    expect(filterFloat(1000.12345, { digits: 0 })).toBe("1.000");
+    expect(filterFloat("", { digits: 0 })).toBe("0");
+    expect(filterFloat("Aloha", { digits: 0 })).toBe("Aloha");
+    expect(filterFloat(undefined, { digits: 0 })).toBe("");
+    expect(filterFloat(null, { digits: 0 })).toBe("");
+    expect(filterFloat("1.000.000,00", { digits: 0 })).toBe("1.000.000");
+  });
+
+  it("digitGrouping", () => {
+    expect(filterFloat(123456, { digitGrouping: true })).toBe("123.456,00");
+    expect(filterFloat(123456, { digitGrouping: false })).toBe("123456,00");
+    expect(filterFloat(1000000, { digitGrouping: true })).toBe("1.000.000,00");
+    expect(filterFloat(1000000, { digitGrouping: false })).toBe("1000000,00");
+  });
+
+  it("negative value", () => {
+    expect(filterFloat(-123456)).toBe("-123.456,00");
+    expect(filterFloat(-1000000)).toBe("-1.000.000,00");
+    expect(filterFloat(-1)).toBe("-1,00");
+    expect(filterFloat(-0)).toBe("0,00");
+    expect(filterFloat("-1.000.000,00")).toBe("-1.000.000,00");
+    expect(filterFloat("-1.000.000,01234")).toBe("-1.000.000,01");
+  });
+
+  it("complex example", () => {
+    expect(filterFloat(1000000, { suffix: "%", digits: 0, digitGrouping: false })).toBe("1000000 %");
+    expect(filterFloat(0.123, { suffix: "%", digits: 0, digitGrouping: false })).toBe("0 %");
+    expect(filterFloat(0.123, { suffix: "%", digits: 4, digitGrouping: false })).toBe("0,1230 %");
+  });
+});
+
+`,z={name:"PageFilterFloat",components:{AlohaPage:G,ATranslation:E,PageFilterArguments:I,PageFilterFloatDigitGrouping:R,PageFilterFloatDigits:D,PageFilterFloatExample:M,PageFilterFloatSuffix:H,PageFilterImportCompositionApi:S,PageFilterImportFunction:T,PageFilterTest:P},setup(){const{pageTitle:t}=Z(),{argumentsText:l}=X();return{argumentsText:l,pageTitle:t,test:q}}};function K(t,l,a,f,c,u){const i=e("a-translation"),_=e("page-filter-import-function"),d=e("page-filter-import-composition-api"),F=e("page-filter-arguments"),x=e("page-filter-float-example"),m=e("page-filter-float-suffix"),B=e("page-filter-float-digits"),$=e("page-filter-float-digit-grouping"),A=e("page-filter-test"),h=e("aloha-page");return p(),s(h,{"page-title":t.pageTitle},{body:J(()=>[n(i,{tag:"p",html:"_PAGE_FILTER_FLOAT_DESCRIPTION_"}),n(_,{"function-name":"filterFloat","type-import":"filters"}),n(d,{"function-name":"filterFloat"}),n(F,{"arguments-text":t.argumentsText,"function-description":'filterFloat(value, { [suffix=""], [digits=2], [digitGrouping=true] })'},null,8,["arguments-text"]),n(x),n(m),n(B),n($),n(A,{test:t.test},null,8,["test"])]),_:1},8,["page-title"])}const gt=r(z,[["render",K]]);export{gt as default};
---
+++ chunk.PageUtilsMathIsEven.DP8CaaTz.js
@@ -0,0 +1,72 @@
+import{A as d,a as x}from"./chunk.AlohaExample.CIA6MZWW.js";import{P as h,a as A,b as T}from"./chunk.PageFilterTest.Cir8b_z_.js";import{F as t,_ as r,A as $}from"./bundle.index.CNwRIZDd.js";import{U as p,e as n,V as l,aS as y,Z as s}from"./chunk.vendor.COYgx38y.js";import"./chunk.vendor-lodash.xyEKcfD7.js";import"./chunk.APageTabTitle.CTgdS-Fo.js";import"./chunk.AlohaHighlightjs.CW1W2LCP.js";import"./chunk.translations-ar.Ba3-jBbd.js";import"./chunk.translations-de.BkjBBp2O.js";import"./chunk.translations-en.BXMPX4Pv.js";import"./chunk.translations-es.C_lJjw9A.js";import"./chunk.translations-fr.Duvlphhj.js";import"./chunk.translations-hr.BcoPLaR6.js";import"./chunk.translations-it.B0l4u3QD.js";import"./chunk.translations-ru.BBvNAu0E.js";function B(){return{codeJs:`isEven(2);
+// ${t(2)}
+isEven(0);
+// ${t(0)}
+isEven(246);
+// ${t(246)}
+
+isEven(-2);
+// ${t(-2)}
+
+isEven(1);
+// ${t(1)}
+isEven(3);
+// ${t(3)}
+isEven(353);
+// ${t(353)}
+
+isEven(-1);
+// ${t(-1)}
+
+isEven(2.5);
+// ${t(2.5)}
+
+isEven("Aloha");
+// ${t("Aloha")}
+isEven(null);
+// ${t(null)}
+isEven(undefined);
+// ${t(void 0)}`}}const F={name:"PageUtilsMathIsOddExample",components:{AlohaExample:d},setup(){const{codeJs:e}=B();return{codeJs:e}}};function P(e,o,a,c,u,m){const i=n("aloha-example");return l(),p(i,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const I=r(F,[["render",P]]);function b(){return{argumentsText:[{value:"value",types:["Number"],text:"_PAGE_UTILS_MATH_IS_EVEN_ARGUMENTS_VALUE_"}]}}function U(){return{pageTitle:"isEven"}}const M=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import {
+  isEven,
+} from "../utilsMath";
+
+describe("isEven function", () => {
+  it("Return true if given an even number", () => {
+    expect(isEven(0)).toBeTruthy();
+    expect(isEven(2)).toBeTruthy();
+    expect(isEven(123454568)).toBeTruthy();
+  });
+
+  it("Return false if given an odd number", () => {
+    expect(isEven(1)).toBeFalsy();
+    expect(isEven(3)).toBeFalsy();
+    expect(isEven(123457)).toBeFalsy();
+  });
+
+  it("Return false if given a non-integer", () => {
+    expect(isEven(2.5)).toBeFalsy();
+    expect(isEven("Aloha")).toBeFalsy();
+    expect(isEven(null)).toBeFalsy();
+    expect(isEven(undefined)).toBeFalsy();
+    expect(isEven({})).toBeFalsy();
+    expect(isEven([])).toBeFalsy();
+  });
+
+  it("Return true if given a negative even number", () => {
+    expect(isEven(-2)).toBeTruthy();
+    expect(isEven(-346)).toBeTruthy();
+  });
+
+  it("Return false if given a negative odd number", () => {
+    expect(isEven(-1)).toBeFalsy();
+    expect(isEven(-3)).toBeFalsy();
+    expect(isEven(-267)).toBeFalsy();
+  });
+});
+`,S={name:"PageUtilsMathIsEven",components:{AlohaPage:x,ATranslation:$,PageFilterArguments:T,PageFilterImportFunction:A,PageFilterTest:h,PageUtilsMathIsEvenExample:I},setup(){const{pageTitle:e}=U(),{argumentsText:o}=b();return{argumentsText:o,pageTitle:e,test:M}}};function R(e,o,a,c,u,m){const i=n("a-translation"),_=n("page-filter-import-function"),v=n("page-filter-arguments"),E=n("page-utils-math-is-even-example"),g=n("page-filter-test"),f=n("aloha-page");return l(),p(f,{"page-title":e.pageTitle},{body:y(()=>[s(i,{tag:"p",html:"_PAGE_UTILS_MATH_IS_EVEN_DESCRIPTION_"}),s(_,{"function-name":"isEven","type-import":"utilsMath"}),s(v,{"arguments-text":e.argumentsText,"function-description":"isEven(value)"},null,8,["arguments-text"]),s(E),s(g,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const K=r(S,[["render",R]]);export{K as default};
