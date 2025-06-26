a72e1d2af972fdcbf8d1feea1362dd82d65adb5f
Ilia Brykin <ilia.brykin@prounix.de>
2025-06-26 07:54:14 +0200

    Add `AList` component with `prepend` slot to `PageSelectStyleFilterGroups` for enhanced filtering options.

--- docs/src/views/Components/Form/PageSelectStyle/PageSelectStyleFilterGroups/PageSelectStyleFilterGroups.js
+++ docs/src/views/Components/Form/PageSelectStyle/PageSelectStyleFilterGroups/PageSelectStyleFilterGroups.js
@@ -4,6 +4,7 @@ import {
 
 import AlohaExample from "../../../../../global/components/AlohaExample/AlohaExample.vue";
 import {
+  AList,
   ASelectStyle,
 } from "aloha-vue";
 
@@ -13,6 +14,7 @@ import JsAPI from "./compositionAPI/JsAPI";
 export default {
   name: "PageSelectStyleFilterGroups",
   components: {
+    AList,
     AlohaExample,
     ASelectStyle,
   },
@@ -25,12 +27,37 @@ export default {
       codeJs,
     } = JsAPI();
 
+    const groups = [
+      [
+        "display",
+        ["mobile", "tablet", "touch", "desktop", "widescreen", "fullhd", "large"],
+      ],
+      [
+        "flex",
+        ["flex_direction", "justify_content", "align_items", "align_self", "justify_self", "flex_wrap", "align_content"],
+      ],
+      [
+        "text_align",
+        ["mobile", "tablet", "touch", "desktop", "widescreen", "fullhd", "large"],
+      ],
+      "text_wrapping",
+      "line_limiting",
+      "text_transform",
+      "font_weight",
+      "line_height",
+      "font_size",
+      "margin",
+      "padding",
+      "color",
+      "background_color",
+    ];
     const model1 = ref(undefined);
     const model2 = ref(undefined);
 
     return {
       codeHtml,
       codeJs,
+      groups,
       model1,
       model2,
     };
