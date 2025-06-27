dd822a4b4a7159e3c4cae8994c2d400845ec4416
NKDante <NKDante@users.noreply.github.com>
2025-06-27 10:32:31 +0000

    deploy: f17b13abca6d609f9906d87bc4c74a423965192f

---
+++ chunk.APageTabTitle.Ce8OKMcV.js
@@ -0,0 +1,1 @@
+import{t}from"./bundle.index.L7kL3tQT.js";const a={name:"APageTabTitle",props:{title:{type:String,required:!0},extra:{type:Object,required:!1,default:void 0}},setup(e){t(e)},render(){return""}};export{a as A};
