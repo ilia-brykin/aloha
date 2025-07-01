1e48883aa8a6d4ef988419f2f29f59fbd3893324
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-07-01 06:16:05 +0000

    deploy: e49393fbfc39fb6168b32892682fa3103adcc5dd

---
+++ chunk.PageTablePreviewDown.CQewfhpp.js
@@ -0,0 +1,67 @@
+import{A as T,a as b}from"./chunk.AlohaExample.CzvFUthm.js";import{_ as p,g as E}from"./bundle.index.CQwUm3aG.js";import{K as h}from"./chunk.vendor-lodash.BGdgYQ9t.js";import{r as P,U as d,aS as r,e as _,V as A,Y as m,Z as u,aT as f,d as i}from"./chunk.vendor.B8i9Zb03.js";import{A as g}from"./chunk.ATable.EyINfk4j.js";import"./chunk.APageTabTitle.BYbKAvYa.js";import"./chunk.AlohaHighlightjs.DOgKZA3M.js";import"./chunk.translations-ar.DEWt73y8.js";import"./chunk.translations-de.BUYjlGcE.js";import"./chunk.translations-en.CPCEcTAP.js";import"./chunk.translations-es.BKTK5PUv.js";import"./chunk.translations-fr.CslimZmU.js";import"./chunk.translations-hr.CiJ7_Y2m.js";import"./chunk.translations-it.H45F8miC.js";import"./chunk.translations-ru.o-dOzdYz.js";function B(){return{codeHtml:`<a-table
+  :columns="columns"
+  :data="data"
+  label="_A_TABLE_GROUP_SIMPLE_LABEL_"
+  key-id="id"
+>
+</a-table>`}}function D(){return{codeJs:`import {
+  ref,
+} from "vue";
+
+import { ATable } from "aloha-vue";
+    
+export default {
+  name: "PageTablePreviewDownExample",
+  components: {
+    ATable,
+  },
+  setup() {
+     const columns = [
+      {
+        id: "column1",
+        keyLabel: "id",
+        label: "_A_TABLE_COLUMN_1_",
+      },
+      {
+        id: "column2",
+        keyLabel: "aloha",
+        label: "_A_TABLE_COLUMN_2_",
+      },
+      {
+        id: "column3",
+        keyLabel: "aloha",
+        label: "_A_TABLE_COLUMN_3_",
+      },
+      {
+        id: "column4",
+        keyLabel: "aloha",
+        label: "_A_TABLE_COLUMN_4_",
+      },
+      {
+        id: "column5",
+        keyLabel: "aloha",
+        label: "_A_TABLE_COLUMN_5_",
+      },
+    ];
+
+    const data = ref([]);
+
+    const setData = () => {
+      const DATA = [];
+      times(20, item => {
+        DATA.push({
+          id: item,
+          aloha: \`aloha \${ item }\`,
+        });
+      });
+      data.value = DATA;
+    };
+
+    setData();
+    
+    return {
+      columns,
+      data,
+    };
+  },
+};`}}const w={name:"PageTablePreviewDownRight",components:{AlohaExample:T,ATable:g},setup(){const{codeHtml:e}=B(),{codeJs:o}=D(),n=[{id:"column1",keyLabel:"id",label:"_A_TABLE_COLUMN_1_"},{id:"column2",keyLabel:"aloha",label:"_A_TABLE_COLUMN_2_"},{id:"column3",keyLabel:"aloha",label:"_A_TABLE_COLUMN_3_"},{id:"column4",keyLabel:"aloha",label:"_A_TABLE_COLUMN_4_"},{id:"column5",keyLabel:"aloha",label:"_A_TABLE_COLUMN_5_"}],t=P([]);return(()=>{const l=[];h(20,a=>{l.push({id:a,aloha:`aloha ${a}`})}),t.value=l})(),{codeHtml:e,codeJs:o,columns:n,data:t}}};function O(e,o,n,t,c,l){const a=_("a-table"),s=_("aloha-example");return A(),d(s,{"code-html":e.codeHtml,"code-js":e.codeJs,header:"_A_TABLE_GROUP_PREVIEW_DOWN_EXAMPLE_HEADER_",description:"_A_TABLE_GROUP_PREVIEW_DOWN_EXAMPLE_DESCRIPTION_"},{default:r(()=>[m("div",null,[u(a,{columns:e.columns,data:e.data,label:"_A_TABLE_GROUP_SIMPLE_LABEL_","key-id":"id",preview:"down"},{preview:r(L=>[m("pre",null,f(L),1)]),_:1},8,["columns","data"])])]),_:1},8,["code-html","code-js"])}const N=p(w,[["render",O]]);function v(){const e=i(()=>E({placeholder:"_A_TABLE_PREVIEW_DOWN_PAGE_TITLE_"}));return{pageTitle:i(()=>`ATable ${e.value}`)}}const U={name:"PageTablePreviewDown",components:{AlohaPage:b,PageTablePreviewDownExample:N},setup(){const{pageTitle:e}=v();return{pageTitle:e}}};function k(e,o,n,t,c,l){const a=_("page-table-preview-down-example"),s=_("aloha-page");return A(),d(s,{"page-title":e.pageTitle},{body:r(()=>[u(a)]),_:1},8,["page-title"])}const K=p(U,[["render",k]]);export{K as default};
