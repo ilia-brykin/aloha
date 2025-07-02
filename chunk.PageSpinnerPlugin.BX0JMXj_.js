f172097bcd890c7db26d9cfd9b1ebd720ef73477
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-07-02 20:27:18 +0000

    deploy: fbb21a8b43a5a33ebfa3c7d604da741071d857b5

---
+++ chunk.PageSpinnerPlugin.BX0JMXj_.js
@@ -0,0 +1,35 @@
+import{A as i}from"./chunk.AlohaHighlightjs.DOgKZA3M.js";import{A as _,_ as l}from"./bundle.index.DSjSLIKP.js";import{W as m,Z as e,Y as s,e as a,V as c}from"./chunk.vendor.B8i9Zb03.js";import"./chunk.vendor-lodash.BGdgYQ9t.js";import{A as u}from"./chunk.APageTabTitle.z816C-NB.js";import"./chunk.translations-ar.DEWt73y8.js";import"./chunk.translations-de.CN5eAU-4.js";import"./chunk.translations-en.CPCEcTAP.js";import"./chunk.translations-es.BKTK5PUv.js";import"./chunk.translations-fr.CslimZmU.js";import"./chunk.translations-hr.CiJ7_Y2m.js";import"./chunk.translations-it.H45F8miC.js";import"./chunk.translations-ru.o-dOzdYz.js";function d(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ASpinnerPlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(ASpinnerPlugin, {
+  propsDefault: {
+    class: "my_custom_spinner",
+    safeHtml: "<strong>Loading...</strong>",
+    size: "large",
+    tag: "div",
+  },
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <a-spinner></a-spinner>
+  </div>
+</template>
+
+<script>
+import { 
+  ASpinner,
+} from "aloha-vue";
+
+export default {
+  name: "SpinnerExample",
+  components: {
+    ASpinner,
+  },
+};
+<\/script>`}}const g={name:"PageSpinnerPlugin",components:{AlohaHighlightjs:i,APageTabTitle:u,ATranslation:_},setup(){const{connectionCode:o,useCode:t}=d();return{connectionCode:o,useCode:t}}};function N(o,t,P,A,f,E){const r=a("a-page-tab-title"),n=a("a-translation"),p=a("aloha-highlightjs");return c(),m("div",null,[e(r,{title:"_PAGE_PLUGIN_SPINNER_PAGE_TITLE_"}),e(n,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_SPINNER_H1_"}),e(n,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_INTRODUCTION_"}),t[0]||(t[0]=s("hr",{class:"a_my_5"},null,-1)),e(n,{tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_CONNECTION_"}),e(p,{code:o.connectionCode,language:"javascript"},null,8,["code"]),e(n,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_CONNECTION_NOTES_"}),t[1]||(t[1]=s("hr",{class:"a_my_5"},null,-1)),e(n,{tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_EXAMPLES_"}),e(p,{code:o.useCode,language:"html"},null,8,["code"])])}const $=l(g,[["render",N]]);export{$ as default};
---
+++ chunk.PageSvg.DAEubb7L.js
@@ -0,0 +1,11 @@
+import{x as a,_ as l}from"./bundle.index.DSjSLIKP.js";import{W as r,Y as o,w as n,bo as p,V as s}from"./chunk.vendor.B8i9Zb03.js";import"./chunk.vendor-lodash.BGdgYQ9t.js";import"./chunk.translations-ar.DEWt73y8.js";import"./chunk.translations-de.CN5eAU-4.js";import"./chunk.translations-en.CPCEcTAP.js";import"./chunk.translations-es.BKTK5PUv.js";import"./chunk.translations-fr.CslimZmU.js";import"./chunk.translations-hr.CiJ7_Y2m.js";import"./chunk.translations-it.H45F8miC.js";import"./chunk.translations-ru.o-dOzdYz.js";const m={name:"PageSvg",directives:{ASafeHtml:a},setup(){return{linkHtml:"<a href='https://www.google.com/' target='_blank'>https://www.google.com/</a>",pathD:`M3,1 
+    A3,1 0 0,0 3,2
+    A0.8,1 0 0,1 3,4 
+    A3,1 0 0,0 0,5 
+    V15 
+    A5,1 0 1,0 10,15 
+    V5 
+    A3,1 0 0,0 7,4 
+    A0.8,1 0 0,1 7,2 
+    A3,1 0 0,0 7,1
+    A2.4,1 0 0,0 3,1`}}},c={width:"300",height:"300",xmlns:"http://www.w3.org/2000/svg",version:"1.1",viewBox:"0 0 18 18",style:{border:"1px solid red"}},d=["d"];function h(t,e,w,g,v,_){const i=p("a-safe-html");return s(),r("div",null,[e[0]||(e[0]=o("h1",null,"SVG",-1)),(s(),r("svg",c,[o("path",{d:t.pathD},null,8,d)])),n(o("div",null,null,512),[[i,t.linkHtml]])])}const b=l(m,[["render",h]]);export{b as default};
