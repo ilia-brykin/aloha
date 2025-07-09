c95cf314cbcf2e660f507b7c03e13c590edb4119
NKDante <NKDante@users.noreply.github.com>
2025-07-09 07:33:21 +0000

    deploy: 2efaa0b78ed4ce50c99e55d3043fbdeac1629162

---
+++ chunk.PageFilterUpperCase.9wyzuH69.js
@@ -0,0 +1,46 @@
+import{A as U,a as x}from"./chunk.AlohaExample.BxtiM1ms.js";import{P as h,a as P,b as B}from"./chunk.PageFilterTest.BVZ7HSf4.js";import{P as E}from"./chunk.PageFilterImportCompositionApi.kxs7zVjb.js";import{aB as a,_ as s,A as F}from"./bundle.index.7CLnsWkq.js";import{V as i,e as t,W as l,aT as O,_ as r}from"./chunk.vendor.D2EiI1BL.js";import"./chunk.vendor-lodash.FjHQi8Go.js";import"./chunk.APageTabTitle.CJpqfc5k.js";import"./chunk.AlohaHighlightjs.B_1iuPG1.js";import"./chunk.translations-ar.DEWt73y8.js";import"./chunk.translations-de.CN5eAU-4.js";import"./chunk.translations-en.CPCEcTAP.js";import"./chunk.translations-es.BKTK5PUv.js";import"./chunk.translations-fr.CslimZmU.js";import"./chunk.translations-hr.CiJ7_Y2m.js";import"./chunk.translations-it.H45F8miC.js";import"./chunk.translations-ru.o-dOzdYz.js";function R(){return{codeJs:`filterUpperCase("--foo-bar--");
+// ${a("--foo-bar--")}
+
+filterUpperCase("fooBar");
+// ${a("fooBar")}
+
+filterUpperCase("__foo_bar__");
+// ${a("__foo_bar__")}`}}const T={name:"PageFilterUpperCaseExample",components:{AlohaExample:U},setup(){const{codeJs:e}=R();return{codeJs:e}}};function $(e,o,p,c,_,m){const n=t("aloha-example");return l(),i(n,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const b=s(T,[["render",$]]);function L(){return{argumentsText:[{value:"value",types:["String"],text:"_PAGE_FILTER_UPPER_CASE_ARGUMENTS_VALUE_"}]}}function I(){return{pageTitle:"filterUpperCase"}}const S=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterUpperCase from "../filterUpperCase";
+
+describe("filterUpperCase", () => {
+  it("should convert a string to lowercase", () => {
+    expect(filterUpperCase("--Foo-Bar--")).toBe("--FOO-BAR--");
+    expect(filterUpperCase("fooBar")).toBe("FOOBAR");
+    expect(filterUpperCase("__FOO_BAR__")).toBe("__FOO_BAR__");
+  });
+
+  it("should handle empty strings", () => {
+    expect(filterUpperCase("")).toBe("");
+  });
+
+  it("should handle strings with no alphabetic characters", () => {
+    expect(filterUpperCase("12345")).toBe("12345");
+    expect(filterUpperCase("!@#$%")).toBe("!@#$%");
+  });
+
+  it("should handle mixed case strings", () => {
+    expect(filterUpperCase("FoObAr")).toBe("FOOBAR");
+    expect(filterUpperCase("HELLOworld")).toBe("HELLOWORLD");
+  });
+
+  it("should handle strings with spaces", () => {
+    expect(filterUpperCase("Hello World")).toBe("HELLO WORLD");
+    expect(filterUpperCase(" foo bar ")).toBe(" FOO BAR ");
+  });
+
+  it("should return the same string if already in uppercase", () => {
+    expect(filterUpperCase("ALREADY UPPERCASE")).toBe("ALREADY UPPERCASE");
+  });
+});
+`,v={name:"PageFilterUpperCase",components:{AlohaPage:x,ATranslation:F,PageFilterArguments:B,PageFilterImportCompositionApi:E,PageFilterImportFunction:P,PageFilterTest:h,PageFilterUpperCaseExample:b},setup(){const{pageTitle:e}=I(),{argumentsText:o}=L();return{argumentsText:o,pageTitle:e,test:S}}};function y(e,o,p,c,_,m){const n=t("a-translation"),f=t("page-filter-import-function"),u=t("page-filter-import-composition-api"),g=t("page-filter-arguments"),d=t("page-filter-upper-case-example"),C=t("page-filter-test"),A=t("aloha-page");return l(),i(A,{"page-title":e.pageTitle},{body:O(()=>[r(n,{tag:"p",html:"_PAGE_FILTER_UPPER_CASE_DESCRIPTION_"}),r(f,{"function-name":"filterUpperCase","type-import":"filters"}),r(u,{"function-name":"filterUpperCase"}),r(g,{"arguments-text":e.argumentsText,"function-description":"filterUpperCase(value)"},null,8,["arguments-text"]),r(d),r(C,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const X=s(v,[["render",y]]);export{X as default};
