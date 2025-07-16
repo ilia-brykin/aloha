137589dff17aaafbd7adb4b6b23e2cdfee66d94e
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-07-16 13:45:46 +0000

    deploy: 57e3892ce2e6ccd9aeb60a0822766206b5f0042b

---
+++ chunk.PageLoading.CQp1-C8i.js
@@ -0,0 +1,237 @@
+import{A,a as S}from"./chunk.AlohaExample.1Qft8V8u.js";import{A as $}from"./chunk.AlohaTableProps.DfbqGKg0.js";import{A as N}from"./chunk.AlohaTableTranslate.DcuYdfAr.js";import{U as m,_,g as E,A as R}from"./bundle.index.gdMk4HS7.js";import{V as r,aT as n,e as t,W as u,Z as l,_ as i,d as h}from"./chunk.vendor.BxCHGHAU.js";import"./chunk.vendor-lodash.BMPrua2s.js";import"./chunk.APageTabTitle.CPYWAEBF.js";import"./chunk.AlohaHighlightjs.B3F2oL8_.js";import"./chunk.ATable.DW1e7yBh.js";import"./chunk.translations-ar.D8MQqUIX.js";import"./chunk.translations-de.DF2fp9fz.js";import"./chunk.translations-en.Cabsq1fv.js";import"./chunk.translations-es.CTtyunpM.js";import"./chunk.translations-fr.tpgV9obb.js";import"./chunk.translations-hr.v7YKJpdX.js";import"./chunk.translations-it.Dqw3KhB-.js";import"./chunk.translations-ru.Cz76XEJm.js";function x(){return{codeHtml:`<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    align="left"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    align="center"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    align="right"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>`}}function C(){return{codeJs:`import { 
+  ALoading,
+} from "aloha-vue";
+    
+export default {
+  name: "PageLoadingAlign",
+  components: {
+    ALoading,
+  },
+};`}}const H={name:"PageLoadingAlign",components:{ALoading:m,AlohaExample:A},setup(){const{codeHtml:o}=x(),{codeJs:a}=C();return{codeHtml:o,codeJs:a}}},D={class:"a_position_relative"},G={class:"a_position_relative"},J={class:"a_position_relative"};function z(o,a,d,p,c,g){const e=t("a-loading"),s=t("aloha-example");return u(),r(s,{"code-html":o.codeHtml,"code-js":o.codeJs,header:"_A_CLOAK_GROUP_ALIGN_HEADER_",description:"_A_CLOAK_GROUP_ALIGN_DESCRIPTION_",props:["align"]},{default:n(()=>[l("div",D,[i(e,{"is-loading":!0,align:"left"},{default:n(()=>a[0]||(a[0]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",G,[i(e,{"is-loading":!0,align:"center"},{default:n(()=>a[1]||(a[1]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",J,[i(e,{"is-loading":!0,align:"right"},{default:n(()=>a[2]||(a[2]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})])]),_:1},8,["code-html","code-js"])}const b=_(H,[["render",z]]);function K(){return{codeHtml:`<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>`}}function y(){return{codeJs:`import { 
+  ALoading,
+} from "aloha-vue";
+    
+export default {
+  name: "PageLoadingBasic",
+  components: {
+    ALoading,
+  },
+};`}}const j={name:"PageLoadingBasic",components:{ALoading:m,AlohaExample:A},setup(){const{codeHtml:o}=K(),{codeJs:a}=y();return{codeHtml:o,codeJs:a}}},q={class:"a_position_relative"};function B(o,a,d,p,c,g){const e=t("a-loading"),s=t("aloha-example");return u(),r(s,{"code-html":o.codeHtml,"code-js":o.codeJs,header:"_A_BASIC_USAGE_",props:["is-loading"]},{default:n(()=>[l("div",q,[i(e,{"is-loading":!0},{default:n(()=>a[0]||(a[0]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})])]),_:1},8,["code-html","code-js"])}const U=_(j,[["render",B]]);function X(){return{codeHtml:`<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="1"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="2"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="3"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="4"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="5"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="6"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    size="7"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>`}}function M(){return{codeJs:`import { 
+  ALoading,
+} from "aloha-vue";
+    
+export default {
+  name: "PageLoadingSizes",
+  components: {
+    ALoading,
+  },
+};`}}const Z={name:"PageLoadingSizes",components:{ALoading:m,AlohaExample:A},setup(){const{codeHtml:o}=X(),{codeJs:a}=M();return{codeHtml:o,codeJs:a}}},V={class:"a_position_relative"},k={class:"a_position_relative"},w={class:"a_position_relative"},W={class:"a_position_relative"},F={class:"a_position_relative"},Y={class:"a_position_relative"},Q={class:"a_position_relative"};function ll(o,a,d,p,c,g){const e=t("a-loading"),s=t("aloha-example");return u(),r(s,{"code-html":o.codeHtml,"code-js":o.codeJs,header:"_A_CLOAK_GROUP_SIZES_HEADER_",description:"_A_CLOAK_GROUP_SIZES_DESCRIPTION_",props:["size"]},{default:n(()=>[l("div",V,[i(e,{"is-loading":!0,size:"1"},{default:n(()=>a[0]||(a[0]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",k,[i(e,{"is-loading":!0,size:"2"},{default:n(()=>a[1]||(a[1]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",w,[i(e,{"is-loading":!0,size:"3"},{default:n(()=>a[2]||(a[2]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",W,[i(e,{"is-loading":!0,size:"4"},{default:n(()=>a[3]||(a[3]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",F,[i(e,{"is-loading":!0,size:"5"},{default:n(()=>a[4]||(a[4]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",Y,[i(e,{"is-loading":!0,size:"6"},{default:n(()=>a[5]||(a[5]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",Q,[i(e,{"is-loading":!0,size:"7"},{default:n(()=>a[6]||(a[6]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})])]),_:1},8,["code-html","code-js"])}const al=_(Z,[["render",ll]]);function ol(){return{codeHtml:`<div
+  class="a_position_relative"
+>
+  <a-loading
+    :extra="{ number: 1 }"
+    :is-loading="true"
+    text="_PAGE_LOADING_TEXT_{{number}}_"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>`}}function il(){return{codeJs:`import { 
+  ALoading,
+} from "aloha-vue";
+    
+export default {
+  name: "PageLoadingText",
+  components: {
+    ALoading,
+  },
+};`}}const el={name:"PageLoadingText",components:{ALoading:m,AlohaExample:A},setup(){const{codeHtml:o}=ol(),{codeJs:a}=il();return{codeHtml:o,codeJs:a}}},nl={class:"a_position_relative"};function tl(o,a,d,p,c,g){const e=t("a-loading"),s=t("aloha-example");return u(),r(s,{"code-html":o.codeHtml,"code-js":o.codeJs,header:"_A_CLOAK_GROUP_TEXT_HEADER_",description:"_A_CLOAK_GROUP_TEXT_DESCRIPTION_",props:["text","extra"]},{default:n(()=>[l("div",nl,[i(e,{extra:{number:1},"is-loading":!0,text:"_PAGE_LOADING_TEXT_{{number}}_"},{default:n(()=>a[0]||(a[0]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})])]),_:1},8,["code-html","code-js"])}const sl=_(el,[["render",tl]]);function dl(){return{codeHtml:`<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    text-align="left"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>
+<div
+  class="a_position_relative"
+>
+  <a-loading
+    :is-loading="true"
+    text-align="right"
+  >
+    <ul>
+      <li>Aloha 1</li>
+      <li>Aloha 2</li>
+      <li>Aloha 3</li>
+    </ul>
+  </a-loading>
+</div>`}}function _l(){return{codeJs:`import { 
+  ALoading,
+} from "aloha-vue";
+    
+export default {
+  name: "PageLoadingTextAlign",
+  components: {
+    ALoading,
+  },
+};`}}const rl={name:"PageLoadingTextAlign",components:{ALoading:m,AlohaExample:A},setup(){const{codeHtml:o}=dl(),{codeJs:a}=_l();return{codeHtml:o,codeJs:a}}},ul={class:"a_position_relative"},pl={class:"a_position_relative"};function cl(o,a,d,p,c,g){const e=t("a-loading"),s=t("aloha-example");return u(),r(s,{"code-html":o.codeHtml,"code-js":o.codeJs,header:"_A_CLOAK_GROUP_TEXT_ALIGN_HEADER_",description:"_A_CLOAK_GROUP_TEXT_ALIGN_DESCRIPTION_",props:["text-align"]},{default:n(()=>[l("div",ul,[i(e,{"is-loading":!0,"text-align":"left"},{default:n(()=>a[0]||(a[0]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})]),l("div",pl,[i(e,{"is-loading":!0,"text-align":"right"},{default:n(()=>a[1]||(a[1]=[l("ul",null,[l("li",null,"aloha 1"),l("li",null,"aloha 2"),l("li",null,"aloha 3")],-1)])),_:1})])]),_:1},8,["code-html","code-js"])}const gl=_(rl,[["render",cl]]);function Al(){const o=h(()=>E({placeholder:"_A_LOADING_COMPONENT_NAME_"}));return{pageTitle:h(()=>`ALoading${o.value?` (${o.value})`:""}`)}}function ml(){return{dataProps:[{name:"align",description:"_A_CLOAK_PROPS_ALIGN_DESCRIPTION_",type:"String",default:"center",required:!1},{name:"always-translate",description:"_A_TRANSLATION_PROPS_ALWAYS_TRANSLATE_DESCRIPTION_",type:"Boolean",default:void 0,required:!1},{name:"extra",description:"_A_GLOBAL_PROPS_EXTRA_DESCRIPTION_",type:"Object",default:void 0,required:!1},{name:"is-loading",description:"_A_LOADING_PROPS_IS_LOADING_DESCRIPTION_",type:"Boolean",default:void 0,required:!1},{name:"size",description:"_A_CLOAK_PROPS_SIZE_DESCRIPTION_",type:"String / Number",default:"6",required:!1},{name:"spinner-class",description:"_A_CLOAK_PROPS_SPINNER_CLASS_DESCRIPTION_",type:"String / Number",default:void 0,required:!1},{name:"spinner-safe-html",description:"_A_SPINNER_PROPS_SAFE_HTML_DESCRIPTION_",type:"String",default:"",required:!1},{name:"spinner-tag",description:"_A_SPINNER_PROPS_TAG_DESCRIPTION_",type:"String",default:"span",required:!1},{name:"tag",description:"_A_CLOAK_PROPS_TAG_DESCRIPTION_",type:"String",default:"div",required:!1},{name:"text",description:"_A_CLOAK_PROPS_TEXT_DESCRIPTION_",type:"String",default:"_A_CLOAK_LOADING_",required:!1},{name:"text-align",description:"_A_CLOAK_PROPS_TEXT_ALIGN_DESCRIPTION_",type:"String",default:"right",required:!1}]}}function hl(){return{dataTranslate:["_A_LOADING_LOADING_"]}}const fl={name:"PageLoading",components:{AlohaPage:S,AlohaTableProps:$,AlohaTableTranslate:N,ATranslation:R,PageLoadingAlign:b,PageLoadingBasic:U,PageLoadingSizes:al,PageLoadingText:sl,PageLoadingTextAlign:gl},setup(){const{pageTitle:o}=Al(),{dataProps:a}=ml(),{dataTranslate:d}=hl();return{dataProps:a,dataTranslate:d,pageTitle:o}}};function Pl(o,a,d,p,c,g){const e=t("a-translation"),s=t("page-loading-basic"),f=t("page-loading-align"),P=t("page-loading-text-align"),v=t("page-loading-sizes"),I=t("page-loading-text"),L=t("aloha-table-props"),O=t("aloha-table-translate"),T=t("aloha-page");return u(),r(T,{"page-title":o.pageTitle},{body:n(()=>[i(e,{tag:"p",html:"_A_LOADING_COMPONENT_DESCRIPTION_"}),i(s),i(f),i(P),i(v),i(I),i(L,{data:o.dataProps},null,8,["data"]),i(O,{data:o.dataTranslate},null,8,["data"])]),_:1},8,["page-title"])}const bl=_(fl,[["render",Pl]]);export{bl as default};
