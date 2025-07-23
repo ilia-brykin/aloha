0c41ad240ea7fa08bea4330f9aa2f11d6b695d04
Ilia <brykin.ilia@gmail.com>
2025-07-23 20:00:30 +0200

    Add `PageSliderErrors` example with composition API usage, update `PageSlider` component to include new example, and enhance documentation with related APIs, pug template, JavaScript content, and SCSS styles for error handling support.

--- docs/src/views/Components/Form/PageSlider/PageSlider.js
+++ docs/src/views/Components/Form/PageSlider/PageSlider.js
@@ -1,6 +1,7 @@
 import AlohaPage from "../../../../global/components/AlohaPage/AlohaPage.vue";
 import PageSliderBasic from "./PageSliderBasic/PageSliderBasic.vue";
 import PageSliderChange from "./PageSliderChange/PageSliderChange.vue";
+import PageSliderErrors from "./PageSliderErrors/PageSliderErrors.vue";
 import PageSliderHelpText from "./PageSliderHelpText/PageSliderHelpText.vue";
 import {
   ATranslation,
@@ -15,6 +16,7 @@ export default {
     ATranslation,
     PageSliderBasic,
     PageSliderChange,
+    PageSliderErrors,
     PageSliderHelpText,
   },
   setup() {
