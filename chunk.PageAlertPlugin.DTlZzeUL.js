10f8c63bdd088d4c8b258ce41e7cbb69b9661dd8
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-09-14 16:18:35 +0000

    deploy: fcd1072c8e4bde7139f1eafb9d22e7e4b5f687f5

---
+++ chunk.PageAlertPlugin.DTlZzeUL.js
@@ -0,0 +1,51 @@
+import{A as r}from"./chunk.AlohaHighlightjs.BZkAdeSp.js";import{A as p,_ as c}from"./bundle.index.CGNfdx9Q.js";import{Y as m,_ as e,Z as n,e as a,W as _}from"./chunk.vendor.BAPLGb06.js";import"./chunk.vendor-lodash.CStjNiQe.js";import{A as d}from"./chunk.APageTabTitle.BqNhWTka.js";import"./chunk.translations-ar.BiLIUuBX.js";import"./chunk.translations-de.CRLsg1il.js";import"./chunk.translations-en.CWj0a7ej.js";import"./chunk.translations-es.D-4cj_p_.js";import"./chunk.translations-fr.LwkSu7L_.js";import"./chunk.translations-hr.G7gNG4Fu.js";import"./chunk.translations-it.BilXvddc.js";import"./chunk.translations-ru.XOcS94ap.js";function f(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  AAlertPlugin,
+} from "aloha-vue";
+import CheckLg from "aloha-svg/dist/js/bootstrap/CheckLg";
+
+const app = createApp(App);
+
+app.use(AAlertPlugin, {
+  propsDefault: {
+    closable: true,
+    textClose: "Close",
+    type: "info",
+    alertClass: "custom-alert-class",
+  },
+  icons: {
+    info: CheckLg,
+  },
+});
+
+app.mount("#app");`,pluginCode:`import CheckCircleFill from "aloha-svg/dist/js/bootstrap/CheckCircleFill";
+import ExclamationCircleFill from "aloha-svg/dist/js/bootstrap/ExclamationCircleFill";
+import InfoCircleFill from "aloha-svg/dist/js/bootstrap/InfoCircleFill";
+import XCircleFill from "aloha-svg/dist/js/bootstrap/XCircleFill";
+
+export const alertPluginOptions = ref({
+  propsDefault: {
+    alertClass: undefined,
+    alertContentClass: undefined,
+    btnCloseAttributes: {},
+    closable: false,
+    extra: undefined,
+    html: undefined,
+    icon: undefined,
+    iconClass: undefined,
+    isVisible: false,
+    removeAlertOnClose: false,
+    safeHtml: undefined,
+    showIcon: false,
+    text: undefined,
+    textClose: "_ALERT_CLOSE_",
+    type: "danger",
+  },
+  icons: {
+    success: CheckCircleFill,
+    danger: XCircleFill,
+    info: InfoCircleFill,
+    warning: ExclamationCircleFill,
+  },
+});`}}const u={name:"PageAlertPlugin",components:{AlohaHighlightjs:r,APageTabTitle:d,ATranslation:p},setup(){const{connectionCode:t,pluginCode:o}=f();return{connectionCode:t,pluginCode:o}}};function C(t,o,g,A,h,E){const s=a("a-page-tab-title"),l=a("a-translation"),i=a("aloha-highlightjs");return _(),m("div",null,[e(s,{title:"_PAGE_PLUGIN_ALERT_PAGE_TITLE_"}),e(l,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_ALERT_H1_"}),e(l,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_INTRODUCTION_"}),o[0]||(o[0]=n("hr",{class:"a_my_5"},null,-1)),e(l,{tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_CONNECTION_"}),e(i,{code:t.connectionCode,language:"javascript"},null,8,["code"]),o[1]||(o[1]=n("hr",{class:"a_my_5"},null,-1)),e(l,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_NOTES_"}),e(i,{code:t.pluginCode,language:"javascript"},null,8,["code"]),e(l,{tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_NOTES_AFTER_"})])}const k=c(u,[["render",C]]);export{k as default};
---
+++ chunk.PageFilterEscapeHTML.DL-jdXjM.js
@@ -0,0 +1,50 @@
+import{A as H,a as h}from"./chunk.AlohaExample.BCCG0H9g.js";import{P as T,a as A,b as P}from"./chunk.PageFilterTest.DkKSks_4.js";import{aH as a,_ as i,A as S}from"./bundle.index.CGNfdx9Q.js";import{V as r,e,W as l,aT as F,_ as n}from"./chunk.vendor.BAPLGb06.js";import"./chunk.vendor-lodash.CStjNiQe.js";import{P as O}from"./chunk.PageFilterImportCompositionApi.BCN3U4P0.js";import"./chunk.APageTabTitle.BqNhWTka.js";import"./chunk.AlohaHighlightjs.BZkAdeSp.js";import"./chunk.translations-ar.BiLIUuBX.js";import"./chunk.translations-de.CRLsg1il.js";import"./chunk.translations-en.CWj0a7ej.js";import"./chunk.translations-es.D-4cj_p_.js";import"./chunk.translations-fr.LwkSu7L_.js";import"./chunk.translations-hr.G7gNG4Fu.js";import"./chunk.translations-it.BilXvddc.js";import"./chunk.translations-ru.XOcS94ap.js";function L(){return{codeJs:`filterEscapeHtml("<script>alert('XSS Attack')<\/script>");
+// ${a('<script>alert("XSS Attack")<\/script>')}
+
+filterEscapeHtml("Hello, World!");
+// ${a("Hello, World!")}`}}const $={name:"PageFilterEscapeHTMLExample",components:{AlohaExample:H},setup(){const{codeJs:t}=L();return{codeJs:t}}};function I(t,o,s,c,u,m){const p=e("aloha-example");return l(),r(p,{"code-js":t.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const M=i($,[["render",I]]);function B(){return{argumentsText:[{value:"value",types:["String"],text:"_PAGE_FILTER_ESCAPE_HTML_ARGUMENTS_VALUE_"}]}}function C(){return{pageTitle:"filterEscapeHtml"}}const k=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterEscapeHtml from "../filterEscapeHtml";
+
+describe("filterEscapeHtml", () => {
+  it("should escape HTML special characters", () => {
+    const input = "<script>alert(\\"XSS Attack\\")<\/script>";
+    const expectedOutput = "&lt;script&gt;alert(\\"XSS Attack\\")&lt;/script&gt;";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should return the same string if no special characters", () => {
+    const input = "Hello, World!";
+    const expectedOutput = "Hello, World!";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should return an empty string if input is empty", () => {
+    const input = "";
+    const expectedOutput = "";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should escape only special HTML characters and leave the rest", () => {
+    const input = "Hello <strong>world</strong>";
+    const expectedOutput = "Hello &lt;strong&gt;world&lt;/strong&gt;";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should handle special characters like &, \\", '", () => {
+    const input = "Fish & Chips \\"Delicious\\"";
+    const expectedOutput = "Fish &amp; Chips \\"Delicious\\"";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+
+  it("should escape HTML list tags", () => {
+    const input = "<ul><li>test</li></ul>";
+    const expectedOutput = "&lt;ul&gt;&lt;li&gt;test&lt;/li&gt;&lt;/ul&gt;";
+    expect(filterEscapeHtml(input)).toBe(expectedOutput);
+  });
+});
+`,y={name:"PageFilterEscapeHTML",components:{AlohaPage:h,ATranslation:S,PageFilterArguments:P,PageFilterEscapeHTMLExample:M,PageFilterImportCompositionApi:O,PageFilterImportFunction:A,PageFilterTest:T},setup(){const{pageTitle:t}=C(),{argumentsText:o}=B();return{argumentsText:o,pageTitle:t,test:k}}};function b(t,o,s,c,u,m){const p=e("a-translation"),_=e("page-filter-import-function"),g=e("page-filter-import-composition-api"),f=e("page-filter-arguments"),d=e("page-filter-escape-h-t-m-l-example"),E=e("page-filter-test"),x=e("aloha-page");return l(),r(x,{"page-title":t.pageTitle},{body:F(()=>[n(p,{tag:"p",html:"_PAGE_FILTER_ESCAPE_HTML_DESCRIPTION_"}),n(_,{"function-name":"filterEscapeHtml","type-import":"filters"}),n(g,{"function-name":"filterEscapeHtml"}),n(f,{"arguments-text":t.argumentsText,"function-description":"filterEscapeHtml(value)"},null,8,["arguments-text"]),n(d),n(E,{test:t.test},null,8,["test"])]),_:1},8,["page-title"])}const Y=i(y,[["render",b]]);export{Y as default};
