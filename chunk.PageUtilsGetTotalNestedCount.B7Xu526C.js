de249461a1bf212c18f156576fbea6d53b143f6b
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-09-21 19:35:15 +0000

    deploy: 97090baa7c545ba5163994ab5ef5d027e1859c78

---
+++ chunk.PageUtilsGetTotalNestedCount.B7Xu526C.js
@@ -0,0 +1,67 @@
+import{A as y,a as C}from"./chunk.AlohaExample.DbQW_HEv.js";import{P as f,a as N,b as A}from"./chunk.PageFilterTest.D94-fNbf.js";import{S as o,_ as s,A as x}from"./bundle.index.BLKvx0Zd.js";import{V as i,e as t,W as d,aT as E,_ as n}from"./chunk.vendor.DmOTRqbf.js";import"./chunk.vendor-lodash.DsZvX3G1.js";import"./chunk.APageTabTitle.2f7Txk1e.js";import"./chunk.AlohaHighlightjs.8HRkfNnH.js";import"./chunk.translations-ar.BiLIUuBX.js";import"./chunk.translations-de.Cy_u5T72.js";import"./chunk.translations-en.CWj0a7ej.js";import"./chunk.translations-es.D-4cj_p_.js";import"./chunk.translations-fr.LwkSu7L_.js";import"./chunk.translations-hr.G7gNG4Fu.js";import"./chunk.translations-it.BilXvddc.js";import"./chunk.translations-ru.XOcS94ap.js";function w(){return{codeJs:`getTotalNestedCount({ array: ["hello", "Aloha"], keyChildren: "" });
+// ${o({array:["hello","Aloha"],keyChildren:""})}
+
+getTotalNestedCount({ array: [], keyChildren: "" });
+// ${o({array:[],keyChildren:""})}
+
+getTotalNestedCount({ array: [{ children: ["hello", "world"] }, "test"], keyChildren: "children" });
+// ${o({array:[{children:["hello","world"]},"test"],keyChildren:"children"})}
+
+getTotalNestedCount({ array: [{ aloha: [{ aloha: ["hello", "world"] }, { aloha: ["hello1", "world1"] }] }, "test"], keyChildren: "aloha" });
+// ${o({array:[{aloha:[{aloha:["hello","world"]},{aloha:["hello1","world1"]}]},"test"],keyChildren:"aloha"})}`}}const k={name:"PageUtilsGetTotalNestedCountExample",components:{AlohaExample:y},setup(){const{codeJs:e}=w();return{codeJs:e}}};function P(e,a,l,c,p,u){const r=t("aloha-example");return d(),i(r,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const $=s(k,[["render",P]]);function G(){return{argumentsText:[{value:"array",types:["Array"],text:"_PAGE_UTILS_GET_TOTAL_NESTED_COUNT_ARGUMENTS_ARRAY_"},{value:"keyChildren",types:["String"],text:"_PAGE_UTILS_GET_TOTAL_NESTED_COUNT_ARGUMENTS_KEY_CHILDREN_"}]}}function S(){return{pageTitle:"getTotalNestedCount"}}const U=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import {
+  getTotalNestedCount,
+} from "../utils";
+
+describe("getTotalNestedCount function", () => {
+  it("should correctly count elements in a flat array without nested children", () => {
+    const array = ["hello", "world"];
+
+    expect(getTotalNestedCount({ array, keyChildren: "test" })).toBe(2);
+  });
+
+  it("should accurately count elements in a mixed array with and without specified nested children", () => {
+    const array = [{ children: ["hello", "world"] }, "test"];
+
+    expect(getTotalNestedCount({ array, keyChildren: "children" })).toBe(4);
+    expect(getTotalNestedCount({ array, keyChildren: "" })).toBe(2);
+  });
+
+  it("should handle deeply nested structures and count all elements correctly", () => {
+    const array = [
+      {
+        children: [
+          { children: ["hello", "world"] },
+          { children: ["hello", "world"] },
+        ],
+      },
+      {
+        children: [
+          { children: ["hello", "world"] },
+          { children: ["hello", "world"] },
+          { children: ["hello", "world"] },
+        ],
+      },
+      {
+        children: [
+          { children: ["hello", "world"] },
+        ],
+      },
+    ];
+
+    expect(getTotalNestedCount({ array, keyChildren: "children" })).toBe(21);
+  });
+
+  it("should return zero for an empty array, indicating no elements to count", () => {
+    const array = [];
+
+    expect(getTotalNestedCount({ array, keyChildren: "test" })).toBe(0);
+  });
+});
+
+`,I={name:"PageUtilsGetTotalNestedCount",components:{AlohaPage:C,ATranslation:x,PageFilterArguments:A,PageFilterImportFunction:N,PageFilterTest:f,PageUtilsGetTotalNestedCountExample:$},setup(){const{pageTitle:e}=S(),{argumentsText:a}=G();return{argumentsText:a,pageTitle:e,test:U}}};function B(e,a,l,c,p,u){const r=t("a-translation"),h=t("page-filter-import-function"),_=t("page-filter-arguments"),m=t("page-utils-get-total-nested-count-example"),g=t("page-filter-test"),T=t("aloha-page");return d(),i(T,{"page-title":e.pageTitle},{body:E(()=>[n(r,{tag:"p",html:"_PAGE_UTILS_GET_TOTAL_NESTED_COUNT_DESCRIPTION_"}),n(h,{"function-name":"getTotalNestedCount","type-import":"utils"}),n(_,{"arguments-text":e.argumentsText,"function-description":"getTotalNestedCount({ array, keyChildren })"},null,8,["arguments-text"]),n(m),n(g,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const W=s(I,[["render",B]]);export{W as default};
