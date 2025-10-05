0678c99e520af50416ffc280ff76801a098150cd
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-10-05 18:01:36 +0000

    deploy: c4ab17170d6ef7ae2ed841e425bee20244c3b63c

--- chunk.APageTabTitle.C2hE3IJI.js
+++
@@ -1,1 +0,0 @@
-import{v as t}from"./bundle.index.9cZuhpuk.js";const a={name:"APageTabTitle",props:{title:{type:String,required:!0},extra:{type:Object,required:!1,default:void 0}},setup(e){t(e)},render(){return""}};export{a as A};
---
+++ chunk.PageMobilePlugin.DPMtS24J.js
@@ -0,0 +1,36 @@
+import{A as l}from"./chunk.AlohaHighlightjs.8HRkfNnH.js";import{A as r,_}from"./bundle.index.Dj2r79dL.js";import{Y as m,_ as e,Z as s,e as i,W as c}from"./chunk.vendor.DmOTRqbf.js";import"./chunk.vendor-lodash.DsZvX3G1.js";import{A as u}from"./chunk.APageTabTitle.BRewUv2K.js";import"./chunk.translations-ar.BiLIUuBX.js";import"./chunk.translations-de.Cy_u5T72.js";import"./chunk.translations-en.CWj0a7ej.js";import"./chunk.translations-es.D-4cj_p_.js";import"./chunk.translations-fr.LwkSu7L_.js";import"./chunk.translations-hr.G7gNG4Fu.js";import"./chunk.translations-it.BilXvddc.js";import"./chunk.translations-ru.XOcS94ap.js";function d(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  AMobilePlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(AMobilePlugin, {
+  breakpoint: 1023,
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <p v-if="isMobileWidth">You are using a mobile device.</p>
+    <p v-else>You are using a desktop device.</p>
+  </div>
+</template>
+
+<script>
+import {
+  AMobileAPI,
+} from "aloha-vue";
+
+export default {
+  setup() {
+    const {
+      isMobileWidth,
+    } = AMobileAPI;
+    
+    return {
+      isMobileWidth,
+    };
+  },
+};
+<\/script>`}}const g={name:"PageMobilePlugin",components:{AlohaHighlightjs:l,APageTabTitle:u,ATranslation:r},setup(){const{connectionCode:t,useCode:o}=d();return{connectionCode:t,useCode:o}}};function A(t,o,P,h,f,E){const p=i("a-page-tab-title"),a=i("a-translation"),n=i("aloha-highlightjs");return c(),m("div",null,[e(p,{title:"_PAGE_PLUGIN_MOBILE_PAGE_TITLE_"}),e(a,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_MOBILE_H1_"}),e(a,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_INTRODUCTION_"}),o[0]||(o[0]=s("hr",{class:"a_my_5"},null,-1)),e(a,{tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_CONNECTION_"}),e(n,{code:t.connectionCode,language:"javascript"},null,8,["code"]),e(a,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_CONNECTION_NOTES_"}),o[1]||(o[1]=s("hr",{class:"a_my_5"},null,-1)),e(a,{tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_EXAMPLES_"}),e(n,{code:t.useCode,language:"html"},null,8,["code"])])}const k=_(g,[["render",A]]);export{k as default};
