715acee0485d101f66a37ae23e06dfec59e0bb59
ilia-brykin <ilia-brykin@users.noreply.github.com>
2025-10-13 08:19:40 +0000

    deploy: e82046204f498bb5ecddbd3147f0acd85ad8c5d9

---
+++ chunk.APageTabTitle.dkaNV-gN.js
@@ -0,0 +1,1 @@
+import{v as t}from"./bundle.index.CP-9f_BY.js";const a={name:"APageTabTitle",props:{title:{type:String,required:!0},extra:{type:Object,required:!1,default:void 0}},setup(e){t(e)},render(){return""}};export{a as A};
