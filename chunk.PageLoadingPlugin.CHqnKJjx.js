7aa324fb350907b5af0954667cf808ba5f2fda8f
zdilarr <zdilarr@users.noreply.github.com>
2025-11-12 12:59:40 +0000

    deploy: f26c49067a046414fe4b50e7cabf5d02314cea3b

---
+++ chunk.PageLoadingPlugin.CHqnKJjx.js
@@ -0,0 +1,38 @@
+import{A as l}from"./chunk.AlohaHighlightjs.dFk8X2lo.js";import{A as r,_}from"./bundle.index.B0BAYlA6.js";import{Y as m,_ as o,Z as s,e as n,W as c}from"./chunk.vendor.D7UxhUfn.js";import"./chunk.vendor-lodash.BbaK34MN.js";import{A as d}from"./chunk.APageTabTitle.BUYjg9PU.js";import"./chunk.translations-ar.BXWv90e-.js";import"./chunk.translations-de.JSU2GZ-A.js";import"./chunk.translations-en.CviwQZZj.js";import"./chunk.translations-es.D2AGpadl.js";import"./chunk.translations-fr.DQLlT60U.js";import"./chunk.translations-hr.EjwgafbT.js";import"./chunk.translations-it.fnRPaBQW.js";import"./chunk.translations-ru.DfHWC88K.js";function g(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ALoadingPlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(ALoadingPlugin, {
+  propsDefault: {
+    align: "left",
+    size: 8,
+    tag: "section",
+    text: "Loading, please wait...",
+    textAlign: "center",
+  },
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <a-loading :is-loading="true">
+      <div>Aloha</div>
+    </a-loading>
+  </div>
+</template>
+
+<script>
+import { 
+  ALoading,
+} from "aloha-vue";
+
+export default {
+  name: "LoadingExample",
+  components: {
+    ALoading,
+  },
+};
+<\/script>`}}const u={name:"PageLoadingPlugin",components:{AlohaHighlightjs:l,APageTabTitle:d,ATranslation:r},setup(){const{connectionCode:a,useCode:t}=g();return{connectionCode:a,useCode:t}}};function A(a,t,N,L,P,f){const p=n("a-page-tab-title"),e=n("a-translation"),i=n("aloha-highlightjs");return c(),m("div",null,[o(p,{title:"_PAGE_PLUGIN_LOADING_PAGE_TITLE_"}),o(e,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_LOADING_H1_"}),o(e,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_INTRODUCTION_"}),t[0]||(t[0]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_CONNECTION_"}),o(i,{code:a.connectionCode,language:"javascript"},null,8,["code"]),o(e,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_CONNECTION_NOTES_"}),t[1]||(t[1]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_EXAMPLES_"}),o(i,{code:a.useCode,language:"html"},null,8,["code"])])}const $=_(u,[["render",A]]);export{$ as default};
---
+++ chunk.PageQuickStart.XIazqUlT.js
@@ -0,0 +1,36 @@
+import{A as m}from"./chunk.AlohaHighlightjs.dFk8X2lo.js";import{A as _,_ as u}from"./bundle.index.B0BAYlA6.js";import{Y as c,_ as t,Z as i,e as l,W as d}from"./chunk.vendor.D7UxhUfn.js";import"./chunk.vendor-lodash.BbaK34MN.js";import{A}from"./chunk.APageTabTitle.BUYjg9PU.js";import"./chunk.translations-ar.BXWv90e-.js";import"./chunk.translations-de.JSU2GZ-A.js";import"./chunk.translations-en.CviwQZZj.js";import"./chunk.translations-es.D2AGpadl.js";import"./chunk.translations-fr.DQLlT60U.js";import"./chunk.translations-hr.EjwgafbT.js";import"./chunk.translations-it.fnRPaBQW.js";import"./chunk.translations-ru.DfHWC88K.js";function g(){return{installCode:`# Using npm
+$ npm install aloha-vue
+
+# Using yarn
+$ yarn add aloha-vue
+
+# Using pnpm
+$ pnpm add aloha-vue`,stylesCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import "aloha-vue/dist/aloha-vue.css";
+
+const app = createApp(App);
+app.mount("#app");`,useInHtmlCode:`<template>
+  <div>
+    <a-button @click="onClick">Click me</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      alert("The button is pressed!");
+    },
+  },
+};
+<\/script>`,useInMainCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import { AButton } from "aloha-vue";
+import "aloha-vue/dist/aloha-vue.css";
+
+const app = createApp(App);
+
+app.component("AButton", AButton);
+
+app.mount("#app");`}}const h={name:"PageQuickStart",components:{AlohaHighlightjs:m,APageTabTitle:A,ATranslation:_},setup(){const{installCode:a,stylesCode:e,useInHtmlCode:s,useInMainCode:p}=g();return{installCode:a,stylesCode:e,useInHtmlCode:s,useInMainCode:p}}};function C(a,e,s,p,v,f){const r=l("a-page-tab-title"),o=l("a-translation"),n=l("aloha-highlightjs");return d(),c("div",null,[t(r,{title:"_PAGE_QUICK_START_H1_"}),t(o,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_QUICK_START_H1_"}),t(o,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_QUICK_START_INSTALL_"}),t(n,{code:a.installCode,language:"markdown"},null,8,["code"]),e[0]||(e[0]=i("hr",{class:"a_my_5"},null,-1)),t(o,{tag:"div","safe-html":"_PAGE_QUICK_START_STYLES_"}),t(n,{code:a.stylesCode,language:"javascript"},null,8,["code"]),e[1]||(e[1]=i("hr",{class:"a_my_5"},null,-1)),t(o,{tag:"div","safe-html":"_PAGE_QUICK_START_USE_"}),t(n,{code:a.useInMainCode,language:"javascript"},null,8,["code"]),t(o,{class:"a_my_3",tag:"p","safe-html":"_PAGE_QUICK_START_IN_TEMPLATE_"}),t(n,{code:a.useInHtmlCode,language:"html"},null,8,["code"])])}const G=u(h,[["render",C]]);export{G as default};
