937bb97e3b9a42de19d6aa5d0f0f88d3538b1bd0
Ilia <brykin.ilia@gmail.com>
2026-01-18 19:33:58 +0100

    Add `breakpoints` constant for global use in documentation

---
+++ docs/src/global/const/breakpoints.const.js
@@ -0,0 +1,30 @@
+export const breakpoints = [
+  {
+    name: "mobile",
+    value: "<=768px",
+  },
+  {
+    name: "tablet",
+    value: "769-1023px",
+  },
+  {
+    name: "touch",
+    value: "<=1023px",
+  },
+  {
+    name: "desktop",
+    value: "1024-1215px",
+  },
+  {
+    name: "widescreen",
+    value: "1216-1407px",
+  },
+  {
+    name: "fullhd",
+    value: "1408-1599px",
+  },
+  {
+    name: "large",
+    value: ">=1600px",
+  },
+];
