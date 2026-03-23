a7a744ccee0cfb32a6eda9bd6e0aa1e822abd8c1
Ilia.Brykin <ilia.brykin@prounix.de>
2026-03-23 14:53:10 +0100

    Refactor: Update `PageDatepickerPlaceholderDefault` layout with responsive column structure and adjust `HtmlAPI` and Pug examples accordingly.

--- docs/src/views/Components/Form/PageDatepicker/PageDatepickerPlaceholderDefault/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/Form/PageDatepicker/PageDatepickerPlaceholderDefault/compositionAPI/HtmlAPI.js
@@ -1,43 +1,63 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-datepicker
-  v-model="modelDate"
-  label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_DATE_"
-  type="date"
-></a-datepicker>
-<div>modelDate: {{ modelDate }}</div>
-<a-datepicker
-  v-model="modelTimeHour"
-  class="a_mt_3"
-  label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_TIME_HOUR_"
-  time-precision="hour"
-  type="time"
-></a-datepicker>
-<div>modelTimeHour: {{ modelTimeHour }}</div>
-<a-datepicker
-  v-model="modelTimeMinute"
-  class="a_mt_3"
-  label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_TIME_MINUTE_"
-  time-precision="minute"
-  type="time"
-></a-datepicker>
-<div>modelTimeMinute: {{ modelTimeMinute }}</div>
-<a-datepicker
-  v-model="modelDatetimeSecond"
-  class="a_mt_3"
-  label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_DATETIME_SECOND_"
-  time-precision="second"
-  type="datetime"
-></a-datepicker>
-<div>modelDatetimeSecond: {{ modelDatetimeSecond }}</div>
-<a-datepicker
-  v-model="modelOverride"
-  :placeholders-default="placeholdersCustom"
-  class="a_mt_3"
-  label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_OVERRIDE_"
-  time-precision="minute"
-  type="datetime"
-></a-datepicker>
-<div>modelOverride: {{ modelOverride }}</div>`;
+  const codeHtml = `<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="modelDate"
+      label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_DATE_"
+      type="date"
+    ></a-datepicker>
+    <div>modelDate: {{ modelDate }}</div>
+  </div>
+</div>
+<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="modelTimeHour"
+      class="a_mt_3"
+      label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_TIME_HOUR_"
+      time-precision="hour"
+      type="time"
+    ></a-datepicker>
+    <div>modelTimeHour: {{ modelTimeHour }}</div>
+  </div>
+</div>
+<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="modelTimeMinute"
+      class="a_mt_3"
+      label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_TIME_MINUTE_"
+      time-precision="minute"
+      type="time"
+    ></a-datepicker>
+    <div>modelTimeMinute: {{ modelTimeMinute }}</div>
+  </div>
+</div>
+<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="modelDatetimeSecond"
+      class="a_mt_3"
+      label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_DATETIME_SECOND_"
+      time-precision="second"
+      type="datetime"
+    ></a-datepicker>
+    <div>modelDatetimeSecond: {{ modelDatetimeSecond }}</div>
+  </div>
+</div>
+<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="modelOverride"
+      :placeholders-default="placeholdersCustom"
+      class="a_mt_3"
+      label="_PAGE_DATEPICKER_LBL_PLACEHOLDER_DEFAULT_OVERRIDE_"
+      time-precision="minute"
+      type="datetime"
+    ></a-datepicker>
+    <div>modelOverride: {{ modelOverride }}</div>
+  </div>
+</div>`;
 
   return {
     codeHtml,
