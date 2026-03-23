651e6bcbc9e5497b0072c304d836546198664025
Ilia.Brykin <ilia.brykin@prounix.de>
2026-03-23 14:50:06 +0100

    Feature: Update `PageDatepickerLabelDescription` with responsive column layout and add `isLabelFloat` support in example and `HtmlAPI`.

--- docs/src/views/Components/Form/PageDatepicker/PageDatepickerLabelDescription/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/Form/PageDatepicker/PageDatepickerLabelDescription/compositionAPI/HtmlAPI.js
@@ -1,9 +1,15 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-datepicker
-  v-model="model"
-  label="_PAGE_DATEPICKER_LBL_TYPE_DATE_"
-  label-description="_PAGE_DATEPICKER_TXT_ALOHA_"
-></a-datepicker>`;
+  const codeHtml = `<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="model"
+      :is-label-float="false"
+      label-description="_PAGE_DATEPICKER_TXT_ALOHA_"
+      label="_PAGE_DATEPICKER_LBL_TYPE_DATE_"
+    ></a-datepicker>
+  </div>
+</div>
+`;
 
   return {
     codeHtml,
