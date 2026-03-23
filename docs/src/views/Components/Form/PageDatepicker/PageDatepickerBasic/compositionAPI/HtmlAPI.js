495731ef5da036c78d89a4350cfd70943460027d
Ilia.Brykin <ilia.brykin@prounix.de>
2026-03-23 14:47:25 +0100

    Refactor: Update `PageDatepickerBasic` layout with responsive columns and adjust `HtmlAPI` example code accordingly.

--- docs/src/views/Components/Form/PageDatepicker/PageDatepickerBasic/compositionAPI/HtmlAPI.js
+++ docs/src/views/Components/Form/PageDatepicker/PageDatepickerBasic/compositionAPI/HtmlAPI.js
@@ -1,19 +1,31 @@
 export default function HtmlAPI() {
-  const codeHtml = `<a-datepicker
-  v-model="model1"
-  label="_PAGE_DATEPICKER_LBL_INPUT_1_"
-></a-datepicker>
-<div>model1: {{ model1 }}</div>
-<a-datepicker
-  v-model="model2"
-  label="_PAGE_DATEPICKER_LBL_INPUT_2_"
-></a-datepicker>
-<div>model2: {{ model2 }}</div>
-<a-datepicker
-  v-model="model3"
-  label="_PAGE_DATEPICKER_LBL_INPUT_3_"
-></a-datepicker>
-<div>model3: {{ model3 }}</div>`;
+  const codeHtml = `<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="model1"
+      label="_PAGE_DATEPICKER_LBL_INPUT_1_"
+    ></a-datepicker>
+    <div>model1: {{ model1 }}</div>
+  </div>
+</div>
+<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="model2"
+      label="_PAGE_DATEPICKER_LBL_INPUT_2_"
+    ></a-datepicker>
+    <div>model2: {{ model2 }}</div>
+  </div>
+</div>
+<div class="a_columns a_columns_count_12">
+  <div class="a_column a_column_6 a_columns_count_12_touch">
+    <a-datepicker
+      v-model="model3"
+      label="_PAGE_DATEPICKER_LBL_INPUT_3_"
+    ></a-datepicker>
+    <div>model3: {{ model3 }}</div>
+  </div>
+</div>`;
 
   return {
     codeHtml,
