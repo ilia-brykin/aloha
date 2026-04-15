fc1e72cb17a7ab3d43b9a800f59bf1ce65b2fb21
ilia-brykin <ilia-brykin@users.noreply.github.com>
2026-04-15 13:45:47 +0000

    deploy: 38dbad40e91a68293ffe7129f40376a295cf65cf

---
+++ chunk.PageFiltersRight.C73QfBln.js
@@ -0,0 +1,50 @@
+import{A as T,a as f}from"./chunk.AlohaExample.CbbfJlzx.js";import{A as P}from"./chunk.AlohaTableProps.CnbT3-wg.js";import{_ as E,g as R,A as S}from"./bundle.index.CJ_hZnrp.js";import{r as d,W as c,V as u,aT as m,e as n,Z as I,_ as s,d as p}from"./chunk.vendor.7qYoMuVo.js";import"./chunk.vendor-lodash.CDw4fzB6.js";import{A as O}from"./chunk.AFilters.Dc5-kJVZ.js";import"./chunk.APageTabTitle.BsiomPxd.js";import"./chunk.AlohaHighlightjs.CqftpDCI.js";import"./chunk.ATable.k8ZKZFof.js";import"./chunk.translations-ar.CY7HI9me.js";import"./chunk.translations-de.C8-mViTt.js";import"./chunk.translations-en.Biv6OsUn.js";import"./chunk.translations-es.DJOC65Nl.js";import"./chunk.translations-fr.DbQjo2b0.js";import"./chunk.translations-hr.BcLs9lS1.js";import"./chunk.translations-it.ejoXsLio.js";import"./chunk.translations-ru.5jlOOxzJ.js";function L(){return{codeHtml:`<a-filters
+  :filters="filters"
+  v-model:applied-model="appliedModel"
+  v-model:unapplied-model="unappliedModel"
+>
+</a-filters>`}}function C(){return{codeJs:`import {
+  ref,
+} from "vue";
+
+import { 
+  AFilters,
+} from "aloha-vue";
+    
+export default {
+  name: "PageFiltersExample",
+  components: {
+    AFilters,
+  },
+  setup() {
+    const filters = [
+      {
+        type: "text",
+        id: "search",
+        label: "_A_PAGE_FILTER_SEARCH_",
+        main: true,
+      },
+      {
+        type: "text",
+        id: "aloha",
+        label: "_A_PAGE_FILTER_TEXT_",
+        alwaysVisible: true,
+      },
+      {
+        type: "date",
+        id: "date",
+        label: "_A_PAGE_FILTER_DATE_",
+        alwaysVisible: true,
+      },
+    ];
+
+    const appliedModel = ref({});
+    const unappliedModel = ref({});
+    
+    return {
+      appliedModel,
+      filters,
+      unappliedModel,
+    };
+  },
+};`}}const g={name:"PageFiltersRightExample",components:{AFilters:O,AlohaExample:T},setup(){const{codeHtml:e}=L(),{codeJs:t}=C(),a=[{type:"text",id:"search",label:"_A_PAGE_FILTER_SEARCH_",main:!0},{type:"text",id:"aloha",label:"_A_PAGE_FILTER_TEXT_",alwaysVisible:!0},{type:"date",id:"date",label:"_A_PAGE_FILTER_DATE_",alwaysVisible:!0}],o=d({}),i=d({});return{appliedModel:o,codeHtml:e,codeJs:t,filters:a,unappliedModel:i}}};function N(e,t,a,o,i,A){const _=n("a-filters"),r=n("aloha-example");return c(),u(r,{"code-html":e.codeHtml,"code-js":e.codeJs,header:"_A_PAGE_FILTERS_HEADER_",description:"_A_PAGE_FILTERS_DESCRIPTION_",props:["view"]},{default:m(()=>[I("div",null,[s(_,{filters:e.filters,view:"right","applied-model":e.appliedModel,"onUpdate:appliedModel":t[0]||(t[0]=l=>e.appliedModel=l),"unapplied-model":e.unappliedModel,"onUpdate:unappliedModel":t[1]||(t[1]=l=>e.unappliedModel=l)},null,8,["filters","applied-model","unapplied-model"])])]),_:1},8,["code-html","code-js"])}const y=E(g,[["render",N]]);function h(){return{dataEvents:[{name:"close",description:"_A_ALERT_EVENTS_CLOSE_DESCRIPTION_",type:"Function"}]}}function D(){const e=p(()=>R({placeholder:"_A_FILTERS_RIGHT_COMPONENT_NAME_"}));return{pageTitle:p(()=>`AFilters${e.value?` (${e.value})`:""}`)}}function F(){return{dataProps:[{name:"alert-class",description:"_A_ALERT_PROPS_ALERT_CLASS_DESCRIPTION_",type:"String / Object",default:void 0,required:!1},{name:"alert-content-class",description:"_A_ALERT_PROPS_ALERT_CONTENT_CLASS_DESCRIPTION_",type:"String / Object",default:void 0,required:!1},{name:"btn-close-attributes",description:"_A_ALERT_PROPS_BTN_CLOSE_ATTRIBUTES_DESCRIPTION_",type:"Object",default:"{}",required:!1},{name:"closable",description:"_A_ALERT_PROPS_CLOSABLE_DESCRIPTION_",type:"Boolean",default:!1,required:!1},{name:"show-icon",description:"_A_ALERT_PROPS_HAS_ICON_DESCRIPTION_",type:"Boolean",default:!1,required:!1},{name:"html",description:"_A_ALERT_PROPS_HTML_DESCRIPTION_",type:"String",default:void 0,required:!1},{name:"icon",description:"_A_ALERT_PROPS_ICON_DESCRIPTION_",type:"String",default:void 0,required:!1},{name:"icon-class",description:"_A_ALERT_PROPS_ICON_CLASS_DESCRIPTION_",type:"String",default:void 0,required:!1},{name:"is-visible",description:"_A_ALERT_PROPS_IS_VISIBLE_DESCRIPTION_",type:"Boolean",default:!1,required:!1},{name:"removeAlertOnClose",description:"_A_ALERT_PROPS_REMOVE_ALERT_ON_CLOSE_DESCRIPTION_",type:"Boolean",default:!1,required:!1},{name:"safe-html",description:"_A_ALERT_PROPS_SAFE_HTML_DESCRIPTION_",type:"String",default:void 0,required:!1},{name:"text",description:"_A_ALERT_PROPS_TEXT_DESCRIPTION_",type:"String",default:void 0,required:!1},{name:"text-close",description:"_A_ALERT_PROPS_TEXT_CLOSE_DESCRIPTION_",type:"String",default:"_ALERT_CLOSE_",required:!1},{name:"type",description:"_A_ALERT_PROPS_TYPE_DESCRIPTION_",type:"String",default:"danger",required:!1}]}}function M(){return{dataSlots:[{name:"default",description:"_A_ALERT_SLOTS_DEFAULT_DESCRIPTION_"}]}}const b={name:"PageFiltersRight",components:{AlohaPage:f,AlohaTableProps:P,ATranslation:S,PageFiltersRightExample:y},setup(){const{pageTitle:e}=D(),{dataProps:t}=F(),{dataSlots:a}=M(),{dataEvents:o}=h();return{dataEvents:o,dataProps:t,dataSlots:a,pageTitle:e}}};function v(e,t,a,o,i,A){const _=n("a-translation"),r=n("page-filters-right-example"),l=n("aloha-page");return c(),u(l,{"page-title":e.pageTitle},{body:m(()=>[s(_,{tag:"p",html:"_A_FILTERS_RIGHT_COMPONENT_DESCRIPTION_"}),s(r)]),_:1},8,["page-title"])}const K=E(b,[["render",v]]);export{K as default};
