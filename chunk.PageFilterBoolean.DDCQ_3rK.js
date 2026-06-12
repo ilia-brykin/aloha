891f40bf3cd39abedd37df3bc1ab4df911fdc35e
ilia-brykin <ilia-brykin@users.noreply.github.com>
2026-06-12 12:31:20 +0000

    deploy: 2163a34241ebcf91c96410cdc08debd710a18db5

---
+++ chunk.PageFilterBoolean.DDCQ_3rK.js
@@ -0,0 +1,40 @@
+import{A as c,a as P}from"./chunk.AlohaExample.B0C4zaqf.js";import{P as T,a as x,b as h}from"./chunk.PageFilterTest.CrczwyNR.js";import{aI as t,_ as s,A as F}from"./bundle.index.DFiUMV_p.js";import{d as m,Y as u,W as i,e as o,aU as I,$ as l}from"./chunk.vendor.Mky9wq52.js";import"./chunk.vendor-lodash.CCcdKCCS.js";import{P as J}from"./chunk.PageFilterImportCompositionApi.DH0K2DeG.js";import{f as L}from"./chunk.filterBoolean.test.9cX79x5q.js";import"./chunk.APageTabTitle.CnIelNsa.js";import"./chunk.AlohaHighlightjs.Cs2B8kR8.js";import"./chunk.translations-ar.DStCCijA.js";import"./chunk.translations-de.BVoiITyD.js";import"./chunk.translations-en.DLCxJau6.js";import"./chunk.translations-es.BYz5u4IQ.js";import"./chunk.translations-fr.B9Nr_iLj.js";import"./chunk.translations-hr.DhPJ8JEd.js";import"./chunk.translations-it.D6f5eU5y.js";import"./chunk.translations-ru.BWP_gszP.js";function O(){return{codeJs:m(()=>`filterBoolean(true);
+// ${t(!0)}
+filterBoolean(false);
+// ${t(!1)}
+
+filterBoolean(1);
+// ${t(1)}
+filterBoolean(0);
+// ${t(0)}
+
+filterBoolean("aloha");
+// ${t("aloha")}
+filterBoolean("");
+// ${t("")}`)}}const S={name:"PageFilterBooleanExample",components:{AlohaExample:c},setup(){const{codeJs:e}=O();return{codeJs:e}}};function U(e,n,r,p,_,f){const a=o("aloha-example");return u(),i(a,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const v=s(S,[["render",U]]);function b(){return{codeJs:m(()=>`filterBoolean(true, { trueValue: "1", falseValue: "0" });
+// ${t(!0,{trueValue:"1",falseValue:"0"})}
+filterBoolean(false, { trueValue: "1", falseValue: "0" });
+// ${t(!1,{trueValue:"1",falseValue:"0"})}
+
+filterBoolean(1, { trueValue: "true", falseValue: "false" });
+// ${t(1,{trueValue:"true",falseValue:"false"})}
+filterBoolean(0, { trueValue: "true", falseValue: "false" });
+// ${t(0,{trueValue:"true",falseValue:"false"})}`)}}const R={name:"PageFilterBooleanTrueFalseValues",components:{AlohaExample:c},setup(){const{codeJs:e}=b();return{codeJs:e}}};function G(e,n,r,p,_,f){const a=o("aloha-example");return u(),i(a,{"code-js":e.codeJs,header:"trueValue, falseValue","is-code-visible-default":!0},null,8,["code-js"])}const y=s(R,[["render",G]]);function j(){return{codeJs:m(()=>`filterBoolean(null, { useNil: true });
+// ${t(null,{useNil:!0})}
+filterBoolean(null, { useNil: false });
+// ${t(null,{useNil:!1})}
+
+filterBoolean(undefined, { useNil: true });
+// ${t(void 0,{useNil:!0})}
+filterBoolean(undefined, { useNil: false });
+// ${t(void 0,{useNil:!1})}
+
+filterBoolean("null", { useNil: true });
+// ${t("null",{useNil:!0})}
+filterBoolean("null", { useNil: false });
+// ${t("null",{useNil:!1})}
+
+filterBoolean("undefined", { useNil: true });
+// ${t("undefined",{useNil:!0})}
+filterBoolean("undefined", { useNil: false });
+// ${t("undefined",{useNil:!1})}`)}}const C={name:"PageFilterBooleanUseNil",components:{AlohaExample:c},setup(){const{codeJs:e}=j();return{codeJs:e}}};function M(e,n,r,p,_,f){const a=o("aloha-example");return u(),i(a,{"code-js":e.codeJs,header:"useNil","is-code-visible-default":!0},null,8,["code-js"])}const Y=s(C,[["render",M]]);function k(){return{argumentsText:[{value:"value",types:["Any"],text:"_PAGE_FILTER_BOOLEAN_ARGUMENTS_VALUE_"},{value:'[trueValue="_YES_"]',types:["String"],text:"_PAGE_FILTER_BOOLEAN_ARGUMENTS_TRUE_VALUE_"},{value:'[falseValue="_NO_"]',types:["String"],text:"_PAGE_FILTER_BOOLEAN_ARGUMENTS_FALSE_VALUE_"},{value:"[useNil=true]",types:["Boolean"],text:"_PAGE_FILTER_BOOLEAN_ARGUMENTS_USE_NIL_"}]}}function w(){return{pageTitle:"filterBoolean"}}const D={name:"PageFilterBoolean",components:{AlohaPage:P,ATranslation:F,PageFilterArguments:h,PageFilterBooleanExample:v,PageFilterBooleanTrueFalseValues:y,PageFilterBooleanUseNil:Y,PageFilterImportCompositionApi:J,PageFilterImportFunction:x,PageFilterTest:T},setup(){const{pageTitle:e}=w(),{argumentsText:n}=k();return{argumentsText:n,pageTitle:e,test:L}}};function W(e,n,r,p,_,f){const a=o("a-translation"),d=o("page-filter-import-function"),g=o("page-filter-import-composition-api"),$=o("page-filter-arguments"),B=o("page-filter-boolean-example"),A=o("page-filter-boolean-true-false-values"),N=o("page-filter-boolean-use-nil"),E=o("page-filter-test"),V=o("aloha-page");return u(),i(V,{"page-title":e.pageTitle},{body:I(()=>[l(a,{tag:"p",html:"_PAGE_FILTER_BOOLEAN_DESCRIPTION_"}),l(d,{"function-name":"filterBoolean","type-import":"filters"}),l(g,{"function-name":"filterBoolean"}),l($,{"function-description":'filterBoolean(value, { [trueValue="_YES_"], [falseValue="_NO_"], [useNil=true] })',"arguments-text":e.argumentsText},null,8,["arguments-text"]),l(B),l(A),l(N),l(E,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const pe=s(D,[["render",W]]);export{pe as default};
