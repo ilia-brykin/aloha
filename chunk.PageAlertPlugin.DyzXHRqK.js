fbb1637544294e1f4af819dbcd24812f3f58359b
ilia-brykin <ilia-brykin@users.noreply.github.com>
2026-06-29 09:04:51 +0000

    deploy: 4057a3b1237809122ad503bb0c3ec7342ab9915a

---
+++ chunk.PageAlertPlugin.DyzXHRqK.js
@@ -0,0 +1,51 @@
+import{A as r}from"./chunk.AlohaHighlightjs.BK8yZFUI.js";import{A as p,_ as c}from"./bundle.index.ChCjitYk.js";import{Y as m,Z as _,$ as e,_ as n,f as a}from"./chunk.vendor.BAXy2grE.js";import"./chunk.vendor-lodash.CCzoES9g.js";import{A as d}from"./chunk.APageTabTitle.CiRmUmgI.js";import"./chunk.translations-ar.B2OoBNnE.js";import"./chunk.translations-de.UgGe9j4v.js";import"./chunk.translations-en.0stqcWf2.js";import"./chunk.translations-es.BwgNL0Sc.js";import"./chunk.translations-fr.C2YYR8Jb.js";import"./chunk.translations-hr.B965uVG9.js";import"./chunk.translations-it.qkDQbydY.js";import"./chunk.translations-ru.x6UIBrxX.js";function f(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  AAlertPlugin,
+} from "aloha-vue";
+import CheckLg from "aloha-svg/dist/js/bootstrap/CheckLg";
+
+const app = createApp(App);
+
+app.use(AAlertPlugin, {
+  propsDefault: {
+    closable: true,
+    textClose: "Close",
+    type: "info",
+    alertClass: "custom-alert-class",
+  },
+  icons: {
+    info: CheckLg,
+  },
+});
+
+app.mount("#app");`,pluginCode:`import CheckCircleFill from "aloha-svg/dist/js/bootstrap/CheckCircleFill";
+import ExclamationCircleFill from "aloha-svg/dist/js/bootstrap/ExclamationCircleFill";
+import InfoCircleFill from "aloha-svg/dist/js/bootstrap/InfoCircleFill";
+import XCircleFill from "aloha-svg/dist/js/bootstrap/XCircleFill";
+
+export const alertPluginOptions = ref({
+  propsDefault: {
+    alertClass: undefined,
+    alertContentClass: undefined,
+    btnCloseAttributes: {},
+    closable: false,
+    extra: undefined,
+    html: undefined,
+    icon: undefined,
+    iconClass: undefined,
+    isVisible: false,
+    removeAlertOnClose: false,
+    safeHtml: undefined,
+    showIcon: false,
+    text: undefined,
+    textClose: "_ALERT_CLOSE_",
+    type: "danger",
+  },
+  icons: {
+    success: CheckCircleFill,
+    danger: XCircleFill,
+    info: InfoCircleFill,
+    warning: ExclamationCircleFill,
+  },
+});`}}const u={name:"PageAlertPlugin",components:{AlohaHighlightjs:r,APageTabTitle:d,ATranslation:p},setup(){const{connectionCode:t,pluginCode:o}=f();return{connectionCode:t,pluginCode:o}}};function C(t,o,g,A,h,E){const s=a("a-page-tab-title"),l=a("a-translation"),i=a("aloha-highlightjs");return m(),_("div",null,[e(s,{title:"_PAGE_PLUGIN_ALERT_PAGE_TITLE_"}),e(l,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_ALERT_H1_"}),e(l,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_INTRODUCTION_"}),o[0]||(o[0]=n("hr",{class:"a_my_5"},null,-1)),e(l,{tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_CONNECTION_"}),e(i,{code:t.connectionCode,language:"javascript"},null,8,["code"]),o[1]||(o[1]=n("hr",{class:"a_my_5"},null,-1)),e(l,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_NOTES_"}),e(i,{code:t.pluginCode,language:"javascript"},null,8,["code"]),e(l,{tag:"div","safe-html":"_PAGE_PLUGIN_ALERT_NOTES_AFTER_"})])}const k=c(u,[["render",C]]);export{k as default};
