6a04bdc5a93d0566cfbea63c915129ca3809f630
NKDante <NKDante@users.noreply.github.com>
2026-06-30 12:40:07 +0000

    deploy: 0ce0f4e8d56231183d873404d10aef35c489d4ed

---
+++ chunk.PageMobilePlugin.Z_rFZYvs.js
@@ -0,0 +1,36 @@
+import{A as l}from"./chunk.AlohaHighlightjs.BK8yZFUI.js";import{A as r,_}from"./bundle.index.C9QAjbob.js";import{Y as m,Z as c,$ as e,_ as s,f as i}from"./chunk.vendor.BAXy2grE.js";import"./chunk.vendor-lodash.CCzoES9g.js";import{A as u}from"./chunk.APageTabTitle.BMoqp8DE.js";import"./chunk.translations-ar.CE_3yjUF.js";import"./chunk.translations-de.R0YWY91X.js";import"./chunk.translations-en.DQ0Z316F.js";import"./chunk.translations-es.XN0jVhL-.js";import"./chunk.translations-fr.D5Z6d0-l.js";import"./chunk.translations-hr.BRYjTgZh.js";import"./chunk.translations-it.D6SsMt4V.js";import"./chunk.translations-ru.BfK09XlZ.js";function d(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  AMobilePlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(AMobilePlugin, {
+  breakpoint: 1023,
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <p v-if="isMobileWidth">You are using a mobile device.</p>
+    <p v-else>You are using a desktop device.</p>
+  </div>
+</template>
+
+<script>
+import {
+  AMobileAPI,
+} from "aloha-vue";
+
+export default {
+  setup() {
+    const {
+      isMobileWidth,
+    } = AMobileAPI;
+    
+    return {
+      isMobileWidth,
+    };
+  },
+};
+<\/script>`}}const g={name:"PageMobilePlugin",components:{AlohaHighlightjs:l,APageTabTitle:u,ATranslation:r},setup(){const{connectionCode:t,useCode:o}=d();return{connectionCode:t,useCode:o}}};function A(t,o,P,h,f,E){const p=i("a-page-tab-title"),a=i("a-translation"),n=i("aloha-highlightjs");return m(),c("div",null,[e(p,{title:"_PAGE_PLUGIN_MOBILE_PAGE_TITLE_"}),e(a,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_MOBILE_H1_"}),e(a,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_INTRODUCTION_"}),o[0]||(o[0]=s("hr",{class:"a_my_5"},null,-1)),e(a,{tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_CONNECTION_"}),e(n,{code:t.connectionCode,language:"javascript"},null,8,["code"]),e(a,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_CONNECTION_NOTES_"}),o[1]||(o[1]=s("hr",{class:"a_my_5"},null,-1)),e(a,{tag:"div","safe-html":"_PAGE_PLUGIN_MOBILE_EXAMPLES_"}),e(n,{code:t.useCode,language:"html"},null,8,["code"])])}const j=_(g,[["render",A]]);export{j as default};
---
+++ chunk.PageSpinnerPlugin.C9YUqfQr.js
@@ -0,0 +1,35 @@
+import{A as i}from"./chunk.AlohaHighlightjs.BK8yZFUI.js";import{A as _,_ as l}from"./bundle.index.C9QAjbob.js";import{Y as m,Z as c,$ as e,_ as s,f as a}from"./chunk.vendor.BAXy2grE.js";import"./chunk.vendor-lodash.CCzoES9g.js";import{A as u}from"./chunk.APageTabTitle.BMoqp8DE.js";import"./chunk.translations-ar.CE_3yjUF.js";import"./chunk.translations-de.R0YWY91X.js";import"./chunk.translations-en.DQ0Z316F.js";import"./chunk.translations-es.XN0jVhL-.js";import"./chunk.translations-fr.D5Z6d0-l.js";import"./chunk.translations-hr.BRYjTgZh.js";import"./chunk.translations-it.D6SsMt4V.js";import"./chunk.translations-ru.BfK09XlZ.js";function d(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ASpinnerPlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(ASpinnerPlugin, {
+  propsDefault: {
+    class: "my_custom_spinner",
+    safeHtml: "<strong>Loading...</strong>",
+    size: "large",
+    tag: "div",
+  },
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <a-spinner></a-spinner>
+  </div>
+</template>
+
+<script>
+import { 
+  ASpinner,
+} from "aloha-vue";
+
+export default {
+  name: "SpinnerExample",
+  components: {
+    ASpinner,
+  },
+};
+<\/script>`}}const g={name:"PageSpinnerPlugin",components:{AlohaHighlightjs:i,APageTabTitle:u,ATranslation:_},setup(){const{connectionCode:o,useCode:t}=d();return{connectionCode:o,useCode:t}}};function N(o,t,P,A,f,E){const r=a("a-page-tab-title"),n=a("a-translation"),p=a("aloha-highlightjs");return m(),c("div",null,[e(r,{title:"_PAGE_PLUGIN_SPINNER_PAGE_TITLE_"}),e(n,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_SPINNER_H1_"}),e(n,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_INTRODUCTION_"}),t[0]||(t[0]=s("hr",{class:"a_my_5"},null,-1)),e(n,{tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_CONNECTION_"}),e(p,{code:o.connectionCode,language:"javascript"},null,8,["code"]),e(n,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_CONNECTION_NOTES_"}),t[1]||(t[1]=s("hr",{class:"a_my_5"},null,-1)),e(n,{tag:"div","safe-html":"_PAGE_PLUGIN_SPINNER_EXAMPLES_"}),e(p,{code:o.useCode,language:"html"},null,8,["code"])])}const j=l(g,[["render",N]]);export{j as default};
