249f030872e6727001806f52fe91fb0c84aac756
ilia-brykin <ilia-brykin@users.noreply.github.com>
2026-07-14 12:14:50 +0000

    deploy: 72e940942995085849d4011d7e548c70b275963b

---
+++ chunk.PageFilterCurrency.BnNHayz2.js
@@ -0,0 +1,44 @@
+import{A as f,a as E}from"./chunk.AlohaExample.CbnehaWD.js";import{P as A,a as G,b as T}from"./chunk.PageFilterTest.B1r5HTSZ.js";import{a_ as r,_ as a,A as F}from"./bundle.index.BaMXkEpL.js";import{Y as c,W as u,f as t,aU as I,$ as o}from"./chunk.vendor.CWr53wb5.js";import"./chunk.vendor-lodash.CWoM99Se.js";import{P as R}from"./chunk.PageFilterImportCompositionApi.C2VxqF78.js";import"./chunk.APageTabTitle.CNIeguXb.js";import"./chunk.AlohaHighlightjs.Cyb5M4zM.js";import"./chunk.translations-ar.DRto1jDw.js";import"./chunk.translations-de.CEjcANRo.js";import"./chunk.translations-en.BjlQK_tj.js";import"./chunk.translations-es.BcLAUo0L.js";import"./chunk.translations-fr.DINmPQrz.js";import"./chunk.translations-hr.BU9Fw-5f.js";import"./chunk.translations-it.Cu9SxZzh.js";import"./chunk.translations-ru.Cs_zBe4-.js";function J(){return{codeJs:`filterCurrency(1000222.55, { digitGrouping: false });
+// ${r(100022255e-2,{digitGrouping:!1})}
+
+filterCurrency(1000222.55, { digitGrouping: true });
+// ${r(100022255e-2,{digitGrouping:!0})}`}}const b={name:"PageFilterCurrencyDigitGrouping",components:{AlohaExample:f},setup(){const{codeJs:e}=J();return{codeJs:e}}};function v(e,i,s,p,l,_){const n=t("aloha-example");return c(),u(n,{"code-js":e.codeJs,header:"digitGrouping","is-code-visible-default":!0},null,8,["code-js"])}const N=a(b,[["render",v]]);function U(){return{codeJs:`filterCurrency(2000, { digits: 0 });
+// ${r(2e3,{digits:0})}
+
+filterCurrency("25.53451", { digits: 4 });
+// ${r("25.53451",{digits:4})}`}}const S={name:"PageFilterCurrencyDigits",components:{AlohaExample:f},setup(){const{codeJs:e}=U();return{codeJs:e}}};function j(e,i,s,p,l,_){const n=t("aloha-example");return c(),u(n,{"code-js":e.codeJs,header:"digits","is-code-visible-default":!0},null,8,["code-js"])}const D=a(S,[["render",j]]);function L(){return{codeJs:`filterCurrency(123456);
+// ${r(123456)}
+
+filterCurrency("1000000.2345");
+// ${r("1000000.2345")}
+
+filterCurrency("aloha");
+// ${r("aloha")}
+
+filterCurrency(undefined);
+// ${r(void 0)}`}}const Y={name:"PageFilterCurrencyExample",components:{AlohaExample:f},setup(){const{codeJs:e}=L();return{codeJs:e}}};function w(e,i,s,p,l,_){const n=t("aloha-example");return c(),u(n,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const B=a(Y,[["render",w]]);function M(){return{codeJs:`filterCurrency(2000, { suffix: "€" });
+// ${r(2e3,{suffix:"€"})}
+
+filterCurrency("25.5", { suffix: "%" });
+// ${r("25.5",{suffix:"%"})}`}}const q={name:"PageFilterCurrencySuffix",components:{AlohaExample:f},setup(){const{codeJs:e}=M();return{codeJs:e}}};function k(e,i,s,p,l,_){const n=t("aloha-example");return c(),u(n,{"code-js":e.codeJs,header:"suffix","is-code-visible-default":!0},null,8,["code-js"])}const O=a(q,[["render",k]]);function V(){return{argumentsText:[{value:"value",types:["Number","String"],text:"_PAGE_FILTER_CURRENCY_ARGUMENTS_VALUE_"},{value:'[suffix="€"]',types:["String"],text:"_PAGE_FILTER_CURRENCY_ARGUMENTS_SUFFIX_"},{value:"[digits=2]",types:["Number"],text:"_PAGE_FILTER_CURRENCY_ARGUMENTS_DIGITS_"},{value:"[digitGrouping=true]",types:["Boolean"],text:"_PAGE_FILTER_CURRENCY_ARGUMENTS_DIGIT_GROUPING_"}]}}function W(){return{pageTitle:"filterCurrency"}}const X=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import filterCurrency from "../filterCurrency";
+
+describe("filterCurrency", () => {
+  it("should properly format a given number with default parameters", () => {
+    expect(filterCurrency(1234.5678)).toEqual("1.234,57 €");
+  });
+
+  it("should properly format a given number with custom parameters", () => {
+    expect(filterCurrency(1234.5678, { suffix: "$", digits: 3, digitGrouping: false })).toEqual("1234,568 $");
+  });
+
+  it("should return undefined when a non-number value is passed", () => {
+    expect(filterCurrency(null)).toEqual("");
+  });
+});
+`,z={name:"PageFilterCurrency",components:{AlohaPage:E,ATranslation:F,PageFilterArguments:T,PageFilterCurrencyDigitGrouping:N,PageFilterCurrencyDigits:D,PageFilterCurrencyExample:B,PageFilterCurrencySuffix:O,PageFilterImportCompositionApi:R,PageFilterImportFunction:G,PageFilterTest:A},setup(){const{pageTitle:e}=W(),{argumentsText:i}=V();return{argumentsText:i,pageTitle:e,test:X}}};function H(e,i,s,p,l,_){const n=t("a-translation"),m=t("page-filter-import-function"),g=t("page-filter-import-composition-api"),d=t("page-filter-arguments"),y=t("page-filter-currency-example"),C=t("page-filter-currency-suffix"),$=t("page-filter-currency-digits"),x=t("page-filter-currency-digit-grouping"),h=t("page-filter-test"),P=t("aloha-page");return c(),u(P,{"page-title":e.pageTitle},{body:I(()=>[o(n,{tag:"p",html:"_PAGE_FILTER_CURRENCY_DESCRIPTION_"}),o(m,{"function-name":"filterCurrency","type-import":"filters"}),o(g,{"function-name":"filterCurrency"}),o(d,{"arguments-text":e.argumentsText,"function-description":'filterCurrency(value, { [suffix="€"], [digits=2], [digitGrouping=true] })'},null,8,["arguments-text"]),o(y),o(C),o($),o(x),o(h,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const fe=a(z,[["render",H]]);export{fe as default};
