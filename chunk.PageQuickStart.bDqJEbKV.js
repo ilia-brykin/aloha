251165be0a0a6ebda14b5591d80202e94016ae8a
NKDante <NKDante@users.noreply.github.com>
2026-07-20 13:32:05 +0000

    deploy: b4741288f2ef97b4448293ba49c289e16ae9e2ef

---
+++ chunk.PageQuickStart.bDqJEbKV.js
@@ -0,0 +1,36 @@
+import{A as m}from"./chunk.AlohaHighlightjs.Cyb5M4zM.js";import{A as _,_ as u}from"./bundle.index.CpueeaOY.js";import{Y as c,Z as d,$ as t,_ as i,f as l}from"./chunk.vendor.CWr53wb5.js";import"./chunk.vendor-lodash.CWoM99Se.js";import{A}from"./chunk.APageTabTitle.DGTxREmI.js";import"./chunk.translations-ar.DRto1jDw.js";import"./chunk.translations-de.CEjcANRo.js";import"./chunk.translations-en.BjlQK_tj.js";import"./chunk.translations-es.BcLAUo0L.js";import"./chunk.translations-fr.DINmPQrz.js";import"./chunk.translations-hr.BU9Fw-5f.js";import"./chunk.translations-it.Cu9SxZzh.js";import"./chunk.translations-ru.Cs_zBe4-.js";function g(){return{installCode:`# Using npm
+$ npm install aloha-vue
+
+# Using yarn
+$ yarn add aloha-vue
+
+# Using pnpm
+$ pnpm add aloha-vue`,stylesCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import "aloha-vue/dist/aloha-vue.css";
+
+const app = createApp(App);
+app.mount("#app");`,useInHtmlCode:`<template>
+  <div>
+    <a-button @click="onClick">Click me</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      alert("The button is pressed!");
+    },
+  },
+};
+<\/script>`,useInMainCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import { AButton } from "aloha-vue";
+import "aloha-vue/dist/aloha-vue.css";
+
+const app = createApp(App);
+
+app.component("AButton", AButton);
+
+app.mount("#app");`}}const h={name:"PageQuickStart",components:{AlohaHighlightjs:m,APageTabTitle:A,ATranslation:_},setup(){const{installCode:a,stylesCode:o,useInHtmlCode:s,useInMainCode:p}=g();return{installCode:a,stylesCode:o,useInHtmlCode:s,useInMainCode:p}}};function C(a,o,s,p,f,v){const r=l("a-page-tab-title"),e=l("a-translation"),n=l("aloha-highlightjs");return c(),d("div",null,[t(r,{title:"_PAGE_QUICK_START_H1_"}),t(e,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_QUICK_START_H1_"}),t(e,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_QUICK_START_INSTALL_"}),t(n,{code:a.installCode,language:"markdown"},null,8,["code"]),o[0]||(o[0]=i("hr",{class:"a_my_5"},null,-1)),t(e,{tag:"div","safe-html":"_PAGE_QUICK_START_STYLES_"}),t(n,{code:a.stylesCode,language:"javascript"},null,8,["code"]),o[1]||(o[1]=i("hr",{class:"a_my_5"},null,-1)),t(e,{tag:"div","safe-html":"_PAGE_QUICK_START_USE_"}),t(n,{code:a.useInMainCode,language:"javascript"},null,8,["code"]),t(e,{class:"a_my_3",tag:"p","safe-html":"_PAGE_QUICK_START_IN_TEMPLATE_"}),t(n,{code:a.useInHtmlCode,language:"html"},null,8,["code"])])}const G=u(h,[["render",C]]);export{G as default};
