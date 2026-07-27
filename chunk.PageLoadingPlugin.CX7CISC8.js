9d82543957ed53f3664a8710436ff9aef298c9fa
ilia-brykin <ilia-brykin@users.noreply.github.com>
2026-07-27 10:23:44 +0000

    deploy: 7004eb1a99bd7edea41a2beb34d9288e35f558a4

---
+++ chunk.PageLoadingPlugin.CX7CISC8.js
@@ -0,0 +1,38 @@
+import{A as l}from"./chunk.AlohaHighlightjs.De6fh8wy.js";import{A as r,_}from"./bundle.index.CMibbGGJ.js";import{Y as m,Z as c,$ as o,_ as s,f as n}from"./chunk.vendor.BEEJbev-.js";import"./chunk.vendor-lodash.CwOx-Q_g.js";import{A as d}from"./chunk.APageTabTitle.zKto--AH.js";import"./chunk.translations-ar.elbT6Y_t.js";import"./chunk.translations-de.Cuff5Ky-.js";import"./chunk.translations-en.CtPp-H2K.js";import"./chunk.translations-es.DzYMGThG.js";import"./chunk.translations-fr.DcK5skg0.js";import"./chunk.translations-hr.C_RX54US.js";import"./chunk.translations-it.BofMQSkf.js";import"./chunk.translations-ru.BFp9cKv_.js";function g(){return{connectionCode:`import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ALoadingPlugin,
+} from "aloha-vue";
+
+const app = createApp(App);
+
+app.use(ALoadingPlugin, {
+  propsDefault: {
+    align: "left",
+    size: 8,
+    tag: "section",
+    text: "Loading, please wait...",
+    textAlign: "center",
+  },
+});
+
+app.mount("#app");`,useCode:`<template>
+  <div>
+    <a-loading :is-loading="true">
+      <div>Aloha</div>
+    </a-loading>
+  </div>
+</template>
+
+<script>
+import { 
+  ALoading,
+} from "aloha-vue";
+
+export default {
+  name: "LoadingExample",
+  components: {
+    ALoading,
+  },
+};
+<\/script>`}}const u={name:"PageLoadingPlugin",components:{AlohaHighlightjs:l,APageTabTitle:d,ATranslation:r},setup(){const{connectionCode:a,useCode:t}=g();return{connectionCode:a,useCode:t}}};function A(a,t,N,f,L,P){const p=n("a-page-tab-title"),e=n("a-translation"),i=n("aloha-highlightjs");return m(),c("div",null,[o(p,{title:"_PAGE_PLUGIN_LOADING_PAGE_TITLE_"}),o(e,{class:"a_mb_3",tag:"h1","safe-html":"_PAGE_PLUGIN_LOADING_H1_"}),o(e,{class:"a_mb_3",tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_INTRODUCTION_"}),t[0]||(t[0]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_CONNECTION_"}),o(i,{code:a.connectionCode,language:"javascript"},null,8,["code"]),o(e,{class:"a_mt_3",tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_CONNECTION_NOTES_"}),t[1]||(t[1]=s("hr",{class:"a_my_5"},null,-1)),o(e,{tag:"div","safe-html":"_PAGE_PLUGIN_LOADING_EXAMPLES_"}),o(i,{code:a.useCode,language:"html"},null,8,["code"])])}const j=_(u,[["render",A]]);export{j as default};
