10149820d451be7d581fa515040b6803a1484348
ilia-brykin <ilia-brykin@users.noreply.github.com>
2026-09-08 11:06:27 +0000

    deploy: 7d3a9e1b2cdcfd9c3c8513cc92fee3038cbca85d

---
+++ chunk.PageUtilsMathIsOdd.B6Dpc05L.js
@@ -0,0 +1,72 @@
+import{A as x,a as h}from"./chunk.AlohaExample.DPX_yN8b.js";import{P as $,a as A,b as T}from"./chunk.PageFilterTest.BWTSTUQY.js";import{b0 as t,_ as d,A as y}from"./bundle.index.CBuy1VXL.js";import{Y as r,W as p,f as n,aU as B,$ as s}from"./chunk.vendor.BEEJbev-.js";import"./chunk.vendor-lodash.CwOx-Q_g.js";import"./chunk.APageTabTitle.C4UHJjpP.js";import"./chunk.AlohaHighlightjs.De6fh8wy.js";import"./chunk.translations-ar.D5QoWFzr.js";import"./chunk.translations-de.es3zy6A0.js";import"./chunk.translations-en.CYlx3VY3.js";import"./chunk.translations-es.zp4usr_r.js";import"./chunk.translations-fr.DtjnUZtG.js";import"./chunk.translations-hr.DZGEx8XX.js";import"./chunk.translations-it.D2jWnc95.js";import"./chunk.translations-ru.CUfkkkdW.js";function F(){return{codeJs:`isOdd(1);
+// ${t(1)}
+isOdd(3);
+// ${t(3)}
+isOdd(245);
+// ${t(245)}
+
+isOdd(-1);
+// ${t(-1)}
+
+isOdd(2);
+// ${t(2)}
+isOdd(0);
+// ${t(0)}
+isOdd(352);
+// ${t(352)}
+
+isOdd(-2);
+// ${t(-2)}
+
+isOdd(2.5);
+// ${t(2.5)}
+
+isOdd("Aloha");
+// ${t("Aloha")}
+isOdd(null);
+// ${t(null)}
+isOdd(undefined);
+// ${t(void 0)}`}}const P={name:"PageUtilsMathIsOddExample",components:{AlohaExample:x},setup(){const{codeJs:e}=F();return{codeJs:e}}};function I(e,o,a,l,c,u){const i=n("aloha-example");return r(),p(i,{"code-js":e.codeJs,header:"_A_BASIC_USAGE_","is-code-visible-default":!0},null,8,["code-js"])}const v=d(P,[["render",I]]);function b(){return{argumentsText:[{value:"value",types:["Number"],text:"_PAGE_UTILS_MATH_IS_ODD_ARGUMENTS_VALUE_"}]}}function U(){return{pageTitle:"isOdd"}}const E=`import {
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+import {
+  isOdd,
+} from "../utilsMath";
+
+describe("isOdd function", () => {
+  it("Return true if given an odd number", () => {
+    expect(isOdd(1)).toBeTruthy();
+    expect(isOdd(5)).toBeTruthy();
+    expect(isOdd(123454567)).toBeTruthy();
+  });
+
+  it("Return false if given an even number", () => {
+    expect(isOdd(0)).toBeFalsy();
+    expect(isOdd(2)).toBeFalsy();
+    expect(isOdd(123456)).toBeFalsy();
+  });
+
+  it("Return false if given a non-integer", () => {
+    expect(isOdd(2.5)).toBeFalsy();
+    expect(isOdd("Aloha")).toBeFalsy();
+    expect(isOdd(null)).toBeFalsy();
+    expect(isOdd(undefined)).toBeFalsy();
+    expect(isOdd({})).toBeFalsy();
+    expect(isOdd([])).toBeFalsy();
+  });
+
+  it("Return true if given a negative odd number", () => {
+    expect(isOdd(-1)).toBeTruthy();
+    expect(isOdd(-345)).toBeTruthy();
+  });
+
+  it("Return false if given a negative even number", () => {
+    expect(isOdd(-2)).toBeFalsy();
+    expect(isOdd(-4)).toBeFalsy();
+    expect(isOdd(-268)).toBeFalsy();
+  });
+});
+`,M={name:"PageUtilsMathIsOdd",components:{AlohaPage:h,ATranslation:y,PageFilterArguments:T,PageFilterImportFunction:A,PageFilterTest:$,PageUtilsMathIsOddExample:v},setup(){const{pageTitle:e}=U(),{argumentsText:o}=b();return{argumentsText:o,pageTitle:e,test:E}}};function S(e,o,a,l,c,u){const i=n("a-translation"),m=n("page-filter-import-function"),_=n("page-filter-arguments"),g=n("page-utils-math-is-odd-example"),O=n("page-filter-test"),f=n("aloha-page");return r(),p(f,{"page-title":e.pageTitle},{body:B(()=>[s(i,{tag:"p",html:"_PAGE_UTILS_MATH_IS_ODD_DESCRIPTION_"}),s(m,{"function-name":"isOdd","type-import":"utilsMath"}),s(_,{"arguments-text":e.argumentsText,"function-description":"isOdd(value)"},null,8,["arguments-text"]),s(g),s(O,{test:e.test},null,8,["test"])]),_:1},8,["page-title"])}const z=d(M,[["render",S]]);export{z as default};
